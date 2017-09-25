327b95ac04251856d9402fd4726b18d65493defe
Di <didoesdigital+git@gmail.com>
2017-09-25 18:59:04 +1000

    Do not render as markup or HTML

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import marked from 'marked';
 
 class App extends Component {
   constructor(props) {
@@ -25,12 +24,12 @@ List of words
         </div>
         <div className="main">
           <div className="">
-            <div>Material:</div><div className="material matched" dangerouslySetInnerHTML={{__html: marked(this.state.material)}} />
-            <div>Typed:</div><div className="typed-text" dangerouslySetInnerHTML={{__html: marked(this.state.typedText)}} />
+            <div>Material:</div><div className="material matched">{this.state.material}</div>
+            <div>Typed:</div><div className="typed-text">{this.state.typedText}</div>
             <p className="input-text">
               <textarea classname="input-textarea" rows="1"
                 onChange={this.updateMarkup.bind(this)}
-                value={this.state.markdown}
+                value={this.state.typedText}
                 >
               </textarea>
             </p>
