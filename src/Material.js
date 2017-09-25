ac6b0d700636c8ca1e764756af721cb077e727e3
Di <didoesdigital+git@gmail.com>
2017-09-25 20:16:54 +1000

    Highlight matched material to typed text

--- src/Material.js
+++ src/Material.js
@@ -22,14 +22,25 @@ class Material extends Component {
   // }
 
   markUpMaterial(material, typedText) {
-    return material+typedText;
+    let materialChars = material.split('');
+    let typedTextChars = typedText.split('');
+    let i = 0;
+    for (; i < typedTextChars.length; i++) {
+      if (typedTextChars[i] != materialChars[i]) {
+        break;
+      }
+    };
+    let matchedMaterial = materialChars.slice(0,i).join('');
+    let unmatchedMaterial = materialChars.slice(i).join('');
+    let matchedMarkup = `<span class="matched">${matchedMaterial}</span><span>${unmatchedMaterial}</span>`;
+    return {__html: matchedMarkup};
   }
 
   render() {
     return (
       <div className="">
         <div>Material:</div>
-        <div className="material matched">{this.markUpMaterial(this.props.sourceMaterial, this.props.typedText)}</div>
+        <div className="material" dangerouslySetInnerHTML={this.markUpMaterial(this.props.sourceMaterial, this.props.typedText)} />
       </div>
     );
   }
