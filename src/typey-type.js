a40f7b4062b4505f0a54685aca18bade3874f934
Di <didoesdigital+git@gmail.com>
2017-10-07 11:27:04 +1100

    Fix != with !== in matchSplitText

--- src/typey-type.js
+++ src/typey-type.js
@@ -3,7 +3,7 @@ function matchSplitText(material, typedText) {
   let typedTextChars = typedText.split('');
   let i = 0;
   for (; i < typedTextChars.length; i++) {
-    if (typedTextChars[i] != materialChars[i]) {
+    if (typedTextChars[i] !== materialChars[i]) {
       break;
     }
   };
