1140900cfa9aa350b425330014f8ee0a07a73d4f
Di <didoesdigital+git@gmail.com>
2017-10-23 21:51:02 +1100

    Lay out header

--- src/packages/components/Header.js
+++ src/packages/components/Header.js
@@ -8,22 +8,26 @@ class Header extends Component {
   render() {
     var customMessage;
     if (this.props.settings.customMessage) {
-      customMessage = <h4>{this.props.settings.customMessage}</h4>;
+      customMessage = <h3 className="pad-24">{this.props.settings.customMessage}</h3>;
     } else {
       customMessage = '';
     }
     return (
-      <div className="app-header">
-        <nav>
-          <div role="banner"><a href="/" className="site-heading">Typey type</a></div>
-          <ul>
-            <li><a href="/lesson.txt" onClick={this.props.getLesson}>Lesson one</a></li>
-            <li><a href="/lesson-two.txt" onClick={this.props.getLesson}>Lesson two</a></li>
-            <li><a href="/lesson-three.txt" onClick={this.props.getLesson}>Lesson three</a></li>
-          </ul>
-        </nav>
-        <h1>{this.props.lessonTitle}</h1>
-        <h2>{this.props.lessonSubTitle}</h2>
+      <div>
+        <div className="header">
+          <nav>
+            <div role="banner" className="site-heading-banner"><a href="/" className="site-heading-link">Typey type</a></div>
+            <ul>
+              <li><a href="/lesson.txt" onClick={this.props.getLesson}>Lesson one</a></li>
+              <li><a href="/lesson-two.txt" onClick={this.props.getLesson}>Lesson two</a></li>
+              <li><a href="/lesson-three.txt" onClick={this.props.getLesson}>Lesson three</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div className="subheader">
+          <h1>{this.props.lessonTitle}</h1>
+          <h2>{this.props.lessonSubTitle}</h2>
+        </div>
         {customMessage}
       </div>
     )
