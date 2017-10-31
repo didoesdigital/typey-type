334159a71a5adba6a665212cfa007ea6972d10ca
Di <didoesdigital+git@gmail.com>
2017-10-31 22:25:03 +1100

    Table layout header and type styles

--- src/Header.js
+++ src/Header.js
@@ -11,36 +11,38 @@ class Header extends Component {
           <nav>
             <label htmlFor="lessons-autocomplete" className="visually-hidden">Search for a lesson</label>
             <div role="banner" className="site-heading-banner"><a href="/" className="site-heading-link">Typey type</a></div>
-            <Autocomplete
-              getItemValue={(item) => item.title}
-              inputProps={{ id: 'lessons-autocomplete' }}
-              items={this.props.items}
-              menuStyle={{ }}
-              onChange={this.props.onChange}
-              onSelect={this.props.onSelect}
-              renderInput={function(props) {
-                return <input {...props} className="form-control" />
-              }}
-              renderItem={(item, highlighted) =>
-                <div
-                  key={item.path}
-                  style={{ backgroundColor: highlighted ? '#ffd073' : 'transparent'}}
-                >
-                  <h5>{item.title}</h5>
-                  <h6>{item.subtitle}</h6>
-                  {item.category} >&nbsp;
-                  {item.subcategory}
-                </div>
-              }
-              renderMenu={function(items, value, style) {
-                return <div style={{ ...style, ...this.menuStyle }} className="autocomplete-menu" children={items}/>
-              }}
-              shouldItemRender={matchLessonToTerm}
-              sortItems={sortLessons}
-              value={this.props.value}
-              wrapperStyle={{ position: 'relative', display: 'inline-block' }}
-            />
-            <a href={this.props.path} onClick={this.props.getLesson} className="link-button" role="button">Start lesson</a>
+            <div className="table search-container">
+              <Autocomplete
+                getItemValue={(item) => item.title}
+                inputProps={{ id: 'lessons-autocomplete' }}
+                items={this.props.items}
+                menuStyle={{ }}
+                onChange={this.props.onChange}
+                onSelect={this.props.onSelect}
+                renderInput={function(props) {
+                  return <input {...props} className="inline-form-control" />
+                }}
+                renderItem={(item, highlighted) =>
+                  <div
+                    key={item.path}
+                    style={{ backgroundColor: highlighted ? '#ffd073' : 'transparent'}}
+                  >
+                    <h5>{item.title}</h5>
+                    <h6>{item.subtitle}</h6>
+                    {item.category} >&nbsp;
+                    {item.subcategory}
+                  </div>
+                }
+                renderMenu={function(items, value, style) {
+                  return <div style={{ ...style, ...this.menuStyle }} className="autocomplete-menu" children={items}/>
+                }}
+                shouldItemRender={matchLessonToTerm}
+                sortItems={sortLessons}
+                value={this.props.value}
+                wrapperStyle={{ position: 'relative', display: 'table-cell', paddingRight: '0.5em', width: '100%' }}
+              />
+              <a href={this.props.path} onClick={this.props.getLesson} className="link-button table-cell" role="button">Start lesson</a>
+            </div>
           </nav>
         </div>
         <div className="subheader">
