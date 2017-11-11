e381c1adcdbfab7a10dc1884edaf9b02dc1b0601
Di <didoesdigital+git@gmail.com>
2017-11-11 15:35:28 +1100

    Add aria-label to emoji

--- src/App.js
+++ src/App.js
@@ -443,7 +443,7 @@ class App extends Component {
           </div>
           <footer>
             <p className="text-center mb1 mt1"><small>Learn more about <a href="https://didoesdigital.com/plover/">stenography and Plover</a>.</small></p>
-            <p className="text-center mb1 mt1"><small>Made with ❤️ by <a href="https://twitter.com/didoesdigital">@DiDoesDigital</a>.</small></p>
+            <p className="text-center mb1 mt1"><small>Made with <span aria-label="love" role="img">❤️</span> by <a href="https://twitter.com/didoesdigital">@DiDoesDigital</a>.</small></p>
           </footer>
         </div>
       );
