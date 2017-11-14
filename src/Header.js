3cd0a9ceaae701815cddba94f7f9df30b84af6f6
Di <didoesdigital+git@gmail.com>
2017-11-15 07:54:27 +1100

    Change homepage link to relative link

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
         <div className="header">
           <nav>
             <label htmlFor="lessons-autocomplete" className="visually-hidden">Search for a lesson</label>
-            <div role="banner" className="site-heading-banner"><a href="/" className="site-heading-link">Typey type</a></div>
+            <div role="banner" className="site-heading-banner"><a href="./" className="site-heading-link">Typey type</a></div>
             <div className="table search-container">
               <Autocomplete
                 getItemValue={(item) => item.title}
