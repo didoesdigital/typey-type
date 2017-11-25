e266cf7280ed356f15b59bb70665f5c4549e5ba2
Di <didoesdigital+git@gmail.com>
2017-11-25 14:59:56 +1100

    Pull out test consts

--- src/utils.test.js
+++ src/utils.test.js
@@ -3,22 +3,21 @@
 import { sortLessons } from './utils';
 
 describe('sortLessons', () => {
+  const a = {
+    "title": "Longest Words",
+    "subtitle": "",
+    "category": "Drills",
+    "subcategory": "Longest Words",
+    "path": "./lessons/drills/longest-words/lesson.txt"
+  }, b = {
+    "title": "Common words",
+    "subtitle": "",
+    "category": "Drills",
+    "subcategory": "Common Words",
+    "path": "./lessons/drills/common-words/lesson.txt"
+  };
+
   it('sorts lesson by title alphabetically', () => {
-    const a = {
-        "title": "Longest Words",
-        "subtitle": "",
-        "category": "Drills",
-        "subcategory": "Longest Words",
-        "path": "./lessons/drills/longest-words/lesson.txt"
-      },
-      b = {
-        "title": "Common words",
-        "subtitle": "",
-        "category": "Drills",
-        "subcategory": "Common Words",
-        "path": "./lessons/drills/common-words/lesson.txt"
-      },
-      value = "Words";
-    expect(sortLessons(a, b, value)).toEqual(1);
+    expect(sortLessons(a, b, "Words")).toEqual(1);
   });
 });
