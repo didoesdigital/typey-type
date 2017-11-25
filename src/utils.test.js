e75e18645f158a6ae92fe72a0493ff7b01c51cdc
Di <didoesdigital+git@gmail.com>
2017-11-25 15:08:08 +1100

    Expand sortLessons test

--- src/utils.test.js
+++ src/utils.test.js
@@ -4,7 +4,7 @@ import { sortLessons } from './utils';
 
 describe('sortLessons', () => {
   const a = {
-    "title": "Longest Words",
+    "title": "The Longest Words",
     "subtitle": "",
     "category": "Drills",
     "subcategory": "Longest Words",
@@ -15,9 +15,22 @@ describe('sortLessons', () => {
     "category": "Drills",
     "subcategory": "Common Words",
     "path": "./lessons/drills/common-words/lesson.txt"
+  }, c = {
+    "title": "A good word",
+    "subtitle": "",
+    "category": "Drills",
+    "subcategory": "A Good Word",
+    "path": "./lessons/drills/common-words/lesson.txt"
   };
 
-  it('sorts lesson by title alphabetically', () => {
-    expect(sortLessons(a, b, "Words")).toEqual(1);
+  it('sorts lesson by title alphabetically where a is first', () => {
+    expect(sortLessons(a, b, "Words")).toEqual(12-7);
+  });
+
+  it('sorts lesson by title alphabetically where b is first', () => {
+    expect(sortLessons(b, a, "Words")).toEqual(7-12);
+  });
+  it('sorts lesson by title alphabetically where the query is at the same position', () => {
+    expect(sortLessons(b, c, "Word")).toEqual(1);
   });
 });
