87090f167d278db2cd375045e157269363ee0f0b
Di <didoesdigital+git@gmail.com>
2017-11-26 18:38:12 +1100

    Add methods to get phrases from Lesson and visit next phrase

--- src/lesson.js
+++ src/lesson.js
@@ -1,10 +1,23 @@
 class Lesson {
-  constructor(props) {
+  constructor(phrases) {
     this.completedPhrases = [];
-    this.currentPhrase = '';
-    this.remainingPhrases = [];
+    this.currentPhrase = phrases[0];
+    this.remainingPhrases = phrases.slice(1);
   }
 
+  getCompletedPhrases() {
+    return this.completedPhrases;
+  }
+  getCurrentPhrase() {
+    return this.currentPhrase;
+  }
+  getRemainingPhrases() {
+    return this.remainingPhrases;
+  }
+  visitNextPhrase() {
+    this.completedPhrases.push(this.currentPhrase);
+    this.currentPhrase = this.remainingPhrases.shift();
+  }
 }
 
 export default Lesson;
--- src/lesson.test.js
+++ src/lesson.test.js
@@ -1,10 +1,28 @@
 import Lesson from './lesson';
 
 describe('Lesson', () => {
-  let lesson = new Lesson();
 
   it('works', () => {
   });
 
+  describe('Initialisation', () => {
+    it('sets initial properties', () => {
+      let lesson = new Lesson(['one','two','three']);
+      expect(lesson.getCompletedPhrases()).toEqual([]);
+      expect(lesson.getCurrentPhrase()).toEqual('one');
+      expect(lesson.getRemainingPhrases()).toEqual(['two', 'three']);
+    });
+  });
+  describe('Traversal', () => {
+    it('next', () => {
+      let lesson = new Lesson(['one','two','three']);
+      lesson.visitNextPhrase();
+      expect(lesson.getCompletedPhrases()).toEqual(['one']);
+      expect(lesson.getCurrentPhrase()).toEqual('two');
+      expect(lesson.getRemainingPhrases()).toEqual(['three']);
+    });
+    it('previous', () => {
+    });
+  });
 });
 
