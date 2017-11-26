82c883b4647c059f7191e7ae614101bd0b82e860
Di <didoesdigital+git@gmail.com>
2017-11-26 18:43:34 +1100

    Add test to not mutate source in Lesson

--- src/lesson.test.js
+++ src/lesson.test.js
@@ -6,13 +6,20 @@ describe('Lesson', () => {
   });
 
   describe('Initialisation', () => {
+    let lessonSource = ['one','two','three'];
+
     it('sets initial properties', () => {
-      let lesson = new Lesson(['one','two','three']);
+      let lesson = new Lesson(lessonSource);
       expect(lesson.getCompletedPhrases()).toEqual([]);
       expect(lesson.getCurrentPhrase()).toEqual('one');
       expect(lesson.getRemainingPhrases()).toEqual(['two', 'three']);
     });
+
+    it('does not mutate source', () => {
+      expect(lessonSource).toEqual(['one','two','three']);
+    });
   });
+
   describe('Traversal', () => {
     it('next', () => {
       let lesson = new Lesson(['one','two','three']);
@@ -20,8 +27,6 @@ describe('Lesson', () => {
       expect(lesson.getCompletedPhrases()).toEqual(['one']);
       expect(lesson.getCurrentPhrase()).toEqual('two');
       expect(lesson.getRemainingPhrases()).toEqual(['three']);
-    });
-    it('previous', () => {
     });
   });
 });
