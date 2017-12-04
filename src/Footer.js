3b1d88260a5d35d0d351e24feaf75b8cda4f6771
Di <didoesdigital+git@gmail.com>
2017-12-04 20:49:56 +1100

    Change footer learn more link to Link to About

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <p className="text-center mb1 mt1"><small>Learn more about <a href="https://didoesdigital.com/plover/">stenography and Plover</a>.</small></p>
+        <p className="text-center mb1 mt1"><small><Link to="/about">About</Link></small></p>
         <p className="text-center mb1 mt1"><small>Email: <a href="mailto:didoesdigital+steno@gmail.com" className="link-missing-full-stop">DiDoesDigital+Steno@gmail.com</a></small></p>
         <p className="text-center mb1 mt1"><small>Made with <span aria-label="love" role="img">❤️</span> by <a href="https://twitter.com/didoesdigital">@DiDoesDigital</a>.</small></p>
       </footer>
