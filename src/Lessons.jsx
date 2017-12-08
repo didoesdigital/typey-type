64c2d1ce6da8207e79a044d1ad981e008f051406
Di <didoesdigital+git@gmail.com>
2017-12-08 17:35:26 +1100

    WIP: start to assemble a pathname from URL to load lesson

--- src/Lessons.jsx
+++ src/Lessons.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Lesson from './Lesson';
 
 const Lessons = ({match, lessonIndex, handleLesson, lesson, ...lessonProps}) => {
+  let currentPathname = lessonProps.location.pathname + 'lesson.txt';
+  function pathnameIsAValidLesson(lessonObject, index, array) {
+    // console.log('/lessons'+lessonObject.path);
+    // console.log(currentPathname);
+    return '/lessons'+lessonObject.path === currentPathname;
+  }
+  // let isPathnameAValidLesson = lessonIndex.some(pathnameIsAValidLesson);
+  if(lessonIndex.some(pathnameIsAValidLesson)) {
+  }
+
+  console.log(lesson);
+  const currentLesson = lessonIndex.filter(function(lessonObject){
+    return '/lessons'+lessonObject.path === currentPathname;
+  });
+  if(currentLesson.length>0){
+    console.log(currentLesson[0]);
+    lesson = currentLesson[0];
+  }
+
+  // console.log(lessonProps.location);
+  // console.log(lessonIndex);
+  // console.log(lessonProps);
+  // console.log(match);
+  // console.log(match.path);
+  // console.log(lessonIndex);
+  // console.log(lesson);
   const linkList = lessonIndex.map( (lesson) => {
     let lessonsubtitle = '';
     if (lesson.subtitle.length > 0) {
@@ -17,32 +43,34 @@ const Lessons = ({match, lessonIndex, handleLesson, lesson, ...lessonProps}) => 
 
   return(
     <div>
-      <Route path={`${match.url}/:category/:subcategory/:lessonPath`} render={ (props) =>
+      <Switch>
+        <Route path={`${match.url}/:category/:subcategory/:lessonPath`} render={ (props) =>
           <Lesson lessonIndex={lessonIndex}
             handleLesson={handleLesson}
             lesson={lesson}
             {...lessonProps}
             {...props}
-            />
+          />
+          } />
+    <Route path={`${match.url}/drills/:lessonPath`} render={ (props) =>
+      <Lesson lessonIndex={lessonIndex}
+        handleLesson={handleLesson}
+        lesson={lesson}
+        {...lessonProps}
+        {...props}
+      />
       } />
-      <Route path={`${match.url}/drills/:lessonPath`} render={ (props) =>
-          <Lesson lessonIndex={lessonIndex}
-            handleLesson={handleLesson}
-            lesson={lesson}
-            {...lessonProps}
-            {...props}
-            />
-      } />
-      <Route exact={true} path={match.url} render={() => (
-        <div>
-          <div className="main">
-            <div className="p4">
-              <h3>Lessons</h3>
-              <ul className="unstyled-list">{linkList}</ul>
-            </div>
-          </div>
-        </div>
-      )} />
+    <Route exact={true} path={match.url} render={() => (
+      <div>
+        <div className="main">
+          <div className="p4">
+            <h3>Lessons</h3>
+            <ul className="unstyled-list">{linkList}</ul>
+      </div>
+      </div>
+      </div>
+    )} />
+  </Switch>
     </div>
   )
 
