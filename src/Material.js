b80048af9009407110488fa0ffcc8e29a6195df5
Di <didoesdigital+git@gmail.com>
2017-12-24 19:48:41 +1100

    Fix spacing around zipper for different placement settings

--- src/Material.js
+++ src/Material.js
@@ -4,6 +4,15 @@ import './App.css';
 
 class Material extends Component {
   render() {
+    let spaceAfterOutput = " ";
+    if (this.props.userSettings.spacePlacement === 'spaceBeforeOutput') {
+      spaceAfterOutput = "";
+    }
+    let spaceBeforeOutput = " ";
+    if (this.props.userSettings.spacePlacement === 'spaceAfterOutput') {
+      spaceBeforeOutput = "";
+    }
+
     const [matched, unmatched] = matchSplitText(this.props.currentPhrase, this.props.actualText, this.props.settings, this.props.userSettings);
     return (
       <div className="mb1">
@@ -15,7 +24,7 @@ class Material extends Component {
         <div className="expected">
           <div className="visually-hidden">Matching and unmatching material typed:</div>
           <div className="material">
-            <pre className="material-pre"><div className="dib current-and-upcoming-phrases"><span className="matched steno-material">{matched}</span><span className="steno-material">{unmatched}</span> <span className="de-emphasized upcoming-phrases">{this.props.upcomingPhrases}</span></div><span className="dib de-emphasized completed-phrases">&#8203;{this.props.completedPhrases}</span></pre>
+            <pre className="material-pre"><div className="dib current-and-upcoming-phrases">{spaceAfterOutput}<span className="matched steno-material">{matched}</span><span className="steno-material">{unmatched}</span>{spaceBeforeOutput}<span className="de-emphasized upcoming-phrases">{this.props.upcomingPhrases}</span></div><span className="dib de-emphasized completed-phrases">&#8203;{this.props.completedPhrases}</span></pre>
           </div>
         </div>
       </div>
