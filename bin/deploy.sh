e4c6d6a0f8a5d907ca6e24fb73a57403ab4a181f
Di <didoesdigital+git@gmail.com>
2018-06-01 08:09:17 +1000

    Update deploy scripts to use new run build dict script
    run-build-dict-for-typey-type-for-standard-dict-set

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,30 +13,7 @@ if [[ $REPLY =~ ^[Yy]$ ]];
     exit
 fi
 
-ruby ~/projects/plover-tools/typey-type-lesson-generator/build-dict-for-typey-type.rb \
-  ~/dropbox-personal/projects/Plover/dictionaries/nouns.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/abbreviations.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/briefs.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/condensed-strokes.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/currency.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/dict.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/dict_en_AU_phonetic.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/dict_en_AU_vocab.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/dict_en_AU_with_extra_stroke.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/nouns.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/numbers.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/numbers_powerups.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/phonetic.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/pronouns.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/punctuation_di.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/punctuation_powerups.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/punctuation_unspaced.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/symbols.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/symbols_briefs.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/symbols_currency_culled.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/top-10000-english-words.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/top-10000-project-gutenberg-words.json \
-  ~/dropbox-personal/projects/Plover/dictionaries/top-level-domains.json
+ruby ~/projects/plover-tools/typey-type-lesson-generator/run-build-dict-for-typey-type-for-standard-dict-set.rb
 
 yarn run build
 git tag -n
