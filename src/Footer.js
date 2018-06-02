5b12c62cbca827bbc9b3aabf8818832717e10036
Di <didoesdigital+git@gmail.com>
2018-06-03 08:50:34 +1000

    Update footer link: external icon, outbound, tooltip

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import GoogleAnalytics from 'react-ga';
 import { Link } from 'react-router-dom';
+import { IconExternal } from './Icon';
+import {
+  Tooltip,
+} from 'react-tippy';
+import 'react-tippy/dist/tippy.css'
 import './App.css';
 
 class Footer extends Component {
@@ -14,7 +20,30 @@ class Footer extends Component {
       <footer role="contentinfo" className={"hide-in-fullscreen" + fullscreen}>
         <p className="text-center mt1 mb0"><small>Made with <span aria-label="love" role="img">❤️</span> by <a href="https://didoesdigital.com/">DiDoesDigital</a>.</small></p>
         <p className="text-center mb0"><small><Link to="/support">Learn more about Typey&nbsp;type</Link> and <Link to="/support#privacy">privacy</Link>.</small></p>
-        <p className="text-center mb1"><small><Link to="/contribute">Contribute to Typey&nbsp;type</Link> and <a href="https://didoesdigital.com/#newsletter" target="_blank" rel="noopener noreferrer">sign up to steno news<span className="external-link" aria-label=" (External link)" title="(External link)"></span></a>.</small></p>
+        <p className="text-center mb1"><small><Link to="/contribute">Contribute to Typey&nbsp;type</Link> and{" "}
+          <GoogleAnalytics.OutboundLink
+            eventLabel="DiDoesDigital: Typey type updates and steno news (external link opens in new tab)"
+            to="https://didoesdigital.com/#newsletter"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Typey type updates and steno news (external link opens in new tab)"
+          >
+            sign up to steno news
+            <Tooltip
+              animation="shift"
+              arrow="true"
+              className=""
+              duration="200"
+              tabIndex="0"
+              tag="span"
+              theme="didoesdigital"
+              title="(external link opens in new tab)"
+              trigger="mouseenter focus click"
+              onShow={this.props.setAnnouncementMessage}
+            >
+              <IconExternal ariaHidden="true" role="presentation" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="" />
+            </Tooltip>
+          </GoogleAnalytics.OutboundLink>.</small></p>
       </footer>
     )
   }
