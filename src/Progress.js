1e437eef25f8dac233ab712021cd44b2c368ebbd
Di <didoesdigital+git@gmail.com>
2018-06-03 22:04:15 +1000

    Show lessonsProgress checkmarks on Progress page

--- src/Progress.js
+++ src/Progress.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import Clipboard from 'clipboard';
+import { IconCheckmark } from './Icon';
+import { Link } from 'react-router-dom';
+import {
+  Tooltip,
+} from 'react-tippy';
+import 'react-tippy/dist/tippy.css'
 
 class Progress extends Component {
   componentDidMount() {
@@ -14,9 +20,75 @@ class Progress extends Component {
   };
 
   render () {
-    let lessonsProgressFromTypeyType = JSON.stringify(this.props.flashcardsProgress);
+    let lessonsProgressFromTypeyType = this.props.lessonsProgress;
+    const linkList = this.props.lessonIndex.map( (lesson) => {
+      let lessonsubtitle = '';
+      let lessonWordCount = 0;
+      let lessonWordCountInIndex = '';
+      let numberOfWordsSeen = 0;
+      let lessonCompletion;
+      if (lesson.subtitle.length > 0) {
+        lessonsubtitle = ': '+lesson.subtitle;
+      }
+      if (lesson.wordCount && lesson.wordCount > 0) {
+        lessonWordCount = lesson.wordCount;
+        lessonWordCountInIndex = '' + lessonWordCount;
+      }
+      if (lessonsProgressFromTypeyType && lessonsProgressFromTypeyType["/lessons" + lesson.path]) {
+        numberOfWordsSeen = lessonsProgressFromTypeyType["/lessons" + lesson.path].numberOfWordsSeen;
+        if ((numberOfWordsSeen >= lessonWordCountInIndex) || (numberOfWordsSeen > 100)) {
+          lessonCompletion = (
+            <Tooltip
+              title="100 words done or lesson complete"
+              className=""
+              animation="shift"
+              arrow="true"
+              duration="200"
+              tabIndex="0"
+              tag="span"
+              theme="didoesdigital"
+              trigger="mouseenter focus click"
+              onShow={this.props.setAnnouncementMessage}
+            >
+              <IconCheckmark role="img" className="svg-icon-wrapper svg-baseline color-green-bright" iconTitle="" />
+              <span className="visually-hidden">100 words done or lesson complete</span>
+            </Tooltip>
+          );
+        } else if (numberOfWordsSeen > 0) {
+          lessonCompletion = ( <small className="ml1">Started</small> );
+          // lessonCompletion = (
+          //       <Tooltip
+          //         title="In progress"
+          //         className=""
+          //         animation="shift"
+          //         arrow="true"
+          //         duration="200"
+          //         tabIndex="0"
+          //         tag="span"
+          //         theme="didoesdigital"
+          //         trigger="mouseenter focus click"
+          //         onShow={this.props.setAnnouncementMessage}
+          //       >
+          //         <IconCheckmark ariaHidden="true" role="presentation" iconFill="#8242a8" className="svg-icon-wrapper svg-baseline" iconTitle="" />
+          //       </Tooltip>
+          // );
+        }
+      }
+      if (lesson.category === "Fundamentals") {
+        return(
+          <li className="unstyled-list-item mb1" key={ lesson.path }>
+            <Link to={`/lessons${lesson.path}`.replace(/lesson\.txt$/,'').replace(/\/{2,}/g,'/')} id={'ga--lesson-index-'+lesson.path.replace(/\/lesson\.txt/g,'').replace(/[/.]/g,'-')}>{lesson.title}{lessonsubtitle}</Link> {lessonCompletion}
+            { // <!-- {numberOfWordsSeen} words seen of {lessonWordCountInIndex}-->
+            }
+          </li>
+        )
+      } else {
+        return "";
+      }
+    });
+
     let metWordsFromTypeyType = JSON.stringify(this.props.metWords);
-    let wordCount = Object.keys(this.props.metWords).length || 0;
+    let yourWordCount = Object.keys(this.props.metWords).length || 0;
     let progressPercent = Math.round(Object.keys(this.props.metWords).length / 10000 * 100) || 0;
     return (
       <div>
@@ -30,115 +102,52 @@ class Progress extends Component {
               </div>
             </div>
           </div>
-          <div className="progress-layout p3 mx-auto mw-1024">
-
-            <div className="mw-584">
-              <p>You’ve successfully typed {wordCount} words without misstrokes. You’re {progressPercent}% of the way to 10,000 words.</p>
-              <h3>Lessons progress</h3>
-              {// <p id="js-lessons-progress-from-typey-type" className="w-100 mt3 mb3 quote wrap">{lessonsProgressFromTypeyType}</p>
-                }
-                <ul class="unstyled-list">
-                  <li><ol>Fundamentals:
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-simple-keys/">One-syllable words with simple keys</a> 25 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-more-consonants/">One-syllable words with more consonants</a> 178 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-f-as-v-or-s/">One-syllable words with F as V or S</a> 2944 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-unstressed-vowels/">One-syllable words with unstressed vowels</a> 529 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-inversion/">One-syllable words with inversion</a> 98 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-short-i-vowel/">One-syllable words with short I vowel</a> 92 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-short-vowels/">One-syllable words with short vowels</a> 137 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-long-vowels/">One-syllable words with long vowels</a> 294 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-diphthongs/">One-syllable words with diphthongs</a> 29 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-vowel-disambiguators/">One-syllable words with vowel disambiguators</a> 159 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-left-hand-consonants-with-multiple-keys/">One-syllable words with left-hand consonants with multiple keys</a> 137 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-right-hand-consonants-with-multiple-keys/">One-syllable words with right-hand consonants with multiple keys</a> 294 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-digraphs/">One-syllable words with digraphs</a> 164 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-compound-clusters/">One-syllable words with compound clusters</a> 23 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/one-syllable-words-with-multiple-strokes/">One-syllable words with multiple strokes</a> 36 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/multi-syllable-words-with-briefs/">Multi-syllable words with briefs</a> 25 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/multi-syllable-words-with-prefixes/">Multi-syllable words with prefixes</a> 25 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/multi-syllable-words-with-suffixes/">Multi-syllable words with suffixes</a> 25 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/multi-syllable-words-with-multiple-strokes/">Multi-syllable words with multiple strokes</a> 25 words</li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/fundamentals/fingerspelling/">Fingerspelling</a> 25 words</li>
-                  </ol></li>
-                  <li><ol>Drills:
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/top-10000-project-gutenberg-words/">Top 10000 Project Gutenberg Words</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/top-10000-english-words/">Top 10000 English Words</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/longest-words/">Longest Words</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/common-words/">Common words</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/single-stroke-briefs/">Single-stroke briefs</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/apostrophes/">Apostrophes</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/email-salutations-and-complimentary-closings/">Email Salutations and Complimentary Closings</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/homophones/">Homophones</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/pronouns/">Pronouns</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/pronouns-continued/">Pronouns continued</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/pronouns-contractions/">Pronouns and Contractions</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/titles/">Titles</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/calendar/">Calendar</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/time/">Time</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/emotions/">Emotions</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/superlatives/">Superlatives</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/fr-fer/">FR: -fer</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/fr-ver/">FR: -ver</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/prefixes/">Prefixes</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/suffixes/">Suffixes</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/two-letter-words-common/">Two Letter Words: Common</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/two-letter-words-rare/">Two Letter Words: Rare</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/drills/steno-party-tricks/">Steno party tricks</a></li>
-                  </ol></li>
-                  <li><ol>Collections:
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/collections/user-experience/ux-vocabulary/">UX design vocabulary</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/collections/tech/jquery/">Tech</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/collections/tech/jquery/">Medical</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/collections/tech/jquery/">HR</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/collections/tech/jquery/">Two word briefs</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/collections/tech/jquery/">Two-key briefs</a></li>
-                  </ol></li>
-                  <li><ol>Stories:
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/stories/charles-perrault/the-fairy/">Charles Perrault</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/stories/hans-christian-andersen/the-ugly-duckling/">Hans Christian Andersen</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/stories/grimm/lily-and-the-lion/">Grimm</a></li>
-                    <li class="unstyled-list-item"><a href="/typey-type/lessons/stories/fables/the-lion-in-love/">Aesop's fables</a></li>
-                  </ol></li>
-                </ul>
-
-              <p>Words you’ve seen and times you’ve typed them well:</p>
-              <p id="js-metwords-from-typey-type" className="w-100 mt3 mb3 quote wrap">{metWordsFromTypeyType}</p>
+          <div className="progress-layout pl3 pr3 pt3 mx-auto mw-1024">
+            <div className="panel p3 mb3">
+              <h2>Save your progress</h2>
+              <p>Typey&nbsp;type saves your brief progress in your browser’s local storage.<strong className="bg-danger"> You’ll lose your progress if you clear your browsing data (history, cookies, and cache).</strong> If you share this device with other people or use Typey&nbsp;type across several devices and browsers, you should save your progress elsewhere. Copy your progress to your clipboard and save it in a text file somewhere safe. When you return, enter your progress to load it back into Typey&nbsp;type.</p>
+              <p className="mb0">
+                <button className="js-clipboard-button link-button copy-to-clipboard fade-out-up" data-clipboard-target="#js-metwords-from-typey-type">
+                  Copy progress to clipboard
+                </button>
+              </p>
             </div>
 
-            <div>
-              <div className="panel p3">
-                <h2>Save your progress</h2>
-                <p>Typey&nbsp;type saves your brief progress in your browser’s local storage.<strong className="bg-danger"> You’ll lose your progress if you clear your browsing data (history, cookies, and cache).</strong> If you share this device with other people or use Typey&nbsp;type across several devices and browsers, you should save your progress elsewhere. Copy your progress to your clipboard and save it in a text file somewhere safe. When you return, enter your progress to load it back into Typey&nbsp;type.</p>
-                <p className="text-right mb0">
-                  <button className="js-clipboard-button link-button copy-to-clipboard fade-out-up" data-clipboard-target="#js-metwords-from-typey-type">
-                    Copy progress to clipboard
-                  </button>
-                </p>
-              </div>
-
-              <div className="panel mt3 p3">
-                <h3 className="mt0">Restore your progress</h3>
-                <p className="mt3 mb3 lh1">
-                  <label htmlFor="metWords-from-personal-store" className="inline-block">Enter your progress here:</label>
-                  <textarea
-                    id="metwords-from-personal-store"
-                    className="js-metwords-from-personal-store db w-100"
-                    autoCapitalize="off"
-                    autoComplete="off"
-                    autoCorrect="off"
-                    spellCheck="false"
-                    rows="1"
-                  />
-                </p>
-                <p className="text-right mb0">
-                  <button onClick={this.restoreButtonOnClickFunction.bind(this)} className="link-button load-progress fade-out-up">
-                    Load progress
-                  </button>
-                </p>
-              </div>
-
+            <div className="panel p3 mb3">
+              <h2 className="mt0">Load your progress</h2>
+              <p className="mt2 mb3">
+                Restore your progress from a previous session by entering your saved progress and loading it into Typey&nbsp;type. You can also clear your progress by loading in empty curly braces, <code>{"{}"}</code>.
+              </p>
+              <p className="mt4 mb0">
+                <label htmlFor="metWords-from-personal-store" className="inline-block mb05">Enter your progress here:</label>
+                <textarea
+                  id="metwords-from-personal-store"
+                  className="js-metwords-from-personal-store db w-100"
+                  autoCapitalize="off"
+                  autoComplete="off"
+                  autoCorrect="off"
+                  spellCheck="false"
+                  rows="2"
+                />
+              </p>
+              <p className="mt2 mb0">
+                <button onClick={this.restoreButtonOnClickFunction.bind(this)} className="link-button load-progress fade-out-up">
+                  Load progress into Typey&nbsp;type
+                </button>
+              </p>
             </div>
+          </div>
 
+          <div className="p3 mx-auto mw-1024">
+            <h2>Your progress</h2>
+            <p>You’ve successfully typed {yourWordCount} words without misstrokes. You’re {progressPercent}% of the way to 10,000 words.</p>
+
+            <h3>Fundamental lessons progress</h3>
+            <ul className="unstyled-list">{linkList}</ul>
+
+            <h3>Vocabulary progress</h3>
+            <p>Words you’ve seen and times you’ve typed them well:</p>
+            <p id="js-metwords-from-typey-type" className="w-100 mt3 mb3 quote wrap"><small>{metWordsFromTypeyType}</small></p>
           </div>
         </main>
       </div>
