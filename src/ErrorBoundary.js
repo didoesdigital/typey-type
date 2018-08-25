d3b0590cd20a28916a727ae1e07fdd963db92bf2
Di <didoesdigital+git@gmail.com>
2018-08-25 15:42:41 +1000

    Remove comment noise

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -16,15 +16,11 @@ class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     // Display fallback UI
     this.setState({ hasError: true });
-    // You can also log the error to an error reporting service
-    // Sentry capture here!
-    // logErrorToMyService(error, info);
     Sentry.captureException(error, { extra: info });
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return (
         <div className="center-all">
           <p>Sorry, Typey Type hit an error. I’ve been notified and will look into this as soon as I can. Meanwhile, try <a href=".">refresh the page</a>. Thanks for your patience. —Di</p>
