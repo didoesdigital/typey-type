9cdbb9a8281154b209f0b48682e0aa602e7606d8
Di <didoesdigital+git@gmail.com>
2018-08-25 15:46:20 +1000

    Housekeeping: whitespace

--- bin/deploy-staging.sh
+++ bin/deploy-staging.sh
@@ -16,7 +16,14 @@ fi
 
 ruby ~/projects/plover-tools/typey-type-lesson-generator/run-build-dict-for-typey-type-for-standard-dict-set.rb
 
+
+
 yarn run build
+
+
 # git tag -n
 rsync --archive --verbose --delete --exclude=".DS_Store" -e "ssh -p 2222" ~/projects/typey-type/build/ di@localhost:www/typey-type/
+
+
+
 say "Deployed to staging on port 8080."
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -15,6 +15,9 @@ fi
 
 ruby ~/projects/plover-tools/typey-type-lesson-generator/run-build-dict-for-typey-type-for-standard-dict-set.rb
 
+
+
+
 yarn run build
 git tag -n
 # cd ~/projects/thebakery/diidau-src/typey-type/
@@ -23,9 +26,14 @@ git tag -n
 # cd ~/projects/thebakery/di.id.au/
 tig status
 
+
+
 # yarn run build
 # git tag -n
 # rsync --archive --verbose --delete --exclude=".DS_Store" -e "ssh -p 4242" ~/projects/typey-type/build/ di@didoesdigital:www/typey-type/
 # rsync -avz --delete --exclude=".DS_Store" ~/projects/typey-type/build/ di@didoesdigital:www/typey-type/
 rsync -avz --delete --exclude=".DS_Store" ~/projects/typey-type/build/ di@167.99.9.71:www/typey-type/
+
+
+
 say "Deployed to production at DaɪDoesDigital.com"
