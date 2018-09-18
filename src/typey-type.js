196d40c032b9288188d3770a9b31ec50f428d6d9
Di <didoesdigital+git@gmail.com>
2018-09-19 07:16:49 +1000

    Remove suffix and prefix strokes from myWords revision

--- src/typey-type.js
+++ src/typey-type.js
@@ -6,6 +6,16 @@ function createWordListFromMetWords (metWords) {
   let metWordsEntries = Object.entries(munged).sort(function (a, b) {
     return b[1] - a[1]
   });
+
+  // remove prefix and suffix entries because they're annoying to write
+  // while practicing regular words
+  metWordsEntries = metWordsEntries.filter(item => {
+    let containsCaret = item[0].indexOf('^') !== -1;
+    let isJustACaretCharacter = item[0] === '^';
+    let isJustACaretCharacterWithSpaceBefore = item[0] === ' ^';
+    let isJustACaretCharacterWithSpaceAfter = item[0] === '^ ';
+    return !containsCaret || isJustACaretCharacter || isJustACaretCharacterWithSpaceBefore || isJustACaretCharacterWithSpaceAfter;
+  });
   return metWordsEntries.map(entry => entry[0].trim());
 }
 
