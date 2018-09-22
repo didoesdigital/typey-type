ede7e993492bb16e22e34d3c949dc193b72d2744
Di <didoesdigital+git@gmail.com>
2018-09-22 13:43:26 +1000

    Add link to community's lessons on Lessons index page
    Turn link into CTA link button for 'create a custom lesson'

--- src/LessonsIndex.jsx
+++ src/LessonsIndex.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAnalytics from 'react-ga';
+import { IconExternal } from './Icon';
+import { Tooltip } from 'react-tippy';
 
 class LessonsIndex extends Component {
 
@@ -40,12 +43,37 @@ class LessonsIndex extends Component {
           </div>
         </div>
         <div className="p3 mx-auto mw-1024">
-          <p><Link to={`${this.props.match.url}/custom`.replace(/\/{2,}/g,'/')} id="ga--lesson-index--custom">Create a custom lesson</Link></p>
+          <h3>Custom lessons</h3>
+          <p>To help Typey&nbsp;Type grow even faster, add your custom lessons to the{' '}
+            <GoogleAnalytics.OutboundLink
+              eventLabel="community's lessons"
+              to="https://docs.google.com/spreadsheets/d/1AlO2SSUwuv3yrz7RI9ix_z1Efbiu_j50c_ibGYwdsgc/edit?usp=sharing"
+              target="_blank">
+              community’s lessons
+              <Tooltip
+                title="Opens in a new tab"
+                animation="shift"
+                arrow="true"
+                className=""
+                duration="200"
+                tabIndex="0"
+                tag="span"
+                theme="didoesdigital"
+                trigger="mouseenter focus click"
+                onShow={this.props.setAnnouncementMessage}
+              >
+                <IconExternal ariaHidden="true" role="presentation" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="" />
+              </Tooltip>
+            </GoogleAnalytics.OutboundLink>.
+          </p>
+          <p><Link to={`${this.props.match.url}/custom`.replace(/\/{2,}/g,'/')} className="link-button dib"  style={{lineHeight: 2}} id="ga--lesson-index--create-a-custom-lesson">Create a custom lesson</Link></p>
+          <h3>Typey&nbsp;Type lessons</h3>
           <ul className="unstyled-list">{linkList}</ul>
         </div>
       </main>
     )
   }
 }
+            // There are also <Link to="/support#palantype">palantype</Link> lessons available.
 
 export default LessonsIndex;
