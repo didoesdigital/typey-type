a7e5c1320e5649dc60e51693c6531c5a0b0e738f
Di <didoesdigital+git@gmail.com>
2018-10-20 09:04:56 +1100

    Fix redundant main element in PageNotFound; add link

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -5,7 +5,7 @@ import {
 
 const PageNotFound = () => {
   return (
-    <main id="main">
+    <div>
       <a href="#main" className="skip-to-main-link link-button link-button-ghost">Skip to main content</a>
       <div className="header" role="banner">
         <div className="mx-auto mw-1024 p3">
@@ -22,12 +22,13 @@ const PageNotFound = () => {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/lessons">Lessons</Link></li>
+          <li><Link to="/lessons/drills/top-100-words/">Top 100 Words</Link></li>
           <li><Link to="/support">Help and about</Link></li>
           <li><Link to="/contribute">Contribute to Typey&nbsp;Type</Link></li>
           <li><Link to="/progress">Your progress</Link></li>
         </ul>
       </main>
-    </main>
+    </div>
   )
 }
 
