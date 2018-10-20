5c8f9d77fc469929864925d70cc4675f86560982
Di <didoesdigital+git@gmail.com>
2018-10-21 08:10:57 +1100

    Add subheader to page loading past delay

--- src/PageLoadingPastDelay.js
+++ src/PageLoadingPastDelay.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class PageLoadingPastDelay extends Component {
   render() {
-    return <div className="p3 mx-auto mw-1024">Loading…</div>;
+    return (
+      <main id="main">
+        <div className="subheader">
+          <div className="flex items-baseline mx-auto mw-1024 justify-between p3">
+            <div className="flex mr1 self-center">
+              <header className="flex items-baseline">
+                <h2 ref={(heading) => { this.mainHeading = heading; }} tabIndex="-1">Loading…</h2>
+              </header>
+            </div>
+          </div>
+        </div>
+        <div className="p3 mx-auto mw-1024">Loading…</div>;
+      </main>
+    );
   }
 }
 
