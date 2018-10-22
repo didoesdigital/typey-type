da5d23252628322c65a896cc46a01d41c0ea7eb5
Di <didoesdigital+git@gmail.com>
2018-10-22 17:02:25 +1100

    Housekeeping: formatting

--- src/Contribute.js
+++ src/Contribute.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import GoogleAnalytics from 'react-ga';
 import { Link } from 'react-router-dom';
 import { IconExternal } from './Icon';
-import {
-  Tooltip,
-} from 'react-tippy';
+import { Tooltip } from 'react-tippy';
 import 'react-tippy/dist/tippy.css'
 
 class Contribute extends Component {
