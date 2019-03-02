5a9ffa23c741ce24293e59d75ada55963bde9045
Di <didoesdigital+git@gmail.com>
2019-03-02 13:56:25 +1100

    Rename Japanese key variables

--- src/typey-type.js
+++ src/typey-type.js
@@ -138,43 +138,43 @@ function mapBriefToDanishStenoKeys (brief) {
 
 function mapBriefToJapaneseStenoKeys (brief) {
   let keys = {
-    the漢Key: false,
-    theLeftKagikakkoKey: false,
-    theLeft4Key: false,
-    theLeftたKey: false,
-    theLeftなKey: false,
-    theLeft3Key: false,
-    theLeftかKey: false,
-    theLeftさKey: false,
-    theLeft2Key: false,
-    theLeftいKey: false,
-    theLeftうKey: false,
-    theLeft1Key: false,
-    theLeftおKey: false,
-    theLeftっKey: false,
-    theStarKey: false,
+    the漢: false,
+    theLeftKagikakko: false,
+    theLeft4: false,
+    theLeftた: false,
+    theLeftな: false,
+    theLeft3: false,
+    theLeftか: false,
+    theLeftさ: false,
+    theLeft2: false,
+    theLeftい: false,
+    theLeftう: false,
+    theLeft1: false,
+    theLeftお: false,
+    theLeftっ: false,
+    theStar: false,
     dash: false,
-    theRight4Key: false,
-    theRightたKey: false,
-    theRightなKey: false,
-    theRight3Key: false,
-    theRightかKey: false,
-    theRightさKey: false,
-    theRight2Key: false,
-    theRightいKey: false,
-    theRightうKey: false,
-    theRight1Key: false,
-    theRightおKey: false,
-    theRightっKey: false,
-    theRightKagikakkoKey: false,
-    theカKey: false
+    theRight4: false,
+    theRightた: false,
+    theRightな: false,
+    theRight3: false,
+    theRightか: false,
+    theRightさ: false,
+    theRight2: false,
+    theRightい: false,
+    theRightう: false,
+    theRight1: false,
+    theRightお: false,
+    theRightっ: false,
+    theRightKagikakko: false,
+    theカ: false
   };
 
   let briefLetters = brief.split("");
 
-  // stenoOrder and stenoKeys should always be updated together
+  // stenoOrder and stenos should always be updated together
   let stenoOrder = ["漢", "「", "4", "た", "な", "3", "か", "さ", "2", "い", "う", "1", "お", "っ", "*", "-", "4", "た", "な", "3", "か", "さ", "2", "い", "う", "1", "お", "っ", "」", "カ"];
-  let stenoKeys = ["the漢Key", "theLeftKagikakkoKey", "theLeft4Key", "theLeftたKey", "theLeftなKey", "theLeft3Key", "theLeftかKey", "theLeftさKey", "theLeft2Key", "theLeftいKey", "theLeftうKey", "theLeft1Key", "theLeftおKey", "theLeftっKey", "theStarKey", "dash", "theRight4Key", "theRightたKey", "theRightなKey", "theRight3Key", "theRightかKey", "theRightさKey", "theRight2Key", "theRightいKey", "theRightうKey", "theRight1Key", "theRightおKey", "theRightっKey", "theRightKagikakkoKey", "theカKey"];
+  let stenoKeys = ["the漢", "theLeftKagikakko", "theLeft4", "theLeftた", "theLeftな", "theLeft3", "theLeftか", "theLeftさ", "theLeft2", "theLeftい", "theLeftう", "theLeft1", "theLeftお", "theLeftっ", "theStar", "dash", "theRight4", "theRightた", "theRightな", "theRight3", "theRightか", "theRightさ", "theRight2", "theRightい", "theRightう", "theRight1", "theRightお", "theRightっ", "theRightKagikakko", "theカ"];
 
   for (let i = 0; i < stenoOrder.length; i++) {
     if (briefLetters.length > 0) {
