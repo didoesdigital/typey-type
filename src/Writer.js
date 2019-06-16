f2b2f8d31a27c0efea2ea4f416f5f184c06e0fd2
Di <didoesdigital+git@gmail.com>
2019-06-16 10:12:26 +1000

    Update UI, add qwerty/raw setting & send stroke button

--- src/Writer.js
+++ src/Writer.js
@@ -31,6 +31,7 @@ type State = {
   stenoBrief: string,
   stenoStroke: Stroke,
   stenoDictionary: Object,
+  writerInput: string,
   writtenText: string,
   valueRawSteno: string,
   valueQWERTYSteno: string
@@ -43,6 +44,7 @@ class Writer extends Component<Props, State> {
     stenoBrief: '',
     stenoStroke: new Stroke(),
     stenoDictionary: {},
+    writerInput: 'qwerty',
     writtenText: '',
     valueRawSteno: '',
     valueQWERTYSteno: ''
@@ -113,6 +115,15 @@ class Writer extends Component<Props, State> {
     });
   }
 
+  sendDiagramStroke() {
+    let stenoBrief = this.state.stenoStroke.toString();
+    let writtenText = this.lookUpStrokeInDictionary(stenoBrief);
+    this.setState({
+      stenoStroke: new Stroke(),
+      writtenText: writtenText
+    });
+  }
+
   lookUpStrokeInDictionary(stenoBrief: string) {
     let translation = stenoBrief;
     if (this.state.stenoDictionary[stenoBrief]) {
@@ -132,6 +143,7 @@ class Writer extends Component<Props, State> {
 
   addKeyToStenoBrief(key: string) {
     let stenoStroke = this.state.stenoStroke.set(key);
+
     let stenoBrief = this.state.stenoBrief;
     if (!stenoBrief.includes(key)) {
       stenoBrief = stenoBrief + key;
@@ -139,6 +151,18 @@ class Writer extends Component<Props, State> {
     this.setState({
       stenoBrief: stenoStroke.toString(),
       stenoStroke: stenoStroke
+    });
+  }
+
+  changeWriterInput(event: SyntheticInputEvent<HTMLInputElement>) {
+    let writerInput = this.state.writerInput;
+
+    if (event && event.target && event.target.name) {
+      writerInput = event.target.name;
+    }
+
+    this.setState({
+      writerInput: writerInput
     });
   }
 
@@ -189,72 +213,109 @@ class Writer extends Component<Props, State> {
           </div>
         </div>
         <div className="p3 mx-auto mw-1024">
-          <div className="mx-auto mw-568">
-            <p className="mt3 mb3 h3">
-              <span className="visually-hidden">Your written text:</span>{this.state.writtenText}&nbsp;
-            </p>
-            <div>
-              <StenoLayoutDiagram {...mapBriefsFunction(this.state.stenoBrief)} newOnClick={this.addKeyToStenoBrief.bind(this)} brief={"STKPWHRAO*EUFRPBLGTSDZ"} diagramWidth="440" />
+          <div className="flex flex-wrap justify-between">
+            <div className="mw-384 w-336 order-1">
+              <h3>Settings</h3>
+              <div className="flex flex-wrap">
+                { this.props.userSettings.stenoLayout === "stenoLayoutAmericanSteno" && this.state.writerInput === "qwerty" ?
+                  <p className="mt3 mb3 mr1">
+                    <label htmlFor="qwertyStenoInput" className="db">
+                      QWERTY steno input
+                    </label>
+                    <input
+                      id="qwertyStenoInput"
+                      autoCapitalize="off"
+                      autoComplete="off"
+                      autoCorrect="off"
+                      className="input-textarea"
+                      onChange={this.updateQWERTYSteno}
+                      value={this.state.valueQWERTYSteno}
+                    />
+                  </p>
+                  :
+                  null
+                }
+                { this.state.writerInput === "raw" || !(this.props.userSettings.stenoLayout === "stenoLayoutAmericanSteno") ?
+                  <p className="mt3 mb3 mr1">
+                    <label htmlFor="rawStenoInput" className="db">
+                      Raw steno input
+                    </label>
+                    <input
+                      id="rawStenoInput"
+                      autoCapitalize="off"
+                      autoComplete="off"
+                      autoCorrect="off"
+                      className="input-textarea"
+                      onChange={this.updateRawSteno}
+                      value={this.state.valueRawSteno}
+                    />
+                  </p>
+                  :
+                  null
+                }
+              </div>
+              <div className="mb2 mw-240">
+                <Tooltip
+                  title="Show a steno diagram that suits your steno layout"
+                  className="mw-240"
+                  animation="shift"
+                  arrow="true"
+                  duration="200"
+                  tabIndex="0"
+                  tag="span"
+                  theme="didoesdigital didoesdigital-sm"
+                  trigger="mouseenter focus click"
+                  onShow={this.props.setAnnouncementMessage}
+                >
+                  <label className="mb1 db" htmlFor="stenoLayout">Steno layout</label>
+                </Tooltip>
+                <select id="stenoLayout" name="stenoLayout" value={this.props.userSettings.stenoLayout} onChange={this.props.changeStenoLayout} className="text-small form-control w6">
+                  <option value="stenoLayoutAmericanSteno">American steno (Ward Stone Ireland)</option>
+                  <option value="stenoLayoutPalantype">Palantype</option>
+                  <option value="stenoLayoutDanishSteno">Danish steno</option>
+                  <option value="stenoLayoutItalianMichelaSteno">Italian Michela steno</option>
+                  <option value="stenoLayoutJapaneseSteno">Japanese steno</option>
+                  <option value="stenoLayoutKoreanModernCSteno">Korean Modern C steno</option>
+                </select>
+              </div>
+              <fieldset>
+                <legend>Raw, QWERTY or diagram</legend>
+                <div className="flex">
+                  <div className="flex flex-wrap justify-between">
+                    <div className="radio mr3">
+                      <label htmlFor="raw">
+                        <input type="radio" name="raw" id="raw" onChange={this.changeWriterInput.bind(this)} checked={this.state.writerInput === "raw"} /> Raw
+                      </label>
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap justify-between">
+                    <div className="radio mr3">
+                      <label htmlFor="qwerty">
+                        <input type="radio" name="qwerty" id="qwerty" onChange={this.changeWriterInput.bind(this)} checked={this.state.writerInput === "qwerty"} /> QWERTY
+                      </label>
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap justify-between">
+                    <div className="radio mr3">
+                      <label htmlFor="diagram">
+                        <input type="radio" name="diagram" id="diagram" onChange={this.changeWriterInput.bind(this)} checked={this.state.writerInput === "diagram"} /> Diagram
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </fieldset>
             </div>
-            <div className="flex flex-wrap">
-              { this.props.userSettings.stenoLayout === "stenoLayoutAmericanSteno" ?
-                <p className="mt3 mb3 mr1">
-                  <label htmlFor="qwertyStenoInput" className="db">
-                    QWERTY steno input
-                  </label>
-                  <input
-                    id="qwertyStenoInput"
-                    autoCapitalize="off"
-                    autoComplete="off"
-                    autoCorrect="off"
-                    className="input-textarea"
-                    onChange={this.updateQWERTYSteno}
-                    value={this.state.valueQWERTYSteno}
-                  />
-                </p>
-                :
-                null
-              }
-              <p className="mt3 mb3 mr1">
-                <label htmlFor="rawStenoInput" className="db">
-                  Raw steno input
-                </label>
-                <input
-                  id="rawStenoInput"
-                  autoCapitalize="off"
-                  autoComplete="off"
-                  autoCorrect="off"
-                  className="input-textarea"
-                  onChange={this.updateRawSteno}
-                  value={this.state.valueRawSteno}
-                />
+            <div className="mw-568">
+              <p className="mt3 mb3 h3">
+                <span className="visually-hidden">Your written text:</span>{this.state.writtenText}&nbsp;
+              </p>
+              <div>
+                <StenoLayoutDiagram {...mapBriefsFunction(this.state.stenoBrief)} newOnClick={this.addKeyToStenoBrief.bind(this)} brief={"STKPWHRAO*EUFRPBLGTSDZ"} diagramWidth="440" />
+              </div>
+              <p className="text-center mr4 mt1">
+                <button onClick={this.sendDiagramStroke.bind(this)} className="button text-center">Send stroke</button>
               </p>
             </div>
-            <div className="mb2 mw-240">
-              <Tooltip
-                title="Show a steno diagram that suits your steno layout"
-                className="mw-240"
-                animation="shift"
-                arrow="true"
-                duration="200"
-                tabIndex="0"
-                tag="span"
-                theme="didoesdigital didoesdigital-sm"
-                trigger="mouseenter focus click"
-                onShow={this.props.setAnnouncementMessage}
-              >
-                <label className="mb1 db" htmlFor="stenoLayout">Steno layout</label>
-              </Tooltip>
-              <select id="stenoLayout" name="stenoLayout" value={this.props.userSettings.stenoLayout} onChange={this.props.changeStenoLayout} className="text-small form-control w6">
-                <option value="stenoLayoutAmericanSteno">American steno (Ward Stone Ireland)</option>
-                <option value="stenoLayoutPalantype">Palantype</option>
-                <option value="stenoLayoutDanishSteno">Danish steno</option>
-                <option value="stenoLayoutItalianMichelaSteno">Italian Michela steno</option>
-                <option value="stenoLayoutJapaneseSteno">Japanese steno</option>
-                <option value="stenoLayoutKoreanModernCSteno">Korean Modern C steno</option>
-              </select>
-            </div>
-
           </div>
         </div>
       </main>
