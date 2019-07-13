2869b5ac3bdae5e470da396821e194db4ddd3bb2
Di <didoesdigital+git@gmail.com>
2019-07-13 14:34:47 +1000

    Fix test for new Map instead of object

--- src/StrokesForWords.test.js
+++ src/StrokesForWords.test.js
@@ -3,12 +3,14 @@ import { createListOfStrokes } from './StrokesForWords';
 describe('create list of strokes', () => {
   it('list of strokes and which dictionary they came from', () => {
     let phrase = 'baz';
-    let dictionaryOfWordsStrokesAndSourceDictionary = {
+    let dictionaryOfWordsStrokesAndSourceDictionary = new Map(
+    Object.entries({
       "baz": [
         ["PWAZ", "personal.json"],
         ["PWAZ", "code.json"]
       ]
-    };
+    }));
+
     let listOfStrokesAndDicts = [
       ["PWAZ", "personal.json"],
       ["PWAZ", "code.json"]
