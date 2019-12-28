9028ed04b6ae5936a0d5248b2e5eb8b7dd9be787
Di <didoesdigital+git@gmail.com>
2019-12-28 15:34:25 +1100

    Conditionally add main id="main" to errors
    If the whole main region errors, add a new main region with id="main" so
    that the skip link has a target.
    
    If only a small part of a page errors, don't add a duplicate main region
    with id="main".

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -33,20 +33,32 @@ class ErrorBoundary extends Component {
   render() {
     if (this.state.hasError && this.state.disabledCookieError) {
       return (
-        <div className="mh-page">
-          <div className="center-all">
-            <h1>You have cookies disabled</h1>
-            <p>Typey Type uses local storage to keep track of your steno settings and progress. It won’t work with cookies blocked. Please enable cookies and <a href=".">refresh the page</a>. —Di</p>
+        <main id="main">
+          <div className="mh-page">
+            <div className="center-all">
+              <h1>You have cookies disabled</h1>
+              <p>Typey Type uses local storage to keep track of your steno settings and progress. It won’t work with cookies blocked. Please enable cookies and <a href=".">refresh the page</a>. —Di</p>
+            </div>
           </div>
+        </main>
+      )
+    }
+    else if (this.state.hasError && this.props.relative) {
+      return (
+        <div className="mt3">
+          <p>Sorry, Typey Type hit an error. I’ve been notified and will look into this as soon as I can. Meanwhile, try <a href=".">refresh the page</a>. Thanks for your patience. —Di</p>
         </div>
       )
-    } else if (this.state.hasError) {
+    }
+    else if (this.state.hasError) {
       return (
-        <div className={this.props.relative ? "" : "mh-page"}>
-          <div className={this.props.relative ? "mt3" : "center-all"}>
-            <p>Sorry, Typey Type hit an error. I’ve been notified and will look into this as soon as I can. Meanwhile, try <a href=".">refresh the page</a>. Thanks for your patience. —Di</p>
+        <main id="main">
+          <div className="mh-page">
+            <div className="center-all">
+              <p>Sorry, Typey Type hit an error. I’ve been notified and will look into this as soon as I can. Meanwhile, try <a href=".">refresh the page</a>. Thanks for your patience. —Di</p>
+            </div>
           </div>
-        </div>
+        </main>
       )
     }
 
