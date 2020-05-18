4eb763e0465179b566ac75e6ead6116d3ef9b1fb
Di <didoesdigital+git@gmail.com>
2020-05-18 20:07:07 +1000

    Extract testable function in StrokesForWords
    Effectively removes "trim lookup text whitespace if no matches"
    functionality in previous commit

--- src/components/StrokesForWords.js
+++ src/components/StrokesForWords.js
@@ -50,140 +50,7 @@ class StrokesForWords extends Component {
       this.props.onChange(phrase);
     }
 
-    let lookupText = phrase;
-
-    if (phrase === "{") { lookupText = "\\{{^}"; }
-    if (phrase === "}") { lookupText = "{^}\\}"; }
-    if (phrase === "{ ") { lookupText = "\\{"; }
-    if (phrase === "} ") { lookupText = "\\}"; }
-    if (phrase === "[") { lookupText = "{^[^}"; }
-    if (phrase === "]") { lookupText = "{^]^}"; }
-    if (phrase === "[ ") { lookupText = "{[}"; }
-    if (phrase === "] ") { lookupText = "{]}"; }
-    if (phrase === "?") { lookupText = "{?}"; }
-    if (phrase === ".") { lookupText = "{^.^}"; }
-    if (phrase === ". ") { lookupText = "{.}"; }
-    if (phrase === ", ") { lookupText = "{,}"; }
-    if (phrase === `” `) { lookupText = "{^~|”}"; }
-    if (phrase === `”`) { lookupText = "{^~|”}"; }
-    if (phrase === `“`) { lookupText = "{~|“^}"; }
-    if (phrase === ` “`) { lookupText = "{~|“^}"; }
-    if (phrase === `“`) { lookupText = "{~|“^}"; }
-    if (phrase === ` ‘`) { lookupText = "{~|‘^}"; }
-    if (phrase === `‘`) { lookupText = "{~|‘^}"; }
-    if (phrase === `’ `) { lookupText = "{^~|’}"; }
-    if (phrase === `’`) { lookupText = "{^~|’}"; }
-    if (phrase === `" `) { lookupText = "{^~|\"}"; }
-    if (phrase === `"`) { lookupText = "{~|\"^}"; }
-    if (phrase === ` "`) { lookupText = "{~|\"^}"; }
-    if (phrase === ` '`) { lookupText = "{~|'^}"; }
-    if (phrase === `' `) { lookupText = "{^~|'}"; }
-    if (phrase === ` `) { lookupText = "{^ ^}"; }
-
-    if (phrase === "1") { lookupText = "{&1}"; }
-    if (phrase === "2") { lookupText = "{&2}"; }
-    if (phrase === "3") { lookupText = "{&3}"; }
-    if (phrase === "4") { lookupText = "{&4}"; }
-    if (phrase === "5") { lookupText = "{&5}"; }
-    if (phrase === "6") { lookupText = "{&6}"; }
-    if (phrase === "7") { lookupText = "{&7}"; }
-    if (phrase === "8") { lookupText = "{&8}"; }
-    if (phrase === "9") { lookupText = "{&9}"; }
-    if (phrase === "0") { lookupText = "{&0}"; }
-
-    let listOfStrokesAndDicts = createListOfStrokes(lookupText, this.props.globalLookupDictionary);
-
-    if (phrase === "{") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes("{^}" + lookupText, this.props.globalLookupDictionary)); }
-    if (phrase === "}") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes(lookupText + "{^}", this.props.globalLookupDictionary)); }
-    // if (phrase === "[") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes("{^}" + lookupText, this.props.globalLookupDictionary)); }
-    // if (phrase === "]") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes(lookupText + "{^}", this.props.globalLookupDictionary)); }
-
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^}" + lookupText + "{^}", this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^}" + lookupText, this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes(lookupText + "{^}", this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^" + lookupText + "^}", this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^" + lookupText + "}", this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{" + lookupText + "^}", this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{" + lookupText + "}", this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    if (listOfStrokesAndDicts.length === 0) {
-      let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes(lookupText.trim(), this.props.globalLookupDictionary);
-      listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
-    }
-    listOfStrokesAndDicts = rankOutlines(listOfStrokesAndDicts, phrase);
-
-    if (phrase === "A") { listOfStrokesAndDicts = [["A*P", "fingerspelling"]]; }
-    if (phrase === "B") { listOfStrokesAndDicts = [["PW*P", "fingerspelling"]]; }
-    if (phrase === "C") { listOfStrokesAndDicts = [["KR*P", "fingerspelling"]]; }
-    if (phrase === "D") { listOfStrokesAndDicts = [["TK*P", "fingerspelling"]]; }
-    if (phrase === "E") { listOfStrokesAndDicts = [["*EP", "fingerspelling"]]; }
-    if (phrase === "F") { listOfStrokesAndDicts = [["TP*P", "fingerspelling"]]; }
-    if (phrase === "G") { listOfStrokesAndDicts = [["TKPW*P", "fingerspelling"]]; }
-    if (phrase === "H") { listOfStrokesAndDicts = [["H*P", "fingerspelling"]]; }
-    if (phrase === "I") { listOfStrokesAndDicts = [["*EUP", "fingerspelling"]]; }
-    if (phrase === "J") { listOfStrokesAndDicts = [["SKWR*P", "fingerspelling"]]; }
-    if (phrase === "K") { listOfStrokesAndDicts = [["K*P", "fingerspelling"]]; }
-    if (phrase === "L") { listOfStrokesAndDicts = [["HR*P", "fingerspelling"]]; }
-    if (phrase === "M") { listOfStrokesAndDicts = [["PH*P", "fingerspelling"]]; }
-    if (phrase === "N") { listOfStrokesAndDicts = [["TPH*P", "fingerspelling"]]; }
-    if (phrase === "O") { listOfStrokesAndDicts = [["O*P", "fingerspelling"]]; }
-    if (phrase === "P") { listOfStrokesAndDicts = [["P*P", "fingerspelling"]]; }
-    if (phrase === "Q") { listOfStrokesAndDicts = [["KW*P", "fingerspelling"]]; }
-    if (phrase === "R") { listOfStrokesAndDicts = [["R*P", "fingerspelling"]]; }
-    if (phrase === "S") { listOfStrokesAndDicts = [["S*P", "fingerspelling"]]; }
-    if (phrase === "T") { listOfStrokesAndDicts = [["T*P", "fingerspelling"]]; }
-    if (phrase === "U") { listOfStrokesAndDicts = [["*UP", "fingerspelling"]]; }
-    if (phrase === "V") { listOfStrokesAndDicts = [["SR*P", "fingerspelling"]]; }
-    if (phrase === "W") { listOfStrokesAndDicts = [["W*P", "fingerspelling"]]; }
-    if (phrase === "X") { listOfStrokesAndDicts = [["KP*P", "fingerspelling"]]; }
-    if (phrase === "Y") { listOfStrokesAndDicts = [["KWR*P", "fingerspelling"]]; }
-    if (phrase === "Z") { listOfStrokesAndDicts = [["STKPW*P", "fingerspelling"]]; }
-    if (phrase === "a") { listOfStrokesAndDicts = [["A*", "fingerspelling"]]; }
-    if (phrase === "b") { listOfStrokesAndDicts = [["PW*", "fingerspelling"]]; }
-    if (phrase === "c") { listOfStrokesAndDicts = [["KR*", "fingerspelling"]]; }
-    if (phrase === "d") { listOfStrokesAndDicts = [["TK*", "fingerspelling"]]; }
-    if (phrase === "e") { listOfStrokesAndDicts = [["*E", "fingerspelling"]]; }
-    if (phrase === "f") { listOfStrokesAndDicts = [["TP*", "fingerspelling"]]; }
-    if (phrase === "g") { listOfStrokesAndDicts = [["TKPW*", "fingerspelling"]]; }
-    if (phrase === "h") { listOfStrokesAndDicts = [["H*", "fingerspelling"]]; }
-    if (phrase === "i") { listOfStrokesAndDicts = [["*EU", "fingerspelling"]]; }
-    if (phrase === "j") { listOfStrokesAndDicts = [["SKWR*", "fingerspelling"]]; }
-    if (phrase === "k") { listOfStrokesAndDicts = [["K*", "fingerspelling"]]; }
-    if (phrase === "l") { listOfStrokesAndDicts = [["HR*", "fingerspelling"]]; }
-    if (phrase === "m") { listOfStrokesAndDicts = [["PH*", "fingerspelling"]]; }
-    if (phrase === "n") { listOfStrokesAndDicts = [["TPH*", "fingerspelling"]]; }
-    if (phrase === "o") { listOfStrokesAndDicts = [["O*", "fingerspelling"]]; }
-    if (phrase === "p") { listOfStrokesAndDicts = [["P*", "fingerspelling"]]; }
-    if (phrase === "q") { listOfStrokesAndDicts = [["KW*", "fingerspelling"]]; }
-    if (phrase === "r") { listOfStrokesAndDicts = [["R*", "fingerspelling"]]; }
-    if (phrase === "s") { listOfStrokesAndDicts = [["S*", "fingerspelling"]]; }
-    if (phrase === "t") { listOfStrokesAndDicts = [["T*", "fingerspelling"]]; }
-    if (phrase === "u") { listOfStrokesAndDicts = [["*U", "fingerspelling"]]; }
-    if (phrase === "v") { listOfStrokesAndDicts = [["SR*", "fingerspelling"]]; }
-    if (phrase === "w") { listOfStrokesAndDicts = [["W*", "fingerspelling"]]; }
-    if (phrase === "x") { listOfStrokesAndDicts = [["KP*", "fingerspelling"]]; }
-    if (phrase === "y") { listOfStrokesAndDicts = [["KWR*", "fingerspelling"]]; }
-    if (phrase === "z") { listOfStrokesAndDicts = [["STKPW*", "fingerspelling"]]; }
+    const listOfStrokesAndDicts = lookupListOfStrokesAndDicts(phrase, this.props.globalLookupDictionary);
 
     this.setState({
       phrase: phrase,
@@ -365,6 +232,141 @@ class StrokesForWords extends Component {
         loadingOrError
     );
   }
+}
+
+function lookupListOfStrokesAndDicts(phrase, globalLookupDictionary) {
+  let lookupText = phrase;
+
+  if (phrase === "{") { lookupText = "\\{{^}"; }
+  if (phrase === "}") { lookupText = "{^}\\}"; }
+  if (phrase === "{ ") { lookupText = "\\{"; }
+  if (phrase === "} ") { lookupText = "\\}"; }
+  if (phrase === "[") { lookupText = "{^[^}"; }
+  if (phrase === "]") { lookupText = "{^]^}"; }
+  if (phrase === "[ ") { lookupText = "{[}"; }
+  if (phrase === "] ") { lookupText = "{]}"; }
+  if (phrase === "?") { lookupText = "{?}"; }
+  if (phrase === ".") { lookupText = "{^.^}"; }
+  if (phrase === ". ") { lookupText = "{.}"; }
+  if (phrase === ", ") { lookupText = "{,}"; }
+  if (phrase === `” `) { lookupText = "{^~|”}"; }
+  if (phrase === `”`) { lookupText = "{^~|”}"; }
+  if (phrase === `“`) { lookupText = "{~|“^}"; }
+  if (phrase === ` “`) { lookupText = "{~|“^}"; }
+  if (phrase === `“`) { lookupText = "{~|“^}"; }
+  if (phrase === ` ‘`) { lookupText = "{~|‘^}"; }
+  if (phrase === `‘`) { lookupText = "{~|‘^}"; }
+  if (phrase === `’ `) { lookupText = "{^~|’}"; }
+  if (phrase === `’`) { lookupText = "{^~|’}"; }
+  if (phrase === `" `) { lookupText = "{^~|\"}"; }
+  if (phrase === `"`) { lookupText = "{~|\"^}"; }
+  if (phrase === ` "`) { lookupText = "{~|\"^}"; }
+  if (phrase === ` '`) { lookupText = "{~|'^}"; }
+  if (phrase === `' `) { lookupText = "{^~|'}"; }
+  if (phrase === ` `) { lookupText = "{^ ^}"; }
+
+  if (phrase === "1") { lookupText = "{&1}"; }
+  if (phrase === "2") { lookupText = "{&2}"; }
+  if (phrase === "3") { lookupText = "{&3}"; }
+  if (phrase === "4") { lookupText = "{&4}"; }
+  if (phrase === "5") { lookupText = "{&5}"; }
+  if (phrase === "6") { lookupText = "{&6}"; }
+  if (phrase === "7") { lookupText = "{&7}"; }
+  if (phrase === "8") { lookupText = "{&8}"; }
+  if (phrase === "9") { lookupText = "{&9}"; }
+  if (phrase === "0") { lookupText = "{&0}"; }
+
+  let listOfStrokesAndDicts = createListOfStrokes(lookupText, globalLookupDictionary);
+
+  if (phrase === "{") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes("{^}" + lookupText, globalLookupDictionary)); }
+  if (phrase === "}") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes(lookupText + "{^}", globalLookupDictionary)); }
+  // if (phrase === "[") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes("{^}" + lookupText, globalLookupDictionary)); }
+  // if (phrase === "]") { listOfStrokesAndDicts = listOfStrokesAndDicts.concat(createListOfStrokes(lookupText + "{^}", globalLookupDictionary)); }
+
+  if (listOfStrokesAndDicts.length === 0) {
+    let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^}" + lookupText + "{^}", globalLookupDictionary);
+    listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
+  }
+  if (listOfStrokesAndDicts.length === 0) {
+    let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^}" + lookupText, globalLookupDictionary);
+    listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
+  }
+  if (listOfStrokesAndDicts.length === 0) {
+    let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes(lookupText + "{^}", globalLookupDictionary);
+    listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
+  }
+  if (listOfStrokesAndDicts.length === 0) {
+    let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^" + lookupText + "^}", globalLookupDictionary);
+    listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
+  }
+  if (listOfStrokesAndDicts.length === 0) {
+    let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{^" + lookupText + "}", globalLookupDictionary);
+    listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
+  }
+  if (listOfStrokesAndDicts.length === 0) {
+    let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{" + lookupText + "^}", globalLookupDictionary);
+    listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
+  }
+  if (listOfStrokesAndDicts.length === 0) {
+    let listOfStrokesAndDictsWithSuppressedSpaces = createListOfStrokes("{" + lookupText + "}", globalLookupDictionary);
+    listOfStrokesAndDicts = listOfStrokesAndDicts.concat(listOfStrokesAndDictsWithSuppressedSpaces);
+  }
+  listOfStrokesAndDicts = rankOutlines(listOfStrokesAndDicts, phrase);
+
+  if (phrase === "A") { listOfStrokesAndDicts = [["A*P", "fingerspelling"]]; }
+  if (phrase === "B") { listOfStrokesAndDicts = [["PW*P", "fingerspelling"]]; }
+  if (phrase === "C") { listOfStrokesAndDicts = [["KR*P", "fingerspelling"]]; }
+  if (phrase === "D") { listOfStrokesAndDicts = [["TK*P", "fingerspelling"]]; }
+  if (phrase === "E") { listOfStrokesAndDicts = [["*EP", "fingerspelling"]]; }
+  if (phrase === "F") { listOfStrokesAndDicts = [["TP*P", "fingerspelling"]]; }
+  if (phrase === "G") { listOfStrokesAndDicts = [["TKPW*P", "fingerspelling"]]; }
+  if (phrase === "H") { listOfStrokesAndDicts = [["H*P", "fingerspelling"]]; }
+  if (phrase === "I") { listOfStrokesAndDicts = [["*EUP", "fingerspelling"]]; }
+  if (phrase === "J") { listOfStrokesAndDicts = [["SKWR*P", "fingerspelling"]]; }
+  if (phrase === "K") { listOfStrokesAndDicts = [["K*P", "fingerspelling"]]; }
+  if (phrase === "L") { listOfStrokesAndDicts = [["HR*P", "fingerspelling"]]; }
+  if (phrase === "M") { listOfStrokesAndDicts = [["PH*P", "fingerspelling"]]; }
+  if (phrase === "N") { listOfStrokesAndDicts = [["TPH*P", "fingerspelling"]]; }
+  if (phrase === "O") { listOfStrokesAndDicts = [["O*P", "fingerspelling"]]; }
+  if (phrase === "P") { listOfStrokesAndDicts = [["P*P", "fingerspelling"]]; }
+  if (phrase === "Q") { listOfStrokesAndDicts = [["KW*P", "fingerspelling"]]; }
+  if (phrase === "R") { listOfStrokesAndDicts = [["R*P", "fingerspelling"]]; }
+  if (phrase === "S") { listOfStrokesAndDicts = [["S*P", "fingerspelling"]]; }
+  if (phrase === "T") { listOfStrokesAndDicts = [["T*P", "fingerspelling"]]; }
+  if (phrase === "U") { listOfStrokesAndDicts = [["*UP", "fingerspelling"]]; }
+  if (phrase === "V") { listOfStrokesAndDicts = [["SR*P", "fingerspelling"]]; }
+  if (phrase === "W") { listOfStrokesAndDicts = [["W*P", "fingerspelling"]]; }
+  if (phrase === "X") { listOfStrokesAndDicts = [["KP*P", "fingerspelling"]]; }
+  if (phrase === "Y") { listOfStrokesAndDicts = [["KWR*P", "fingerspelling"]]; }
+  if (phrase === "Z") { listOfStrokesAndDicts = [["STKPW*P", "fingerspelling"]]; }
+  if (phrase === "a") { listOfStrokesAndDicts = [["A*", "fingerspelling"]]; }
+  if (phrase === "b") { listOfStrokesAndDicts = [["PW*", "fingerspelling"]]; }
+  if (phrase === "c") { listOfStrokesAndDicts = [["KR*", "fingerspelling"]]; }
+  if (phrase === "d") { listOfStrokesAndDicts = [["TK*", "fingerspelling"]]; }
+  if (phrase === "e") { listOfStrokesAndDicts = [["*E", "fingerspelling"]]; }
+  if (phrase === "f") { listOfStrokesAndDicts = [["TP*", "fingerspelling"]]; }
+  if (phrase === "g") { listOfStrokesAndDicts = [["TKPW*", "fingerspelling"]]; }
+  if (phrase === "h") { listOfStrokesAndDicts = [["H*", "fingerspelling"]]; }
+  if (phrase === "i") { listOfStrokesAndDicts = [["*EU", "fingerspelling"]]; }
+  if (phrase === "j") { listOfStrokesAndDicts = [["SKWR*", "fingerspelling"]]; }
+  if (phrase === "k") { listOfStrokesAndDicts = [["K*", "fingerspelling"]]; }
+  if (phrase === "l") { listOfStrokesAndDicts = [["HR*", "fingerspelling"]]; }
+  if (phrase === "m") { listOfStrokesAndDicts = [["PH*", "fingerspelling"]]; }
+  if (phrase === "n") { listOfStrokesAndDicts = [["TPH*", "fingerspelling"]]; }
+  if (phrase === "o") { listOfStrokesAndDicts = [["O*", "fingerspelling"]]; }
+  if (phrase === "p") { listOfStrokesAndDicts = [["P*", "fingerspelling"]]; }
+  if (phrase === "q") { listOfStrokesAndDicts = [["KW*", "fingerspelling"]]; }
+  if (phrase === "r") { listOfStrokesAndDicts = [["R*", "fingerspelling"]]; }
+  if (phrase === "s") { listOfStrokesAndDicts = [["S*", "fingerspelling"]]; }
+  if (phrase === "t") { listOfStrokesAndDicts = [["T*", "fingerspelling"]]; }
+  if (phrase === "u") { listOfStrokesAndDicts = [["*U", "fingerspelling"]]; }
+  if (phrase === "v") { listOfStrokesAndDicts = [["SR*", "fingerspelling"]]; }
+  if (phrase === "w") { listOfStrokesAndDicts = [["W*", "fingerspelling"]]; }
+  if (phrase === "x") { listOfStrokesAndDicts = [["KP*", "fingerspelling"]]; }
+  if (phrase === "y") { listOfStrokesAndDicts = [["KWR*", "fingerspelling"]]; }
+  if (phrase === "z") { listOfStrokesAndDicts = [["STKPW*", "fingerspelling"]]; }
+
+  return listOfStrokesAndDicts;
 }
 
 function createListOfStrokes(phrase, dictionaryOfWordsStrokesAndSourceDictionary) {
