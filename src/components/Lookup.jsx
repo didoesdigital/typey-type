d279cd5677e60c9802c21cbe2b24d20317be5bbc
Di <didoesdigital+git@gmail.com>
2020-09-21 14:15:11 +1000

    Remove redundant div for mh-page

--- src/components/Lookup.jsx
+++ src/components/Lookup.jsx
@@ -38,55 +38,53 @@ class Lookup extends Component {
             </div>
           </div>
         </div>
-        <div className="p3 mx-auto mw-1024">
-          <div className="mh-page">
-            <div className="flex flex-wrap justify-between">
-              <div className="mw-368 order-1">
-                <div className="mt0">
-                  <h3 className="h4">Share link</h3>
-                  <p className="mb0 mw-320 truncate"><a className="py05 dib" id="js-bookmark-url" href={this.state.bookmarkURL}>https://didoesdigital.com{this.state.bookmarkURL}</a></p>
-                </div>
-                <div className="panel p3 mt3">
-                  <p>This lookup uses Plover’s latest dictionary and Typey&nbsp;Type’s suggestions.</p>
-                  <p className="mb0">If you notice any odd strokes,{" "}
-                    <GoogleAnalytics.OutboundLink
-                      eventLabel="post to the feedback form"
-                      aria-label="post to the feedback form (external link opens in new tab)"
-                      to="https://docs.google.com/forms/d/e/1FAIpQLSeevsX2oYEvnDHd3y8weg5_7-T8QZsF93ElAo28JO9Tmog-7Q/viewform?usp=sf_link"
-                      target="_blank"
-                      rel="noopener noreferrer"
+        <div className="p3 mx-auto mw-1024 mh-page">
+          <div className="flex flex-wrap justify-between">
+            <div className="mw-368 order-1">
+              <div className="mt0">
+                <h3 className="h4">Share link</h3>
+                <p className="mb0 mw-320 truncate"><a className="py05 dib" id="js-bookmark-url" href={this.state.bookmarkURL}>https://didoesdigital.com{this.state.bookmarkURL}</a></p>
+              </div>
+              <div className="panel p3 mt3">
+                <p>This lookup uses Plover’s latest dictionary and Typey&nbsp;Type’s suggestions.</p>
+                <p className="mb0">If you notice any odd strokes,{" "}
+                  <GoogleAnalytics.OutboundLink
+                    eventLabel="post to the feedback form"
+                    aria-label="post to the feedback form (external link opens in new tab)"
+                    to="https://docs.google.com/forms/d/e/1FAIpQLSeevsX2oYEvnDHd3y8weg5_7-T8QZsF93ElAo28JO9Tmog-7Q/viewform?usp=sf_link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    use the <span className="nowrap">feedback form
+                    <Tooltip
+                      title="Opens in new tab"
+                      className=""
+                      animation="shift"
+                      arrow="true"
+                      duration="200"
+                      tabIndex="0"
+                      tag="span"
+                      theme="didoesdigital"
+                      trigger="mouseenter focus click"
+                      onShow={this.props.setAnnouncementMessage}
                     >
-                      use the <span className="nowrap">feedback form
-                      <Tooltip
-                        title="Opens in new tab"
-                        className=""
-                        animation="shift"
-                        arrow="true"
-                        duration="200"
-                        tabIndex="0"
-                        tag="span"
-                        theme="didoesdigital"
-                        trigger="mouseenter focus click"
-                        onShow={this.props.setAnnouncementMessage}
-                      >
-                        <IconExternal ariaHidden="true" role="presentation" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="" />
-                      </Tooltip></span>
-                    </GoogleAnalytics.OutboundLink>.</p>
-                </div>
-                <div className="panel panel--warning p3 mt3"><p className="mb0">This feature is evolving. Improvements yet to&nbsp;come.</p></div>
+                      <IconExternal ariaHidden="true" role="presentation" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="" />
+                    </Tooltip></span>
+                  </GoogleAnalytics.OutboundLink>.</p>
               </div>
-              <div className="mw-584 w-100 flex-grow mr3 mh-384">
-                <StrokesForWords
-                  fetchAndSetupGlobalDict={this.props.fetchAndSetupGlobalDict}
-                  globalLookupDictionary={this.props.globalLookupDictionary}
-                  globalLookupDictionaryLoaded={this.props.globalLookupDictionaryLoaded}
-                  globalUserSettings={this.props.globalUserSettings}
-                  lookupTerm={this.props.lookupTerm}
-                  onChange={this.strokesForWordsChange.bind(this)}
-                  updateGlobalLookupDictionary={this.props.updateGlobalLookupDictionary}
-                  userSettings={this.props.userSettings}
-                />
-              </div>
+              <div className="panel panel--warning p3 mt3"><p className="mb0">This feature is evolving. Improvements yet to&nbsp;come.</p></div>
+            </div>
+            <div className="mw-584 w-100 flex-grow mr3 min-h-384">
+              <StrokesForWords
+                fetchAndSetupGlobalDict={this.props.fetchAndSetupGlobalDict}
+                globalLookupDictionary={this.props.globalLookupDictionary}
+                globalLookupDictionaryLoaded={this.props.globalLookupDictionaryLoaded}
+                globalUserSettings={this.props.globalUserSettings}
+                lookupTerm={this.props.lookupTerm}
+                onChange={this.strokesForWordsChange.bind(this)}
+                updateGlobalLookupDictionary={this.props.updateGlobalLookupDictionary}
+                userSettings={this.props.userSettings}
+              />
             </div>
           </div>
         </div>
