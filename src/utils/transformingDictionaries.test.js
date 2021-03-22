7ec8087125ff4fac16441ca551f42212bfbc9f28
Di <didoesdigital+git@gmail.com>
2021-03-22 15:49:48 +1000

    Add test for rankOutlines with gutenberg entries

--- src/utils/transformingDictionaries.test.js
+++ src/utils/transformingDictionaries.test.js
@@ -1388,6 +1388,32 @@ describe('rank outlines', () => {
     });
   });
 
+  describe('with gutenberg entries', () => {
+    it('returns sorted list of outlines for "get" where the gutenberg entry comes first', () => {
+      let arrayOfStrokesAndTheirSourceDictNames = [
+        ["TKWET", "misstrokes.json"],
+        ["TPWET", "misstrokes.json"],
+        ["TKPWHET", "misstrokes.json"],
+        ["TKPWETD", "misstrokes.json"],
+        ["TKPWET", "top-10000-project-gutenberg-words.json"],
+        ["TKPW-T", "typey-type.json"],
+        ["TKPWELT", "misstrokes.json"],
+        ["TKPET", "misstrokes.json"],
+      ];
+
+      expect(rankOutlines(arrayOfStrokesAndTheirSourceDictNames, "upstarted")).toEqual([
+        ["TKWET", "misstrokes.json"],
+        ["TPWET", "misstrokes.json"],
+        ["TKPET", "misstrokes.json"],
+        ["TKPWET", "top-10000-project-gutenberg-words.json"],
+        ["TKPW-T", "typey-type.json"],
+        ["TKPWETD", "misstrokes.json"],
+        ["TKPWHET", "misstrokes.json"],
+        ["TKPWELT", "misstrokes.json"],
+      ]);
+    });
+  });
+
   // describe('with different outlines across dictionaries', () => {
   //   it('returns sorted list of outlines for "upholstery", showing user dictionaries before typey-type.json', () => {
   //     let arrayOfStrokesAndTheirSourceDictNames = [
