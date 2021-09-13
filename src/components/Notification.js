cad9db00f38311d413604cc399116a8aa2e2e68c
Di <didoesdigital+git@gmail.com>
2021-09-13 15:27:44 +1000

    Update Notification styling
    - Removing flex-wrap ensures the closing cross stays on the one line
    - Adding wrap ensures email addresses, URLs, or other long, unbroken
    strings that appear in the Notification body do not force the layout to
    be wider than the viewport

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -36,7 +36,7 @@ class Notification extends Component {
     return (
       <div>
         { this.state.showNotification ?
-          <div className="notification notification--global flex flex-wrap justify-between pa1 p1 p3 pa3 items-center bg-danger">
+          <div className="notification notification--global flex wrap justify-between pa1 p1 p3 pa3 items-center bg-danger">
             <div className="notification__message">
               {this.props.children}
             </div>
