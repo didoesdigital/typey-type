9ec221fcdce38081feb9bf872d320d56adb73438
Di <didoesdigital+git@gmail.com>
2021-10-11 16:14:31 +1000

    Add ComponentLoading.js and friends

---
+++ src/components/ComponentLoading.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ComponentLoadingFailed from './ComponentLoadingFailed';
+import ComponentLoadingPastDelay from './ComponentLoadingPastDelay';
+
+class ComponentLoading extends Component {
+
+  render() {
+    if (this.props.error) {
+      // When the loader has errored
+      return <ComponentLoadingFailed />;
+    } else if (this.props.timedOut) {
+      // When the loader has taken longer than the timeout
+      return <div>Taking a long time… <button onClick={ this.props.retry }>Retry</button></div>;
+    } else if (this.props.pastDelay) {
+      // When the loader has taken longer than the delay
+      return <ComponentLoadingPastDelay />;
+    } else {
+      // When the loader has just started
+      return null;
+    }
+  }
+}
+
+export default ComponentLoading;
---
+++ src/components/ComponentLoadingPastDelay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ComponentLoadingPastDelay extends Component {
+  render() {
+    return (
+      <div className="center-all">
+        <div className="p3 mx-auto mw-1024">Loading…</div>
+      </div>
+    );
+  }
+}
+
+export default ComponentLoadingPastDelay;
