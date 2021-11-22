69ffc189e9c916d0c91b4471d1a1c92914cf23b2
Di <didoesdigital+git@gmail.com>
2021-11-22 17:27:15 +1000

    Add subcategory subheadings on LessonList

--- src/components/LessonList.jsx
+++ src/components/LessonList.jsx
@@ -20,36 +20,78 @@ const LessonLink = ({ lesson, url }) => (
 );
 
 export default function LessonList({ lessonIndex, url }) {
-  const categories = groups(lessonIndex, (d) => d.category);
+  const groupedLessons = groups(
+    lessonIndex,
+    (d) => d.category,
+    (d) => d.subcategory
+  );
   return (
     <div>
       <p className="mb0">Jump to:</p>
       <ul>
-        {categories.map(([category, _]) => (
+        {groupedLessons.map(([category, subcategories]) => (
           <li key={category}>
             <a href={`#${category.toLowerCase()}`}>{category}</a>
+            {subcategories[0][0] &&
+              <ul>
+              {subcategories.map(([subcategory, _]) => (
+                <li><a href={`#${subcategory.toLowerCase()}`}>{subcategory}</a></li>
+              ))}
+              </ul>
+            }
           </li>
         ))}
       </ul>
-      {categories.map(([category, lessons]) => (
-        <div key={category}>
-          <a
-            href={`#${category.toLowerCase()}`}
-            id={category.toLowerCase()}
-            className="heading-link--content"
-          >
-            <h4>{category}</h4>
-          </a>
-          <ul className="unstyled-list">
-            {lessons.map((lesson) => (
-              <li className="unstyled-list-item mb1" key={lesson.path}>
-                <LessonLink lesson={lesson} url={url} />
-                <WordCount lesson={lesson} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      {groupedLessons.map(([category, subcategories]) => {
+        return (
+          <div key={category}>
+            <a
+              href={`#${category.toLowerCase()}`}
+              id={category.toLowerCase()}
+              className="heading-link--content"
+            >
+              <h4>{category}</h4>
+            </a>
+            {subcategories.map(([subcategory, lessons]) => {
+              if (subcategory) {
+                return (
+                  <div>
+                    <a
+                      href={`#${subcategory.toLowerCase()}`}
+                      id={subcategory.toLowerCase()}
+                      className="heading-link--content"
+                    >
+                      <h5>{subcategory}</h5>
+                    </a>
+                    <ul className="unstyled-list">
+                      {lessons.map((lesson) => (
+                        <li
+                          className="unstyled-list-item mb1"
+                          key={lesson.path}
+                        >
+                          <LessonLink lesson={lesson} url={url} />
+                          <WordCount lesson={lesson} />
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                );
+              } else {
+                return (
+                  <ul className="unstyled-list">
+                    {lessons.map((lesson) => (
+                      <li className="unstyled-list-item mb1" key={lesson.path}>
+                        <LessonLink lesson={lesson} url={url} />
+                        <WordCount lesson={lesson} />
+                      </li>
+                    ))}
+                  </ul>
+                );
+              }
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
