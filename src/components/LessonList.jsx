65bbd740a9f77d78b358990a687d26a9219ef9bd
Di <didoesdigital+git@gmail.com>
2021-11-22 16:20:37 +1000

    Add subheadings to LessonIndex

--- src/components/LessonList.jsx
+++ src/components/LessonList.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { groups } from 'd3-array';
+
+const WordCount = ({lesson}) => lesson?.wordCount > 0 && ` · ${lesson.wordCount} words`
+const LessonLink = ({lesson, url}) => <Link to={`${url}${lesson.path}`.replace(/lesson\.txt$/,'').replace(/\/{2,}/g,'/')} id={'ga--lesson-index-'+lesson.path.replace(/\/lesson\.txt/g,'').replace(/[/.]/g,'-')}>{lesson.title}{lesson.subtitle?.length > 0 && `: ${lesson.subtitle}`}</Link>
 
 export default function LessonList({ lessonIndex, url }) {
-  const linkList = lessonIndex.map( (lesson) => {
-    let lessonsubtitle = '';
-    let wordCount = 0;
-    let wordCountInIndex = '';
-    if (lesson.subtitle && lesson.subtitle.length > 0) {
-      lessonsubtitle = ': '+lesson.subtitle;
-    }
-    if (lesson.wordCount && lesson.wordCount > 0) {
-      wordCount = lesson.wordCount;
-      wordCountInIndex = ' · ' + wordCount + ' words';
-    }
-    return(
-      <li className="unstyled-list-item mb1" key={ lesson.path }>
-        <Link to={`${url}${lesson.path}`.replace(/lesson\.txt$/,'').replace(/\/{2,}/g,'/')} id={'ga--lesson-index-'+lesson.path.replace(/\/lesson\.txt/g,'').replace(/[/.]/g,'-')}>{lesson.title}{lessonsubtitle}</Link>{wordCountInIndex}
-      </li>
-    )
-  });
-
-  return <ul className="unstyled-list">{linkList}</ul>
+  const categories = groups(lessonIndex, d => d.category)
+  return <div>
+    <p className="mb0">Jump to:</p>
+    <ul>
+      {categories.map(([category, _]) => <li key={category}><a href={`#${category.toLowerCase()}`}>{category}</a></li>)}
+    </ul>
+    {categories.map(([category, lessons]) => (
+      <div key={category}>
+        <a href={`#${category.toLowerCase()}`} id={category.toLowerCase()} className="heading-link--content"><h4>{category}</h4></a>
+        <ul className="unstyled-list">
+          {lessons.map((lesson) =>
+            <li className="unstyled-list-item mb1" key={ lesson.path }>
+              <LessonLink lesson={lesson} url={url} /><WordCount lesson={lesson} />
+            </li>
+          )}
+        </ul>
+      </div>
+    ))}
+  </div>
 }
+
