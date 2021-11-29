493955ca0941259971f86757470eaeb176006ce8
Di <didoesdigital+git@gmail.com>
2021-11-29 15:08:26 +1000

    Add transformingFinishedData.test.js

---
+++ src/utils/transformingFinishedData.test.js
@@ -0,0 +1,75 @@
+import {
+  // stitchTogetherLessonData,
+  transformLessonDataToChartData,
+} from "./transformingFinishedData";
+
+const lessonData = {
+  version: 1,
+  lessonStrokes: [
+    {
+      numberOfMatchedWordsSoFar: 0.6,
+      word: "on",
+      typedText: " on",
+      attempts: [],
+      stroke: "OPB",
+      checked: false,
+      accuracy: true,
+      time: 1638161791613,
+    },
+    {
+      numberOfMatchedWordsSoFar: 1.4,
+      word: "him",
+      typedText: " him",
+      attempts: [
+        {
+          text: " hix",
+          time: 1638161792156,
+          numberOfMatchedWordsSoFar: 1.4,
+        },
+      ],
+      stroke: "HEUPL",
+      checked: true,
+      accuracy: false,
+      time: 1638161792828,
+    },
+  ],
+  startTime: 1638161791284, // "2021-11-29T04:56:31.284Z",
+  wpm: 28,
+};
+
+describe("Transforming lesson data to chart data", () => {
+  it("returns flat chart data", () => {
+    const expectedChartData = {
+      averageWPM: 28,
+      marks: [
+        {
+          elapsedTime: 329,
+          wordsPerMinute: 548.3333333333334,
+          typedText: " on",
+          material: "on",
+          markedCorrect: true,
+          hint: "OPB",
+        },
+        {
+          elapsedTime: 1544,
+          wordsPerMinute: 1102.857142857143,
+          typedText: " hix",
+          material: "him",
+          markedCorrect: false,
+          hint: "HEUPL",
+        },
+        {
+          elapsedTime: 1544,
+          wordsPerMinute: 1102.857142857143,
+          typedText: " him",
+          material: "him",
+          markedCorrect: false,
+          hint: "HEUPL",
+        },
+      ],
+    };
+    expect(transformLessonDataToChartData(lessonData)).toEqual(
+      expectedChartData
+    );
+  });
+});
