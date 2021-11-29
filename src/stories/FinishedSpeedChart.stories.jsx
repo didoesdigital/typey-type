fd7b15878e117a73a6c5f838f6218a5db7486503
Di <didoesdigital+git@gmail.com>
2021-11-29 10:10:16 +1000

    Add FinishedSpeedChart.stories.jsx

---
+++ src/stories/FinishedSpeedChart.stories.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import FinishedSpeedChart from '../components/FinishedSpeedChart';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Speed chart',
+  component: FinishedSpeedChart,
+};
+
+const Template = (args) => <FinishedSpeedChart {...args} />;
+
+export const ShortLesson = Template.bind({});
+ShortLesson.args = {
+  // data: {}
+};
+ShortLesson.storyName = "Short lesson"
+
+export const LongLesson = Template.bind({});
+LongLesson.args = {
+  // data: {}
+};
+LongLesson.storyName = "Long lesson"
+
+export const WPM5 = Template.bind({});
+WPM5.args = {
+  // data: {}
+};
+WPM5.storyName = "5 WPM lesson"
+
+export const WPM21 = Template.bind({});
+WPM21.args = {
+  // data: {}
+};
+WPM21.storyName = "21 WPM lesson"
+
+export const WPM23 = Template.bind({});
+WPM23.args = {
+  // data: {}
+};
+WPM23.storyName = "23 WPM lesson"
+
+export const WPM28 = Template.bind({});
+WPM28.args = {
+  // data: {}
+};
+WPM28.storyName = "28 WPM lesson"
+
+export const WPM41 = Template.bind({});
+WPM41.args = {
+  // data: {}
+};
+WPM41.storyName = "41 WPM lesson"
+
+export const WPM61 = Template.bind({});
+WPM61.args = {
+  // data: {}
+};
+WPM61.storyName = "61 WPM lesson"
+
+export const WPM151 = Template.bind({});
+WPM151.args = {
+  // data: {}
+};
+WPM151.storyName = "151 WPM lesson"
+
+export const WPM226 = Template.bind({});
+WPM226.args = {
+  // data: {}
+};
+WPM226.storyName = "226 WPM lesson"
+
