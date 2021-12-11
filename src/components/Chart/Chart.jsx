d574c26ffc1ae0268b16356d17134d1a0c8dd1ce
Di <didoesdigital+git@gmail.com>
2021-12-11 13:33:02 +1000

    Add event listener rect to Chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,17 +1,33 @@
-import React, { createContext, useContext } from "react"
+import React, { createContext, useContext } from "react";
 
-const ChartContext = createContext()
+const ChartContext = createContext();
 
-export const useDimensionsContext = () => useContext(ChartContext)
+export const useDimensionsContext = () => useContext(ChartContext);
 
-const Chart = ({ dimensions, children }) => (
+const Chart = ({
+  dimensions,
+  onMouseMove = null,
+  onTouchMove = null,
+  children,
+}) => (
   <ChartContext.Provider value={dimensions}>
     <svg width={dimensions.width} height={dimensions.height}>
-      <g transform={`translate(${dimensions.marginLeft}, ${dimensions.marginTop})`}>
-        { children }
+      <g
+        transform={`translate(${dimensions.marginLeft}, ${dimensions.marginTop})`}
+      >
+        {children}
       </g>
+      <rect
+        x={0}
+        y={0}
+        width={dimensions.width}
+        height={dimensions.height}
+        fill="transparent"
+        onMouseMove={onMouseMove}
+        onTouchMove={onTouchMove}
+      />
     </svg>
   </ChartContext.Provider>
-)
+);
 
-export default Chart
+export default Chart;
