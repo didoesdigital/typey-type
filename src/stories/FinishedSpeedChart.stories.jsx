070741e6d5aea41985c1e9008407070864bdbf13
Di <didoesdigital+git@gmail.com>
2022-01-04 22:38:18 +1000

    Housekeeping: update story formatting

--- src/stories/FinishedSpeedChart.stories.jsx
+++ src/stories/FinishedSpeedChart.stories.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import FinishedSpeedChart from '../components/FinishedSpeedChart';
-import customTestPhrases from './fixtures/customTestPhrases.json';
-import shortAndSlow from './fixtures/shortAndSlow.json';
-import discoverInversion from './fixtures/discoverInversion.json';
-import discoverInversionTwice from './fixtures/discoverInversionTwice.json';
-import discoverIntroduction from './fixtures/discoverIntroduction.json';
-import practiceProverb from './fixtures/practiceProverb.json';
-import practiceProverbZ from './fixtures/practiceProverbZ.json';
+import React from "react";
+import FinishedSpeedChart from "../components/FinishedSpeedChart";
+import customTestPhrases from "./fixtures/customTestPhrases.json";
+import shortAndSlow from "./fixtures/shortAndSlow.json";
+import discoverInversion from "./fixtures/discoverInversion.json";
+import discoverInversionTwice from "./fixtures/discoverInversionTwice.json";
+import discoverIntroduction from "./fixtures/discoverIntroduction.json";
+import practiceProverb from "./fixtures/practiceProverb.json";
+import practiceProverbZ from "./fixtures/practiceProverbZ.json";
 
 /* NOTE:
  * To make more fixtures, add a console.log for the transformedData
@@ -15,23 +15,23 @@ import practiceProverbZ from './fixtures/practiceProverbZ.json';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  title: 'Speed chart',
+  title: "Speed chart",
   component: FinishedSpeedChart,
 };
 
 const generateData = (numberOfWords) => {
   let generatedData = {
     averageWPM: 120,
-    "dataPoints": []
-  }
+    dataPoints: [],
+  };
 
   let dataPoints = [];
 
   for (let i = 1; i < numberOfWords + 1; i++) {
-    let elapsedTime = (i * 1000) + (Math.random() * 1000); // [1200, 2900, 3100]
+    let elapsedTime = i * 1000 + Math.random() * 1000; // [1200, 2900, 3100]
     let wordsPerMinute = i / (elapsedTime / 1000 / 60);
     let typedText = i % 2 === 0 ? "five." : "chars";
-    let material =  i % 2 === 0 ? "five." : "chars";
+    let material = i % 2 === 0 ? "five." : "chars";
     let markedCorrect = true;
     let hint = "TPAOEUF/TP-PL";
     dataPoints.push({
@@ -42,25 +42,25 @@ const generateData = (numberOfWords) => {
       material: material,
       markedCorrect: markedCorrect,
       hint: hint,
-    })
+    });
   }
 
   generatedData.dataPoints = dataPoints;
 
   return generatedData;
-}
+};
 
-
-
-const Template = (args) => <div className="mt10 p3"><FinishedSpeedChart {...args} /></div>;
+const Template = (args) => (
+  <div className="mt10 p3">
+    <FinishedSpeedChart {...args} />
+  </div>
+);
 
 export const ShortLesson = Template.bind({});
 ShortLesson.args = {
   data: practiceProverb,
 };
-ShortLesson.storyName = "Short lesson"
-
-
+ShortLesson.storyName = "Short lesson";
 
 export const LongLesson = (args) => (
   <div className="mt10 p3">
@@ -70,45 +70,43 @@ export const LongLesson = (args) => (
 LongLesson.args = {
   numberOfWords: 1000,
 };
-LongLesson.storyName = "Long lesson"
-
-
+LongLesson.storyName = "Long lesson";
 
 export const TestPhrases = Template.bind({});
 TestPhrases.args = {
   data: customTestPhrases,
 };
-TestPhrases.storyName = "Test phrases"
+TestPhrases.storyName = "Test phrases";
 
 export const WPM2 = Template.bind({});
 WPM2.args = {
   data: shortAndSlow,
 };
-WPM2.storyName = "2 WPM lesson"
+WPM2.storyName = "2 WPM lesson";
 
 export const WPM3 = Template.bind({});
 WPM3.args = {
   data: discoverInversion,
 };
-WPM3.storyName = "3 WPM lesson"
+WPM3.storyName = "3 WPM lesson";
 
 export const WPM4 = Template.bind({});
 WPM4.args = {
   data: discoverInversionTwice,
 };
-WPM4.storyName = "4 WPM lesson"
+WPM4.storyName = "4 WPM lesson";
 
 export const WPM5 = Template.bind({});
 WPM5.args = {
   data: discoverIntroduction,
 };
-WPM5.storyName = "5 WPM lesson"
+WPM5.storyName = "5 WPM lesson";
 
 export const WPM21 = Template.bind({});
 WPM21.args = {
   data: practiceProverbZ,
 };
-WPM21.storyName = "21 WPM lesson"
+WPM21.storyName = "21 WPM lesson";
 
 export const WPM23 = Template.bind({});
 WPM23.args = {
@@ -132,7 +130,7 @@ export const WPM61 = Template.bind({});
 WPM61.args = {
   data: practiceProverb,
 };
-WPM61.storyName = "61 WPM lesson"
+WPM61.storyName = "61 WPM lesson";
 
 export const WPM151 = Template.bind({});
 WPM151.args = {
