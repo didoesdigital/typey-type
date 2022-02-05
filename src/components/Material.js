68802f1ed0f46bd55bfea932b45ab25bf9abe809
Di <didoesdigital+git@gmail.com>
2022-02-05 18:28:56 +1000

    Fix up spacing for upcoming phrases

--- src/components/Material.js
+++ src/components/Material.js
@@ -18,8 +18,8 @@ class Material extends Component {
     }
 
     const [matched, unmatched] = matchSplitText(this.props.currentPhrase, this.props.actualText, this.props.settings, this.props.userSettings);
-    const nextMaterial = this.props.upcomingPhrases.length > 0 ? this.props.upcomingPhrases[0] : '';
-    const nextUpcomingClasses = `de-emphasized upcoming-phrases bw-2 b--brand-primary-tint--60 ${nextMaterial.includes(' ') ? 'bb-dotted' : ''}`
+    const nextUpcomingMaterial = this.props.upcomingPhrases.length > 0 ? this.props.upcomingPhrases[0] : '';
+    const nextUpcomingClasses = `de-emphasized upcoming-phrases bw-2 b--brand-primary-tint--60 ${nextUpcomingMaterial.includes(' ') ? 'bb-dotted' : ''}`
     const restUpcomingMaterial = this.props.upcomingPhrases.length > 1 ? this.props.upcomingPhrases.slice(1).join(' ') : '';
 
     return (
@@ -27,7 +27,7 @@ class Material extends Component {
         <div className="expected">
           <div className="visually-hidden">Matching and unmatching material typed, upcoming words, and previous words:</div>
           <div className="material">
-            <pre className="material-pre relative"><div className={"dib current-and-upcoming-phrases" + blur}><strong className="current-phrase-material" tabIndex="0">{spaceAfterOutput}<span className="matched steno-material">{matched}</span><span className="steno-material">{unmatched}</span>{spaceBeforeOutput}</strong><span className={nextUpcomingClasses}>{nextMaterial}{spaceAfterOutput}</span><span className="de-emphasized upcoming-phrases">{spaceBeforeOutput}{restUpcomingMaterial}{spaceAfterOutput}</span></div><span className="dib de-emphasized completed-phrases">&#8203;{this.props.completedPhrases}</span></pre>
+            <pre className="material-pre relative"><div className={"dib current-and-upcoming-phrases" + blur}><strong className="current-phrase-material" tabIndex="0">{spaceAfterOutput}<span className="matched steno-material">{matched}</span><span className="steno-material">{unmatched}</span>{spaceBeforeOutput}</strong><span className={nextUpcomingClasses}>{nextUpcomingMaterial}</span> <span className="de-emphasized upcoming-phrases">{restUpcomingMaterial}{spaceAfterOutput}</span></div><span className="dib de-emphasized completed-phrases">&#8203;{this.props.completedPhrases}</span></pre>
           </div>
         </div>
       </div>
