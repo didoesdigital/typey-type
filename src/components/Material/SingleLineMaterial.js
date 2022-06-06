2ed568f445d1d3da2ea78df8d6451f27fb724e6f
Di <didoesdigital+git@gmail.com>
2022-06-06 10:14:43 +1000

    Convert to function component

--- src/components/Material/SingleLineMaterial.js
+++ src/components/Material/SingleLineMaterial.js
@@ -1,68 +1,67 @@
-import React, { Component } from "react";
+import React from "react";
 import { matchSplitText } from "./../../utils/typey-type";
 
-class SingleLineMaterial extends Component {
-  render() {
-    const spaceBeforeOutput =
-      this.props.userSettings.spacePlacement === "spaceAfterOutput" ? "" : " ";
-    const spaceAfterOutput =
-      this.props.userSettings.spacePlacement === "spaceBeforeOutput" ? "" : " ";
+export default function SingleLineMaterial({
+  actualText,
+  completedPhrases,
+  currentPhrase,
+  settings,
+  upcomingPhrases,
+  userSettings,
+}) {
+  const spaceBeforeOutput =
+    userSettings.spacePlacement === "spaceAfterOutput" ? "" : " ";
+  const spaceAfterOutput =
+    userSettings.spacePlacement === "spaceBeforeOutput" ? "" : " ";
 
-    const [matched, unmatched] = matchSplitText(
-      this.props.currentPhrase,
-      this.props.actualText,
-      this.props.settings,
-      this.props.userSettings
-    );
-    const completedPhrasesClasses =
-      "dib de-emphasized fw4 left-0 absolute text-right completed-phrases-transform";
-    const currentAndUpcomingPhrasesClasses = `dib current-and-upcoming-phrases${
-      this.props.userSettings.blurMaterial ? " blur-words" : ""
-    }`;
-    const nextUpcomingMaterial =
-      this.props.upcomingPhrases.length > 0
-        ? this.props.upcomingPhrases[0]
-        : "";
-    const nextUpcomingClasses = `de-emphasized fw4 text-left bw-2 b--brand-primary-tint--60 ${
-      nextUpcomingMaterial.includes(" ") ? "bb-dotted" : ""
-    }`;
-    const restUpcomingMaterial =
-      this.props.upcomingPhrases.length > 1
-        ? this.props.upcomingPhrases.slice(1).join(" ")
-        : "";
+  const [matched, unmatched] = matchSplitText(
+    currentPhrase,
+    actualText,
+    settings,
+    userSettings
+  );
+  const completedPhrasesClasses =
+    "dib de-emphasized fw4 left-0 absolute text-right completed-phrases-transform";
+  const currentAndUpcomingPhrasesClasses = `dib current-and-upcoming-phrases${
+    userSettings.blurMaterial ? " blur-words" : ""
+  }`;
+  const nextUpcomingMaterial =
+    upcomingPhrases.length > 0 ? upcomingPhrases[0] : "";
+  const nextUpcomingClasses = `de-emphasized fw4 text-left bw-2 b--brand-primary-tint--60 ${
+    nextUpcomingMaterial.includes(" ") ? "bb-dotted" : ""
+  }`;
+  const restUpcomingMaterial =
+    upcomingPhrases.length > 1 ? upcomingPhrases.slice(1).join(" ") : "";
 
-    return (
-      <div className="mb1 nt1">
-        <div className="expected">
-          <div className="visually-hidden">
-            Matching and unmatching material typed, upcoming words, and previous
-            words:
-          </div>
-          <div className="material mx-auto">
-            <pre className="relative translateX-10px">
-              <span className={completedPhrasesClasses}>
-                {this.props.completedPhrases.join(" ")}&#8203;
+  return (
+    <div className="mb1 nt1">
+      <div className="expected">
+        <div className="visually-hidden">
+          Matching and unmatching material typed, upcoming words, and previous
+          words:
+        </div>
+        <div className="material mx-auto">
+          <pre className="relative translateX-10px">
+            <span className={completedPhrasesClasses}>
+              {completedPhrases.join(" ")}&#8203;
+            </span>
+            <div className={currentAndUpcomingPhrasesClasses}>
+              <strong className="fw7" tabIndex="0">
+                <span className="matched steno-material">{matched}</span>
+                <span className="steno-material">{unmatched}</span>
+              </strong>
+              <span className={nextUpcomingClasses}>
+                &#8203;{spaceBeforeOutput}
+                {nextUpcomingMaterial}
+              </span>{" "}
+              <span className="de-emphasized fw4 text-left">
+                {restUpcomingMaterial}
+                {spaceAfterOutput}
               </span>
-              <div className={currentAndUpcomingPhrasesClasses}>
-                <strong className="fw7" tabIndex="0">
-                  <span className="matched steno-material">{matched}</span>
-                  <span className="steno-material">{unmatched}</span>
-                </strong>
-                <span className={nextUpcomingClasses}>
-                  &#8203;{spaceBeforeOutput}
-                  {nextUpcomingMaterial}
-                </span>{" "}
-                <span className="de-emphasized fw4 text-left">
-                  {restUpcomingMaterial}
-                  {spaceAfterOutput}
-                </span>
-              </div>
-            </pre>
-          </div>
+            </div>
+          </pre>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default SingleLineMaterial;
