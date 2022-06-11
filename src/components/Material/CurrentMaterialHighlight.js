4dc9fb614ab6e249a2236b2b6842d2b2c2ae87b1
Di <didoesdigital+git@gmail.com>
2022-06-12 08:22:39 +1000

    Add accessible labels to current word feedback

--- src/components/Material/CurrentMaterialHighlight.js
+++ src/components/Material/CurrentMaterialHighlight.js
@@ -86,9 +86,9 @@ export default function CurrentMaterialHighlight({
         zIndex: 1,
       }}
     >
-      <strong className="fw7 pre" tabIndex="0">
-        <span className="matched steno-material">{matched}</span>
-        <span className="steno-material">{unmatched}</span>
+      <strong className="fw7 pre" tabIndex="0" aria-label="Current word feedback">
+        <span className="matched steno-material" aria-label="Matched letters">{matched}</span>
+        <span className="steno-material" aria-label="Unmatched letters">{unmatched}</span>
       </strong>
     </div>
   );
