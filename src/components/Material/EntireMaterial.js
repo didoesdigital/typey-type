d2718d4fba069ad0c3513dc8fd20a919dd8b9e6f
Di <didoesdigital+git@gmail.com>
2022-06-12 10:57:39 +1000

    Replace invisible chars with unicode with comment

--- src/components/Material/EntireMaterial.js
+++ src/components/Material/EntireMaterial.js
@@ -7,8 +7,11 @@ export default React.memo(function EntireMaterial({
   const isSpaceBefore = spacePlacement === "spaceBeforeOutput";
   const isSpaceAfter = spacePlacement === "spaceAfterOutput";
 
-  const spaceBeforeOutput = isSpaceBefore ? "​ " : "";
-  const spaceAfterOutput = isSpaceAfter ? " ​­​" : "";
+  // Note: the unicode 200B zero-width space lets the text break,
+  // the unicode 00AD soft hyphen makes sure Safari lets the text break,
+  // and wrapping the soft hyphen in zero-width spaces means there's no reason to render a hyphen
+  const spaceBeforeOutput = isSpaceBefore ? "\u200B " : "";
+  const spaceAfterOutput = isSpaceAfter ? " \u200B\u00AD\u200B" : "";
   const useSeparator =
     spacePlacement === "spaceOff" || spacePlacement === "spaceExact";
 
