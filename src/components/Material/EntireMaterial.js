00dcf9a9ab21b51c93550f2bd26c124ce0c189cf
Di <didoesdigital+git@gmail.com>
2022-06-12 11:34:40 +1000

    Fix current phrase highlight space after in Safari

--- src/components/Material/EntireMaterial.js
+++ src/components/Material/EntireMaterial.js
@@ -9,9 +9,9 @@ export default React.memo(function EntireMaterial({
 
   // Note: the unicode 200B zero-width space lets the text break,
   // the unicode 00AD soft hyphen makes sure Safari lets the text break,
-  // and wrapping the soft hyphen in zero-width spaces means there's no reason to render a hyphen
+  // and pairing the soft hyphen with a zero-width space means there's no reason to render a hyphen
   const spaceBeforeOutput = isSpaceBefore ? "\u200B " : "";
-  const spaceAfterOutput = isSpaceAfter ? " \u200B\u00AD\u200B" : "";
+  const spaceAfterOutput = isSpaceAfter ? " \u200B\u00AD" : "";
   const useSeparator =
     spacePlacement === "spaceOff" || spacePlacement === "spaceExact";
 
