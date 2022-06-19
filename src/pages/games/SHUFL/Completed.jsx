911e3b4371f20336ecf54608b7b7054c51e270a5
Di <didoesdigital+git@gmail.com>
2022-06-19 18:31:21 +1000

    Style Completed component

--- src/pages/games/SHUFL/Completed.jsx
+++ src/pages/games/SHUFL/Completed.jsx
@@ -5,15 +5,18 @@ import { SHUFLDispatch } from "./SHUFLGame";
 export default function SHUFLPuzzle() {
   const dispatch = useContext(SHUFLDispatch);
   return (
-    <p>
-      You win!{" "}
-      <button
-        onClick={() => {
-          dispatch({ type: actions.restartGame });
-        }}
-      >
-        Restart
-      </button>
-    </p>
+    <>
+      <p className="text-center">You win! </p>
+      <p className="mx-auto text-center">
+        <button
+          className="button"
+          onClick={() => {
+            dispatch({ type: actions.restartGame });
+          }}
+        >
+          Restart
+        </button>
+      </p>
+    </>
   );
 }
