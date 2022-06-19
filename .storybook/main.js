cca6435cf62257fe9f56e15d64af46188edc430e
Di <didoesdigital+git@gmail.com>
2022-06-19 17:48:15 +1000

    Use SB interactions addon and format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react"
-}
+  features: {
+    interactionsDebugger: true,
+  },
+  framework: "@storybook/react",
+};
+
