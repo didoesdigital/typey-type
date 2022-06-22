2413a5524ae10127c483b0b0f333b1781d3d8e0b
Di <didoesdigital+git@gmail.com>
2022-06-22 20:10:06 +1000

    Add warning emoji to lesson length preview

--- src/components/LessonLengthPreview.jsx
+++ src/components/LessonLengthPreview.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 import { humanDurationFormatter } from "../utils/formatters";
 
+const Warning = ({ duration }) => {
+  if (duration >= 60) {
+    return (
+      <span role="img" aria-label="Danger: ">
+        {" "}
+        🚨🚨🚨{" "}
+      </span>
+    );
+  } else if (duration >= 45) {
+    return (
+      <span role="img" aria-label="Danger: ">
+        {" "}
+        🚨{" "}
+      </span>
+    );
+  } else if (duration >= 30) {
+    return (
+      <span role="img" aria-label="Warning: ">
+        {" "}
+        ⚠️{" "}
+      </span>
+    );
+  } else {
+    return null;
+  }
+};
+
 const LessonLengthPreview = ({ totalWords, speed, lessonStarted }) => (
   <div
     className={`text-center absolute left-0 right-0 b6 transition-opacity ${
@@ -8,8 +35,10 @@ const LessonLengthPreview = ({ totalWords, speed, lessonStarted }) => (
     }`}
   >
     <p className="absolute left-0 right-0 de-emphasized text-shadow-outline">
+      <Warning duration={totalWords / speed} />
       {totalWords} word{totalWords === 1 ? "" : "s"} at {speed}&nbsp;WPM:{" "}
       {humanDurationFormatter(totalWords / speed)}
+      <Warning duration={totalWords / speed} />
     </p>
   </div>
 );
