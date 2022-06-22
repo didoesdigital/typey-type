e5ea92fd3b8d5c8469cc7ab117c5a1a9016fdb76
Di <didoesdigital+git@gmail.com>
2022-06-22 14:59:26 +1000

    Rename TPEUBGSZPuzzle export

--- src/pages/games/TPEUBGSZ/Puzzle.jsx
+++ src/pages/games/TPEUBGSZ/Puzzle.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function TPEUBGSZPuzzle({ puzzleText }) {
+export default function Puzzle({ puzzleText }) {
   return (
     <p className="text-center">
       <strong>{puzzleText}</strong>
