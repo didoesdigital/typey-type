8c91a9757aa3bf5ee72c07bee72f4d308f3dfe7b
Di <didoesdigital+git@gmail.com>
2022-06-22 20:11:39 +1000

    Refactor lesson length warning emoji

--- src/components/LessonLengthPreview.jsx
+++ src/components/LessonLengthPreview.jsx
@@ -1,46 +1,39 @@
 import React from "react";
 import { humanDurationFormatter } from "../utils/formatters";
 
-const Warning = ({ duration }) => {
-  if (duration >= 60) {
-    return (
-      <span role="img" aria-label="Danger: ">
-        {" "}
-        🚨🚨🚨{" "}
-      </span>
-    );
-  } else if (duration >= 45) {
-    return (
-      <span role="img" aria-label="Danger: ">
-        {" "}
-        🚨{" "}
-      </span>
-    );
-  } else if (duration >= 30) {
-    return (
-      <span role="img" aria-label="Warning: ">
-        {" "}
-        ⚠️{" "}
-      </span>
-    );
-  } else {
-    return null;
-  }
-};
-
-const LessonLengthPreview = ({ totalWords, speed, lessonStarted }) => (
-  <div
-    className={`text-center absolute left-0 right-0 b6 transition-opacity ${
-      lessonStarted ? "o-0" : "o-100"
-    }`}
-  >
-    <p className="absolute left-0 right-0 de-emphasized text-shadow-outline">
-      <Warning duration={totalWords / speed} />
-      {totalWords} word{totalWords === 1 ? "" : "s"} at {speed}&nbsp;WPM:{" "}
-      {humanDurationFormatter(totalWords / speed)}
-      <Warning duration={totalWords / speed} />
-    </p>
-  </div>
+const Warning = ({ duration }) => (
+  <span role="img" aria-label=" Warning ">
+    {" "}
+    {duration >= 60
+      ? "🚨🚨🚨"
+      : duration >= 45
+      ? "🚨"
+      : duration >= 30
+      ? "⚠️"
+      : ""}{" "}
+  </span>
 );
+
+const LessonLengthPreview = ({ totalWords, speed, lessonStarted }) => {
+  const duration = totalWords / speed;
+  return (
+    <div
+      className={`text-center absolute left-0 right-0 b6 transition-opacity ${
+        lessonStarted ? "o-0" : "o-100"
+      }`}
+    >
+      <p
+        className={`absolute left-0 right-0 text-shadow-outline${
+          duration > 30 ? "" : " de-emphasized"
+        }`}
+      >
+        <Warning duration={duration} />
+        {totalWords} word{totalWords === 1 ? "" : "s"} at {speed}&nbsp;WPM:{" "}
+        {humanDurationFormatter(duration)}
+        <Warning duration={duration} />
+      </p>
+    </div>
+  );
+};
 
 export default LessonLengthPreview;
