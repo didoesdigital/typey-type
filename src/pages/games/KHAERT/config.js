f2b57c84f6b660d7585dab1c8fd6999d3c8c416f
Di <didoesdigital+git@gmail.com>
2022-06-25 17:42:20 +1000

    Configure Shazza

--- src/pages/games/KHAERT/config.js
+++ src/pages/games/KHAERT/config.js
@@ -1,7 +1,14 @@
 import { createChatBotMessage } from "react-chatbot-kit";
 
+const botName = "Shazza";
+
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)],
+  initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
+  botName: botName,
+  customStyles: {
+    botMessageBox: { backgroundColor: "#1F74D1" },
+    chatButton: { backgroundColor: "#067551" },
+  },
 };
 
 export default config;
