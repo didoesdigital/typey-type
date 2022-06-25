93523a9a9f0cd83f7a112357f3896727f6609d35
Di <didoesdigital+git@gmail.com>
2022-06-25 18:44:49 +1000

    Add robot image as chat bot avatar

--- src/pages/games/KHAERT/config.js
+++ src/pages/games/KHAERT/config.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createChatBotMessage } from "react-chatbot-kit";
 import DogPicture from "./DogPicture.jsx";
+import { ReactComponent as ThinkingRobot } from "../../../images/ThinkingRobot.svg";
 
 export const botName = "Shazza";
 
@@ -10,6 +11,17 @@ const config = {
   customStyles: {
     botMessageBox: { backgroundColor: "#1F74D1" },
     chatButton: { backgroundColor: "#067551" },
+  },
+  customComponents: {
+    botAvatar: (props) => (
+      <div className="mw-48">
+        <ThinkingRobot
+          role="img"
+          aria-labelledby="thinking-robot-title"
+          {...props}
+        />
+      </div>
+    ),
   },
   widgets: [
     {
