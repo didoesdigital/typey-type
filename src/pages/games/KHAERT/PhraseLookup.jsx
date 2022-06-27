2a7bf79f4c7239eb5f97fa221d9069a404aade94
Di <didoesdigital+git@gmail.com>
2022-06-27 16:02:18 +1000

    Fix brief lookups changing chat history
    https://github.com/FredrikOseberg/react-chatbot-kit/issues/104#issuecomment-1140404040

--- src/pages/games/KHAERT/PhraseLookup.jsx
+++ src/pages/games/KHAERT/PhraseLookup.jsx
@@ -2,16 +2,19 @@ import React, { useEffect, useState } from "react";
 import { createStrokeHintForPhrase } from "../../../utils/transformingDictionaries";
 
 const PhraseLookup = ({ phraseToLookup, globalLookupDictionary }) => {
-  const [brief, setBrief] = useState("");
+  const [strokeHint, setStrokeHint] = useState("");
   useEffect(() => {
-    const newBrief = createStrokeHintForPhrase(
+    const newStrokeHint = createStrokeHintForPhrase(
       phraseToLookup,
       globalLookupDictionary
     );
-    setBrief(newBrief);
-  }, [phraseToLookup, globalLookupDictionary]);
+    setStrokeHint(newStrokeHint);
+    // By ignoring prop changes in the dependency array, we ensure each new phrase lookup does not
+    // re-render and update previous phrase lookups, so we can preserve genuine chat history.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <p>{brief}</p>;
+  return <p>{strokeHint}</p>;
 };
 
 export default PhraseLookup;
