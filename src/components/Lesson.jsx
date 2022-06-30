a7b9b57ca5d2019ae72af71bbc5a1e9d9e7e0d04
Di <didoesdigital+git@gmail.com>
2022-06-30 21:44:50 +1000

    Pass prop to isFlashcards

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -55,7 +55,7 @@ class Lesson extends Component {
       else if(isOverview(this.props.location.pathname)) {
         // do nothing
       }
-      else if(this.isFlashcards()) {
+      else if(this.isFlashcards(this.props.location.pathname)) {
         // do nothing
       }
       else if((this.props.lesson.path!==this.props.location.pathname+'lesson.txt') && (this.props.location.pathname.startsWith('/lessons'))) {
@@ -87,7 +87,7 @@ class Lesson extends Component {
       this.props.startCustomLesson();
     } else if(isOverview(this.props.location.pathname)) {
       // do nothing
-    } else if (this.isFlashcards()) {
+    } else if (this.isFlashcards(this.props.location.pathname)) {
       // do nothing
     } else if((prevProps.match.url!==this.props.match.url) && (this.props.location.pathname.startsWith('/lessons'))) {
       this.props.handleLesson(process.env.PUBLIC_URL + this.props.location.pathname+'lesson.txt');
@@ -104,8 +104,8 @@ class Lesson extends Component {
     this.props.stopLesson()
   }
 
-  isFlashcards() {
-    return (this.props.location.pathname.startsWith('/lessons/') && this.props.location.pathname.endsWith('/flashcards'));
+  isFlashcards(pathname) {
+    return (pathname.startsWith('/lessons/') && pathname.endsWith('/flashcards'));
   }
 
   isFinished() {
@@ -190,7 +190,7 @@ class Lesson extends Component {
     }
 
     if (this.props.lesson) {
-      if (this.isFinished() && !isOverview(this.props.location.pathname) && !this.isFlashcards()) {
+      if (this.isFinished() && !isOverview(this.props.location.pathname) && !this.isFlashcards(this.props.location.pathname)) {
         return (
           <DocumentTitle title={'Typey Type | Lesson: ' + this.props.lesson.title}>
             <main id="main">
