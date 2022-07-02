13af4662b6bba68e6fc04d8d70ff66a2a508f684
Di <didoesdigital+git@gmail.com>
2022-07-02 12:55:22 +1000

    Format Finished.js

--- src/components/Finished.js
+++ src/components/Finished.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import LessonCanvasFooter from '../pages/lessons/LessonCanvasFooter';
-import FinishedZeroAndEmptyStateMessage from '../pages/lessons/FinishedZeroAndEmptyState';
-import UserSettings from './UserSettings';
-import { stitchTogetherLessonData, transformLessonDataToChartData } from '../utils/transformingFinishedData';
-import FinishedActionButtons from '../pages/lessons/FinishedActionButtons';
-import FinishedDataViz from '../pages/lessons/FinishedDataViz';
-import FinishedMisstrokesSummary from '../pages/lessons/FinishedMisstrokesSummary';
-import FinishedSummaryHeadings from '../pages/lessons/FinishedSummaryHeadings';
-import getNumericAccuracy from '../pages/lessons/getNumericAccuracy';
-import 'react-tippy/dist/tippy.css';
+import React, { useEffect, useState } from "react";
+import LessonCanvasFooter from "../pages/lessons/LessonCanvasFooter";
+import FinishedZeroAndEmptyStateMessage from "../pages/lessons/FinishedZeroAndEmptyState";
+import UserSettings from "./UserSettings";
+import {
+  stitchTogetherLessonData,
+  transformLessonDataToChartData,
+} from "../utils/transformingFinishedData";
+import FinishedActionButtons from "../pages/lessons/FinishedActionButtons";
+import FinishedDataViz from "../pages/lessons/FinishedDataViz";
+import FinishedMisstrokesSummary from "../pages/lessons/FinishedMisstrokesSummary";
+import FinishedSummaryHeadings from "../pages/lessons/FinishedSummaryHeadings";
+import getNumericAccuracy from "../pages/lessons/getNumericAccuracy";
+import "react-tippy/dist/tippy.css";
 
 // fullURL = "https://docs.google.com/forms/d/e/1FAIpQLSda64Wi5L-eVzZVo6HLJ2xnD9cu83H2-2af3WEE2atFiaoKyw/viewform?usp=pp_url&entry.1884511690=lesson&entry.1202724812&entry.936119214";
-const googleFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSda64Wi5L-eVzZVo6HLJ2xnD9cu83H2-2af3WEE2atFiaoKyw/viewform?usp=pp_url&entry.1884511690="
+const googleFormURL =
+  "https://docs.google.com/forms/d/e/1FAIpQLSda64Wi5L-eVzZVo6HLJ2xnD9cu83H2-2af3WEE2atFiaoKyw/viewform?usp=pp_url&entry.1884511690=";
 const googleFormParam = "&entry.1202724812&entry.936119214";
 
 const calculateScores = (duration, wordCount) =>
@@ -76,18 +80,34 @@ const Finished = ({
   useEffect(() => {
     const adjustedWPM = calculateScores(timer, totalNumberOfMatchedWords);
     setWpm(adjustedWPM);
-  }, [timer, totalNumberOfMatchedWords])
+  }, [timer, totalNumberOfMatchedWords]);
 
   // update chart in FinishedDataViz
   useEffect(() => {
-    const lessonData = stitchTogetherLessonData(currentLessonStrokes, startTime, wpm);
+    const lessonData = stitchTogetherLessonData(
+      currentLessonStrokes,
+      startTime,
+      wpm
+    );
     setChartData(transformLessonDataToChartData(lessonData));
-  }, [currentLessonStrokes, startTime, wpm])
+  }, [currentLessonStrokes, startTime, wpm]);
 
   // update hero data in FinishedDataViz
   useEffect(() => {
-    setNumericAccuracy(getNumericAccuracy(totalNumberOfMistypedWords, totalNumberOfHintedWords, currentLessonStrokes, wpm));
-  }, [currentLessonStrokes, totalNumberOfHintedWords, totalNumberOfMistypedWords, wpm])
+    setNumericAccuracy(
+      getNumericAccuracy(
+        totalNumberOfMistypedWords,
+        totalNumberOfHintedWords,
+        currentLessonStrokes,
+        wpm
+      )
+    );
+  }, [
+    currentLessonStrokes,
+    totalNumberOfHintedWords,
+    totalNumberOfMistypedWords,
+    wpm,
+  ]);
 
   // update top speed today or ever and headings and confetti
   useEffect(() => {
@@ -95,22 +115,32 @@ const Finished = ({
     const fasterPersonalBest = wpm > topSpeedPersonalBest;
     const minimumStrokes = currentLessonStrokes.length > 3;
     const minimumSpeed = wpm > 3;
-    const thirtyStrokesOrNotRevision = (!revisionMode || currentLessonStrokes.length >= 30);
+    const thirtyStrokesOrNotRevision =
+      !revisionMode || currentLessonStrokes.length >= 30;
 
-    if (fasterSpeedToday && minimumStrokes && minimumSpeed && thirtyStrokesOrNotRevision && fasterPersonalBest) {
-      setConfettiConfig({sparsity: 17, colors: 5});
+    if (
+      fasterSpeedToday &&
+      minimumStrokes &&
+      minimumSpeed &&
+      thirtyStrokesOrNotRevision &&
+      fasterPersonalBest
+    ) {
+      setConfettiConfig({ sparsity: 17, colors: 5 });
       topSpeedToday = wpm;
       updateTopSpeedPersonalBest(wpm);
       setNewTopSpeedPersonalBest(true);
       setNewTopSpeedToday(true);
-    }
-    else if (fasterSpeedToday && minimumStrokes && minimumSpeed && thirtyStrokesOrNotRevision) {
-      setConfettiConfig({sparsity: 170, colors: 2});
+    } else if (
+      fasterSpeedToday &&
+      minimumStrokes &&
+      minimumSpeed &&
+      thirtyStrokesOrNotRevision
+    ) {
+      setConfettiConfig({ sparsity: 170, colors: 2 });
       topSpeedToday = wpm;
       setNewTopSpeedPersonalBest(false);
       setNewTopSpeedToday(true);
-    }
-    else {
+    } else {
       setNewTopSpeedPersonalBest(false);
       setNewTopSpeedToday(false);
     }
@@ -118,20 +148,29 @@ const Finished = ({
     // FIXME: updating the newTopSpeedToday too frequently causes the heading text to revert to
     // lesson title instead of "New top speed for today!"
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentLessonStrokes.length, revisionMode, wpm])
+  }, [currentLessonStrokes.length, revisionMode, wpm]);
 
   return (
     <div>
       <div id="lesson-page" className="flex-wrap-md flex mx-auto mw-1920">
-        <div id="main-lesson-area" className="flex-grow mx-auto mw-1440 min-w-0">
+        <div
+          id="main-lesson-area"
+          className="flex-grow mx-auto mw-1440 min-w-0"
+        >
           <div className="mx-auto mw-1920">
-            {settings?.customMessage && <h3 className='px3 pb0 mb0'>{settings.customMessage}</h3>}
+            {settings?.customMessage && (
+              <h3 className="px3 pb0 mb0">{settings.customMessage}</h3>
+            )}
           </div>
           <div className="mx-auto mw-1920 p3">
             <div className="lesson-canvas lesson-canvas--finished panel p3 mb3">
-              {(lessonLength === 0) ?
-                <FinishedZeroAndEmptyStateMessage startFromWordSetting={userSettings.startFromWord} startFromWordOneClickHandler={startFromWordOne} suggestedNextUrl={suggestedNext} />
-                :
+              {lessonLength === 0 ? (
+                <FinishedZeroAndEmptyStateMessage
+                  startFromWordSetting={userSettings.startFromWord}
+                  startFromWordOneClickHandler={startFromWordOne}
+                  suggestedNextUrl={suggestedNext}
+                />
+              ) : (
                 <div className="w-100">
                   <div className="finished-lesson mx-auto mw-1440">
                     <div className="finished-summary mb3 text-center">
@@ -147,7 +186,9 @@ const Finished = ({
                         numericAccuracy={numericAccuracy}
                         chartData={chartData}
                         totalNumberOfNewWordsMet={totalNumberOfNewWordsMet}
-                        totalNumberOfLowExposuresSeen={totalNumberOfLowExposuresSeen}
+                        totalNumberOfLowExposuresSeen={
+                          totalNumberOfLowExposuresSeen
+                        }
                         totalNumberOfRetainedWords={totalNumberOfRetainedWords}
                         totalNumberOfHintedWords={totalNumberOfHintedWords}
                         totalNumberOfMistypedWords={totalNumberOfMistypedWords}
@@ -172,7 +213,7 @@ const Finished = ({
                     />
                   </div>
                 </div>
-              }
+              )}
             </div>
             <LessonCanvasFooter
               chooseStudy={chooseStudy}
@@ -185,7 +226,21 @@ const Finished = ({
               userSettings={userSettings}
             />
           </div>
-          <p className="text-center"><a href={googleFormURL + encodeURIComponent(location?.pathname || '') + googleFormParam} className="text-small mt0" target="_blank" rel="noopener noreferrer" id="ga--lesson--give-feedback">Give feedback on this lesson (form opens in a new tab)</a></p>
+          <p className="text-center">
+            <a
+              href={
+                googleFormURL +
+                encodeURIComponent(location?.pathname || "") +
+                googleFormParam
+              }
+              className="text-small mt0"
+              target="_blank"
+              rel="noopener noreferrer"
+              id="ga--lesson--give-feedback"
+            >
+              Give feedback on this lesson (form opens in a new tab)
+            </a>
+          </p>
         </div>
         <div>
           <UserSettings
@@ -215,6 +270,6 @@ const Finished = ({
       </div>
     </div>
   );
-}
+};
 
 export default Finished;
