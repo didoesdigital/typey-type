bac52d5eb7901526bb33bf090209f6e856c1250a
Di <didoesdigital+git@gmail.com>
2022-07-03 19:50:26 +1000

    Add LessonSubheader.tsx

---
+++ src/pages/lessons/LessonSubheader.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+type LessonSubheaderProps = {
+  createNewCustomLesson: any;
+  handleStopLesson: () => void;
+  lessonSubTitle: any;
+  lessonTitle: any;
+  overviewLink: any;
+  path: string;
+  restartLesson: () => void;
+};
+
+const LessonSubheader = React.forwardRef(
+  (
+    {
+      createNewCustomLesson,
+      handleStopLesson,
+      lessonSubTitle,
+      lessonTitle,
+      overviewLink,
+      path,
+      restartLesson,
+    }: LessonSubheaderProps,
+    ref: any
+  ) => {
+    return (
+      <div className="subheader">
+        <div className="flex flex-wrap items-baseline mx-auto mw-1920 justify-between px3 py2">
+          <div className="flex mr1 self-center">
+            <header className="flex items-center min-h-40">
+              <a
+                href={path}
+                onClick={restartLesson}
+                className="heading-link table-cell mr2"
+                role="button"
+              >
+                <h2
+                  ref={ref}
+                  tabIndex={-1}
+                >{`${lessonTitle}${lessonSubTitle}`}</h2>
+              </a>
+            </header>
+          </div>
+          <div className="flex flex-wrap mxn2">
+            {createNewCustomLesson ? createNewCustomLesson : overviewLink}
+            {!path.includes("custom") && !path.includes("progress") ? (
+              <Link
+                to={path
+                  .replace("lesson.txt", "flashcards")
+                  .replace("/typey-type", "")}
+                className="link-button link-button-ghost table-cell mr1"
+              >
+                Flashcards
+              </Link>
+            ) : null}
+            <a
+              href={path.replace(/lesson\.txt$/, "")}
+              onClick={restartLesson}
+              className="button button--secondary table-cell mr2"
+              style={{ lineHeight: 2 }}
+              role="button"
+            >
+              Restart
+            </a>
+            <a
+              href={path}
+              onClick={handleStopLesson}
+              className="button button--secondary table-cell mr2"
+              style={{ lineHeight: 2 }}
+              role="button"
+            >
+              Stop
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default LessonSubheader;
