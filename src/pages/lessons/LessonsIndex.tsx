ac35632bf81f2bfe65184b1ebe30318c43d88eef
Di <didoesdigital+git@gmail.com>
2022-07-09 15:44:18 +1000

    Move custom lesson buttons and community blurb
    - Move custom lesson buttons to header
    - Move community lessons blurb to bottom of the page

--- src/pages/lessons/LessonsIndex.tsx
+++ src/pages/lessons/LessonsIndex.tsx
@@ -37,27 +37,53 @@ const LessonsIndex = ({
               </h2>
             </header>
           </div>
+          <div className="flex mxn2">
+            {customLesson.title !== "Steno" ? (
+              <Link
+                to={`${match.url}/custom?study=discover&newWords=1&seenWords=1&retainedWords=1&sortOrder=sortOff&startFromWord=1`.replace(
+                  /\/{2,}/g,
+                  "/"
+                )}
+                onClick={stopLesson}
+                className="dib ml2 link-button link-button-ghost table-cell mr1"
+              >
+                Start custom lesson
+              </Link>
+            ) : null}
+            <Link
+              to={`${match.url}/custom/setup`.replace(/\/{2,}/g, "/")}
+              className="link-button dib button button--secondary table-cell mr2"
+              style={{ lineHeight: 2 }}
+              id="ga--lesson-index--create-a-custom-lesson"
+            >
+              Create a custom lesson
+            </Link>
+          </div>
         </div>
       </div>
       <div className="p3 mx-auto mw-1024">
-        <div className="flex flex-wrap justify-between">
-          <div className="mw-584 flex-grow">
-            <h3>Typey&nbsp;Type lessons</h3>
-            <LessonList lessonIndex={lessonIndex} url={match.url} />
-          </div>
-          <div className="mt1 mw-336 flex-grow">
-            <h3 className="mt3">Custom lessons</h3>
-            <p>
-              To help Typey&nbsp;Type grow even faster, add your custom lessons
-              to the{" "}
-              <GoogleAnalytics.OutboundLink
-                eventLabel="community's lessons"
-                to="https://docs.google.com/spreadsheets/d/1AlO2SSUwuv3yrz7RI9ix_z1Efbiu_j50c_ibGYwdsgc/edit?usp=sharing"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                community’s lessons
-                {/* @ts-ignore theme and trigger wrong: https://github.com/tvkhoa/react-tippy/blob/c6e6169e3f2cabe05f1bfbd7e0dea1ddef4debe8/index.d.ts */}
+        <h3>Typey&nbsp;Type lessons</h3>
+        <LessonList lessonIndex={lessonIndex} url={match.url} />
+        <div className="mw-584">
+          <h3 className="mt3">Community lessons</h3>
+          <p>
+            The Typey&nbsp;Type community lessons cover topics like spacing,
+            capitalisation, quotations, and using{" "}
+            <span className="steno-stroke steno-stroke--subtle px05">
+              SPWER
+            </span>{" "}
+            for “inter-” and “enter-” words. To help Typey&nbsp;Type grow even
+            faster, add your custom lessons to the{" "}
+            <GoogleAnalytics.OutboundLink
+              eventLabel="community's lessons"
+              to="https://docs.google.com/spreadsheets/d/1AlO2SSUwuv3yrz7RI9ix_z1Efbiu_j50c_ibGYwdsgc/edit?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              community’s{" "}
+              <span className="nowrap">
+                lessons
+                {/* @ts-ignore */}
                 <Tooltip
                   title="Opens in a new tab"
                   animation="shift"
@@ -79,45 +105,14 @@ const LessonsIndex = ({
                     iconTitle=""
                   />
                 </Tooltip>
-              </GoogleAnalytics.OutboundLink>
-              .
-            </p>
-            <p>
-              The community lessons include extra topics like spacing,
-              capitalisation, quotations, and using{" "}
-              <span className="steno-stroke steno-stroke--subtle px05">
-                SPWER
-              </span>{" "}
-              for “inter-”/“enter-” words.
-            </p>
-            <p>
-              <Link
-                to={`${match.url}/custom/setup`.replace(/\/{2,}/g, "/")}
-                className="link-button dib"
-                style={{ lineHeight: 2 }}
-                id="ga--lesson-index--create-a-custom-lesson"
-              >
-                Create a custom lesson
-              </Link>
-              {customLesson.title !== "Steno" ? (
-                <Link
-                  to={`${match.url}/custom?study=discover&newWords=1&seenWords=1&retainedWords=1&sortOrder=sortOff&startFromWord=1`.replace(
-                    /\/{2,}/g,
-                    "/"
-                  )}
-                  onClick={stopLesson}
-                  className="dib ml2"
-                >
-                  Start custom lesson
-                </Link>
-              ) : null}
-            </p>
-          </div>
+              </span>
+            </GoogleAnalytics.OutboundLink>
+            .
+          </p>
         </div>
       </div>
     </main>
   );
 };
-// There are also <Link to="/support#palantype">palantype</Link> lessons available.
 
 export default LessonsIndex;
