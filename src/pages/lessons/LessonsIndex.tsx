c0899fec1ce765a4348547cb6136ff192f9797f4
Di <didoesdigital+git@gmail.com>
2022-07-09 13:00:48 +1000

    Re-arrange lesson index page

--- src/pages/lessons/LessonsIndex.tsx
+++ src/pages/lessons/LessonsIndex.tsx
@@ -41,78 +41,77 @@ const LessonsIndex = ({
       </div>
       <div className="p3 mx-auto mw-1024">
         <div className="flex flex-wrap justify-between">
-          <div>
-            <div className="mw-584">
-              <h3>Custom lessons</h3>
-              <p>
-                To help Typey&nbsp;Type grow even faster, add your custom
-                lessons to the{" "}
-                <GoogleAnalytics.OutboundLink
-                  eventLabel="community's lessons"
-                  to="https://docs.google.com/spreadsheets/d/1AlO2SSUwuv3yrz7RI9ix_z1Efbiu_j50c_ibGYwdsgc/edit?usp=sharing"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  community’s lessons
-                  {/* @ts-ignore theme and trigger wrong: https://github.com/tvkhoa/react-tippy/blob/c6e6169e3f2cabe05f1bfbd7e0dea1ddef4debe8/index.d.ts */}
-                  <Tooltip
-                    title="Opens in a new tab"
-                    animation="shift"
-                    arrow="true"
-                    className=""
-                    duration="200"
-                    tabIndex="0"
-                    tag="span"
-                    theme="didoesdigital"
-                    trigger="mouseenter focus click"
-                    onShow={setAnnouncementMessage}
-                  >
-                    <IconExternal
-                      ariaHidden="true"
-                      role="presentation"
-                      iconWidth="24"
-                      iconHeight="24"
-                      className="ml1 svg-icon-wrapper svg-baseline"
-                      iconTitle=""
-                    />
-                  </Tooltip>
-                </GoogleAnalytics.OutboundLink>
-                .
-              </p>
-              <p>
-                The community lessons include extra topics like spacing,
-                capitalisation, quotations, and using{" "}
-                <span className="steno-stroke steno-stroke--subtle px05">
-                  SPWER
-                </span>{" "}
-                for “inter-”/“enter-” words.
-              </p>
-              <p>
-                <Link
-                  to={`${match.url}/custom/setup`.replace(/\/{2,}/g, "/")}
-                  className="link-button dib"
-                  style={{ lineHeight: 2 }}
-                  id="ga--lesson-index--create-a-custom-lesson"
-                >
-                  Create a custom lesson
-                </Link>
-                {customLesson.title !== "Steno" ? (
-                  <Link
-                    to={`${match.url}/custom?study=discover&newWords=1&seenWords=1&retainedWords=1&sortOrder=sortOff&startFromWord=1`.replace(
-                      /\/{2,}/g,
-                      "/"
-                    )}
-                    onClick={stopLesson}
-                    className="dib ml2"
-                  >
-                    Start custom lesson
-                  </Link>
-                ) : null}
-              </p>
-            </div>
-
+          <div className="mw-584">
             <h3>Typey&nbsp;Type lessons</h3>
             <LessonList lessonIndex={lessonIndex} url={match.url} />
+          </div>
+          <div className="mt1 mw-336 flex-grow">
+            <h3 className="mt3">Custom lessons</h3>
+            <p>
+              To help Typey&nbsp;Type grow even faster, add your custom lessons
+              to the{" "}
+              <GoogleAnalytics.OutboundLink
+                eventLabel="community's lessons"
+                to="https://docs.google.com/spreadsheets/d/1AlO2SSUwuv3yrz7RI9ix_z1Efbiu_j50c_ibGYwdsgc/edit?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                community’s lessons
+                {/* @ts-ignore theme and trigger wrong: https://github.com/tvkhoa/react-tippy/blob/c6e6169e3f2cabe05f1bfbd7e0dea1ddef4debe8/index.d.ts */}
+                <Tooltip
+                  title="Opens in a new tab"
+                  animation="shift"
+                  arrow="true"
+                  className=""
+                  duration="200"
+                  tabIndex="0"
+                  tag="span"
+                  theme="didoesdigital"
+                  trigger="mouseenter focus click"
+                  onShow={setAnnouncementMessage}
+                >
+                  <IconExternal
+                    ariaHidden="true"
+                    role="presentation"
+                    iconWidth="24"
+                    iconHeight="24"
+                    className="ml1 svg-icon-wrapper svg-baseline"
+                    iconTitle=""
+                  />
+                </Tooltip>
+              </GoogleAnalytics.OutboundLink>
+              .
+            </p>
+            <p>
+              The community lessons include extra topics like spacing,
+              capitalisation, quotations, and using{" "}
+              <span className="steno-stroke steno-stroke--subtle px05">
+                SPWER
+              </span>{" "}
+              for “inter-”/“enter-” words.
+            </p>
+            <p>
+              <Link
+                to={`${match.url}/custom/setup`.replace(/\/{2,}/g, "/")}
+                className="link-button dib"
+                style={{ lineHeight: 2 }}
+                id="ga--lesson-index--create-a-custom-lesson"
+              >
+                Create a custom lesson
+              </Link>
+              {customLesson.title !== "Steno" ? (
+                <Link
+                  to={`${match.url}/custom?study=discover&newWords=1&seenWords=1&retainedWords=1&sortOrder=sortOff&startFromWord=1`.replace(
+                    /\/{2,}/g,
+                    "/"
+                  )}
+                  onClick={stopLesson}
+                  className="dib ml2"
+                >
+                  Start custom lesson
+                </Link>
+              ) : null}
+            </p>
           </div>
         </div>
       </div>
