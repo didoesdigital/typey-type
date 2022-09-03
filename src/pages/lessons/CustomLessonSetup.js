e6fb31040eeadac5e7ff3d18a7e0a0aa239df286
Di <didoesdigital+git@gmail.com>
2022-09-03 15:34:38 +1000

    Housekeeping: run formatter

--- src/pages/lessons/CustomLessonSetup.js
+++ src/pages/lessons/CustomLessonSetup.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import CustomJSONLesson from "./CustomJSONLesson";
 import CustomLessonIntro from "./CustomLessonIntro";
 import CustomShareLessons from "./CustomShareLessons";
-import CustomWordListLesson from './CustomWordListLesson';
+import CustomWordListLesson from "./CustomWordListLesson";
 
 class CustomLessonSetup extends Component {
   componentDidMount() {
@@ -10,23 +10,30 @@ class CustomLessonSetup extends Component {
       fetchAndSetupGlobalDict,
       globalLookupDictionary,
       globalLookupDictionaryLoaded,
-      personalDictionaries
+      personalDictionaries,
     } = this.props;
 
     if (this.mainHeading) {
       this.mainHeading.focus();
     }
 
-    if (globalLookupDictionary && globalLookupDictionary.size < 2 && !globalLookupDictionaryLoaded) {
-      const shouldUsePersonalDictionaries = personalDictionaries
-        && Object.entries(personalDictionaries).length > 0
-        && !!personalDictionaries.dictionariesNamesAndContents;
+    if (
+      globalLookupDictionary &&
+      globalLookupDictionary.size < 2 &&
+      !globalLookupDictionaryLoaded
+    ) {
+      const shouldUsePersonalDictionaries =
+        personalDictionaries &&
+        Object.entries(personalDictionaries).length > 0 &&
+        !!personalDictionaries.dictionariesNamesAndContents;
 
-      fetchAndSetupGlobalDict(false, shouldUsePersonalDictionaries ? personalDictionaries : null)
-        .catch(error => {
-          console.error(error);
-          // this.showDictionaryErrorNotification();
-        });
+      fetchAndSetupGlobalDict(
+        false,
+        shouldUsePersonalDictionaries ? personalDictionaries : null
+      ).catch((error) => {
+        console.error(error);
+        // this.showDictionaryErrorNotification();
+      });
     }
   }
 
@@ -37,7 +44,7 @@ class CustomLessonSetup extends Component {
       customLessonMaterialValidationState,
       customLessonMaterialValidationMessages,
       globalLookupDictionary,
-      setAnnouncementMessage
+      setAnnouncementMessage,
     } = this.props;
 
     return (
@@ -46,42 +53,50 @@ class CustomLessonSetup extends Component {
           <div className="flex items-baseline mx-auto mw-1920 justify-between px3 py2">
             <div className="flex mr1 self-center">
               <header className="flex items-center min-h-40">
-                <h2 ref={(heading) => { this.mainHeading = heading; }} tabIndex="-1" id="about-typey-type-for-stenographers">Create a custom lesson</h2>
+                <h2
+                  ref={(heading) => {
+                    this.mainHeading = heading;
+                  }}
+                  tabIndex="-1"
+                  id="about-typey-type-for-stenographers"
+                >
+                  Create a custom lesson
+                </h2>
               </header>
             </div>
           </div>
         </div>
 
-          <div className="bg-info landing-page-section">
-            <CustomLessonIntro
-              createCustomLesson={createCustomLesson}
-              customLessonMaterial={customLessonMaterial}
-              customLessonMaterialValidationState={customLessonMaterialValidationState}
-              customLessonMaterialValidationMessages={customLessonMaterialValidationMessages}
-              setAnnouncementMessage={setAnnouncementMessage}
-            />
-          </div>
+        <div className="bg-info landing-page-section">
+          <CustomLessonIntro
+            createCustomLesson={createCustomLesson}
+            customLessonMaterial={customLessonMaterial}
+            customLessonMaterialValidationState={
+              customLessonMaterialValidationState
+            }
+            customLessonMaterialValidationMessages={
+              customLessonMaterialValidationMessages
+            }
+            setAnnouncementMessage={setAnnouncementMessage}
+          />
+        </div>
 
-          <div className="bg-white landing-page-section">
-            <CustomWordListLesson
-              globalLookupDictionary={globalLookupDictionary}
-            />
-          </div>
+        <div className="bg-white landing-page-section">
+          <CustomWordListLesson
+            globalLookupDictionary={globalLookupDictionary}
+          />
+        </div>
 
-          <div className="bg-info landing-page-section">
-            <CustomJSONLesson />
-          </div>
+        <div className="bg-info landing-page-section">
+          <CustomJSONLesson />
+        </div>
 
-          <div className="bg-white landing-page-section">
-            <CustomShareLessons
-              setAnnouncementMessage={setAnnouncementMessage}
-            />
-          </div>
-
+        <div className="bg-white landing-page-section">
+          <CustomShareLessons setAnnouncementMessage={setAnnouncementMessage} />
+        </div>
       </main>
-    )
+    );
   }
-
 }
 
 export default CustomLessonSetup;
