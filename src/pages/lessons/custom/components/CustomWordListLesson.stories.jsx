e67bd9dcd9d4ec0b390631c097705717a0c6b932
Di <didoesdigital+git@gmail.com>
2022-09-03 19:48:08 +1000

    Add CustomWordListLesson.stories.jsx

---
+++ src/pages/lessons/custom/components/CustomWordListLesson.stories.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { within, userEvent } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
+import CustomWordListLesson from "./CustomWordListLesson";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Lessons/Custom/CustomWordListLesson",
+  component: CustomWordListLesson,
+};
+
+const Template = (args) => (
+  <div className="p3">
+    <CustomWordListLesson globalLookupDictionary={new Map([
+      ["hello", [["H-L", "typey:typey-type.json"]]],
+      ["world", [["WORLD", "typey:typey-type.json"]]]
+    ])} {...args} />
+  </div>
+);
+
+export const CustomWordListLessonEmptyState = Template.bind({});
+CustomWordListLessonEmptyState.storyName = "Empty state";
+CustomWordListLessonEmptyState.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await expect(canvas.getByTestId('custom-lesson-formatted-pre')).not.toHaveClass('quote')
+};
+
+export const CustomWordListLessonFilled = Template.bind({});
+CustomWordListLessonFilled.storyName = "Dictionary filled";
+CustomWordListLessonFilled.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  userEvent.type(
+    await within(canvasElement).getByLabelText(
+      "Paste a word list without strokes here to create a custom lesson (using Plover theory by default):"
+    ),
+    `hello
+world`
+  );
+
+  await expect(canvas.getByTestId('custom-lesson-formatted-pre')).toHaveClass('quote')
+  await expect(canvas.getByTestId('custom-lesson-formatted-code')).toHaveTextContent(/WORLD/)
+  await expect(canvas.getByTestId('custom-lesson-formatted-code')).toHaveTextContent(/H-L/)
+};
