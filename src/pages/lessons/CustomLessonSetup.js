f25c795b68feedc0db5796352d0a4f55938cf9d4
Di <didoesdigital+git@gmail.com>
2022-09-03 15:45:01 +1000

    Convert CustomLessonSetup to function component

--- src/pages/lessons/CustomLessonSetup.js
+++ src/pages/lessons/CustomLessonSetup.js
@@ -1,22 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import CustomJSONLesson from "./CustomJSONLesson";
 import CustomLessonIntro from "./CustomLessonIntro";
 import CustomShareLessons from "./CustomShareLessons";
 import CustomWordListLesson from "./CustomWordListLesson";
 
-class CustomLessonSetup extends Component {
-  componentDidMount() {
-    const {
-      fetchAndSetupGlobalDict,
-      globalLookupDictionary,
-      globalLookupDictionaryLoaded,
-      personalDictionaries,
-    } = this.props;
+const CustomLessonSetup = (props) => {
+  const {
+    createCustomLesson,
+    customLessonMaterial,
+    customLessonMaterialValidationMessages,
+    customLessonMaterialValidationState,
+    fetchAndSetupGlobalDict,
+    globalLookupDictionary,
+    globalLookupDictionaryLoaded,
+    personalDictionaries,
+    setAnnouncementMessage,
+  } = props;
+  const mainHeading = useRef(null);
 
-    if (this.mainHeading) {
-      this.mainHeading.focus();
-    }
+  useEffect(() => {
+    mainHeading.current?.focus();
+  }, []);
 
+  useEffect(() => {
     if (
       globalLookupDictionary &&
       globalLookupDictionary.size < 2 &&
@@ -35,68 +41,58 @@ class CustomLessonSetup extends Component {
         // this.showDictionaryErrorNotification();
       });
     }
-  }
+  }, [
+    fetchAndSetupGlobalDict,
+    globalLookupDictionary,
+    globalLookupDictionaryLoaded,
+    personalDictionaries,
+  ]);
 
-  render() {
-    const {
-      createCustomLesson,
-      customLessonMaterial,
-      customLessonMaterialValidationState,
-      customLessonMaterialValidationMessages,
-      globalLookupDictionary,
-      setAnnouncementMessage,
-    } = this.props;
-
-    return (
-      <main id="main">
-        <div className="subheader">
-          <div className="flex items-baseline mx-auto mw-1920 justify-between px3 py2">
-            <div className="flex mr1 self-center">
-              <header className="flex items-center min-h-40">
-                <h2
-                  ref={(heading) => {
-                    this.mainHeading = heading;
-                  }}
-                  tabIndex="-1"
-                  id="about-typey-type-for-stenographers"
-                >
-                  Create a custom lesson
-                </h2>
-              </header>
-            </div>
+  return (
+    <main id="main">
+      <div className="subheader">
+        <div className="flex items-baseline mx-auto mw-1920 justify-between px3 py2">
+          <div className="flex mr1 self-center">
+            <header className="flex items-center min-h-40">
+              <h2
+                ref={mainHeading}
+                tabIndex="-1"
+                id="about-typey-type-for-stenographers"
+              >
+                Create a custom lesson
+              </h2>
+            </header>
           </div>
         </div>
+      </div>
 
-        <div className="bg-info landing-page-section">
-          <CustomLessonIntro
-            createCustomLesson={createCustomLesson}
-            customLessonMaterial={customLessonMaterial}
-            customLessonMaterialValidationState={
-              customLessonMaterialValidationState
-            }
-            customLessonMaterialValidationMessages={
-              customLessonMaterialValidationMessages
-            }
-            setAnnouncementMessage={setAnnouncementMessage}
-          />
-        </div>
+      <div className="bg-info landing-page-section">
+        <CustomLessonIntro
+          createCustomLesson={createCustomLesson}
+          customLessonMaterial={customLessonMaterial}
+          customLessonMaterialValidationState={
+            customLessonMaterialValidationState
+          }
+          customLessonMaterialValidationMessages={
+            customLessonMaterialValidationMessages
+          }
+          setAnnouncementMessage={setAnnouncementMessage}
+        />
+      </div>
 
-        <div className="bg-white landing-page-section">
-          <CustomWordListLesson
-            globalLookupDictionary={globalLookupDictionary}
-          />
-        </div>
+      <div className="bg-white landing-page-section">
+        <CustomWordListLesson globalLookupDictionary={globalLookupDictionary} />
+      </div>
 
-        <div className="bg-info landing-page-section">
-          <CustomJSONLesson />
-        </div>
+      <div className="bg-info landing-page-section">
+        <CustomJSONLesson />
+      </div>
 
-        <div className="bg-white landing-page-section">
-          <CustomShareLessons setAnnouncementMessage={setAnnouncementMessage} />
-        </div>
-      </main>
-    );
-  }
-}
+      <div className="bg-white landing-page-section">
+        <CustomShareLessons setAnnouncementMessage={setAnnouncementMessage} />
+      </div>
+    </main>
+  );
+};
 
 export default CustomLessonSetup;
