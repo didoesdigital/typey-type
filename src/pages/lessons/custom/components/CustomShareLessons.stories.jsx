b4d70208f5f454552ff41a7e9de5c4381c7be66b
Di <didoesdigital+git@gmail.com>
2022-09-03 19:50:28 +1000

    Add CustomShareLessons.stories.jsx

---
+++ src/pages/lessons/custom/components/CustomShareLessons.stories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import CustomShareLessons from "./CustomShareLessons";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Lessons/Custom/CustomShareLessons",
+  component: CustomShareLessons,
+};
+
+const Template = (args) => (
+  <div className="p3">
+    <CustomShareLessons setAnnouncementMessage={() => console.log("announce")} />
+  </div>
+);
+
+export const CustomShareLessonsStory = Template.bind({});
