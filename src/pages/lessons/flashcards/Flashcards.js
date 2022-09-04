244d074abfa1e1150617f9783fa0fc3f735a0cd4
Di <didoesdigital+git@gmail.com>
2022-09-04 14:22:12 +1000

    Extract FlashcardsModal.tsx

--- src/pages/lessons/flashcards/Flashcards.js
+++ src/pages/lessons/flashcards/Flashcards.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoogleAnalytics from 'react-ga';
 import ReactModal from 'react-modal';
 import FlashcardsCarouselActionButtons from './components/FlashcardsCarouselActionButtons';
+import FlashcardsModal from './components/FlashcardsModal';
 import StrokesForWords from '../../../components/StrokesForWords';
 import { IconFullscreen } from '../../../components/Icon';
 import * as Utils from '../../../utils/utils';
@@ -11,8 +12,6 @@ import { CarouselProvider, Slider, ButtonBack, ButtonNext } from 'pure-react-car
 import SlideNodes from './components/SlideNodes';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { Link } from 'react-router-dom';
-import { IconExternal } from '../../../components/Icon';
-import { Tooltip } from 'react-tippy';
 
 const shortestDimension = 3;
 const longestDimension = 4;
@@ -391,87 +390,10 @@ currentSlide: currentSlide
           <div className="p3 mx-auto mw-1024">
             <div className="flex flex-wrap justify-between">
               <p className={"text-small self-center hide-in-fullscreen" + fullscreen}>Back to <Link to={lessonpath} className={"hide-in-fullscreen" + fullscreen}>{this.state.title} lesson</Link>.</p>
-              <p className={"hide-in-fullscreen" + fullscreen}>
-                <button className="button button--secondary" onClick={this.handleOpenModal}>About flashcards</button>
-                <ReactModal
-                  isOpen={this.state.showModal}
-                  aria={{
-                    labelledby: "aria-modal-heading",
-                    describedby: "aria-modal-description"
-                  }}
-                  ariaHideApp={true}
-                  closeTimeoutMS={300}
-                  role="dialog"
-                  onRequestClose={this.handleCloseModal}
-                  className={{
-                    "base": "modal",
-                    "afterOpen": "modal--after-open",
-                    "beforeClose": "modal--before-close"
-                  }}
-                  overlayClassName={{
-                    "base": "modal__overlay",
-                    "afterOpen": "modal__overlay--after-open",
-                    "beforeClose": "modal__overlay--before-close"
-                  }}
-                >
-                  <div className="fr">
-                    <button className="de-emphasized-button hide-md" onClick={this.handleCloseModal}>Close</button>
-                  </div>
-                  <h3 id="aria-modal-heading">Flashcards</h3>
-                  <div id="aria-modal-description">
-                    <p>Flashcards are designed for mobile devices so you can memorise steno briefs on the go. When you’re unable to recall a brief, tap “Hard” to say it was hard to remember. When you can recall a brief without hesitation, tap “Easy”. While studying flashcards, imagine which fingers and the shape of the outline you’d use to stroke a word.</p>
-                    <p>If it’s been a while since you’ve studied, the “threshold” will be set quite high. You’ll see flashcards you’ve studied that are below the threshold. That is, if the threshold is 12, you’ll see flashcards for words you’ve marked “Easy” less than 12 times. If you’ve marked a word as “Easy” 15 times, it won’t shown again until more time has passed.</p>
-            <p>Thanks to Jim Ladd, you can also use the{" "}<GoogleAnalytics.OutboundLink
-                eventLabel="Anki"
-                aria-label="Anki (external link opens in new tab)"
-                to="https://apps.ankiweb.net/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Anki<span className="whitespace-nowrap">
-                <Tooltip
-                  title="(external link opens in new tab)"
-                  className=""
-                  animation="shift"
-                  arrow="true"
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={this.props.setAnnouncementMessage}
-                >
-                  <IconExternal ariaHidden="true" role="presentation" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="" />
-                </Tooltip></span>
-            </GoogleAnalytics.OutboundLink> app to memorise briefs using the{" "}<GoogleAnalytics.OutboundLink
-                eventLabel="flashcard decks for the Top 2000 Words"
-                aria-label="flashcard decks for the Top 2000 Words (external link opens in new tab)"
-                to="https://github.com/jladdjr/anki-decks/tree/master/Plover%20-%20Project%20Gutenberg%20Top%2010k%20Words"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                flashcard decks for the Top 2000 Words<span className="whitespace-nowrap">
-                <Tooltip
-                  title="(external link opens in new tab)"
-                  className=""
-                  animation="shift"
-                  arrow="true"
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={this.props.setAnnouncementMessage}
-                >
-                  <IconExternal ariaHidden="true" role="presentation" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="" />
-                </Tooltip></span>
-            </GoogleAnalytics.OutboundLink> from Project Gutenberg he built using Typey&nbsp;Type steno diagrams.</p>
-                  </div>
-                  <div className="text-right">
-                    <button className="button" onClick={this.handleCloseModal}>OK</button>
-                  </div>
-                </ReactModal>
-              </p>
+              <FlashcardsModal
+                fullscreen={this.props.fullscreen}
+                setAnnouncementMessage={this.props.setAnnouncementMessage}
+              />
             </div>
             <div>
 
