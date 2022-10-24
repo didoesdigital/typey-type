88dd0d2b138d617b8c6cf118714c8ea33b9033cb
Di <didoesdigital+git@gmail.com>
2022-10-24 21:14:02 +1000

    penaliseStars

--- src/utils/transformingDictionaries/penaliseStars.test.js
+++ src/utils/transformingDictionaries/penaliseStars.test.js
@@ -1,9 +1,15 @@
 import penaliseStars from "./penaliseStars";
 
 describe("penaliseStars", () => {
-  it("returns the number of stars", () => {
+  it("returns the number of stars in 1-star outline", () => {
     const outline = "AO*EUL";
-    const translation = "AOEULS";
+    const translation = "aisle";
     expect(penaliseStars(outline, translation)).toEqual(1);
+  });
+
+  it("returns 0 for outline with no stars", () => {
+    const outline = "AOEULS";
+    const translation = "aisle";
+    expect(penaliseStars(outline, translation)).toEqual(0);
   });
 });
