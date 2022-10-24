6aa46bafdec222234f7306f6887c5b5f3068d222
Di <didoesdigital+git@gmail.com>
2022-10-24 21:23:40 +1000

    Expand penaliseStars test

--- src/utils/transformingDictionaries/penaliseStars.test.ts
+++ src/utils/transformingDictionaries/penaliseStars.test.ts
@@ -1,6 +1,12 @@
 import penaliseStars from "./penaliseStars";
 
 describe("penaliseStars", () => {
+  it("returns the number of stars in 2-star outline", () => {
+    const outline = "TKR*UPBG/*EPB";
+    const translation = "drunken";
+    expect(penaliseStars(outline, translation)).toEqual(2);
+  });
+
   it("returns the number of stars in 1-star outline", () => {
     const outline = "AO*EUL";
     const translation = "aisle";
