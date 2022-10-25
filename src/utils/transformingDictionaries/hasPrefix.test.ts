84c0a0f81f7f81039fc02cbb42d1f4b9bfbb35e1
Di <didoesdigital+git@gmail.com>
2022-10-25 21:23:38 +1000

    Add hasPrefix TS and test

---
+++ src/utils/transformingDictionaries/hasPrefix.test.ts
@@ -0,0 +1,42 @@
+import hasPrefix from "./hasPrefix";
+import { AffixList } from "../affixList";
+
+describe("hasPrefix", () => {
+  beforeEach(() => {
+    const affixList = new AffixList(
+      new Map([
+        ["{^en}", [["*EPB", "typey:typey-type.json"]]],
+        ["{^ment}", [["*PLT", "typey:typey-type.json"]]],
+        ["{a^}", [["A", "typey:typey-type.json"]]],
+        ["{in^}", [["EUPB", "typey:typey-type.json"]]],
+        ["{^ly}", [["HREU", "typey:typey-type.json"]]],
+      ])
+    );
+    AffixList.setSharedInstance(affixList);
+  });
+
+  it("returns true for outline with prefix", () => {
+    const outline = "A/HROEPB";
+    const translation = "along";
+
+    expect(
+      hasPrefix(outline, translation, AffixList.getSharedInstance().prefixes)
+    ).toEqual(true);
+  });
+
+  it("returns false for outline that does not start with a prefix", () => {
+    const outline = "TKPWUT/*EPB/PWERG";
+    const translation = "Gutenberg";
+    expect(
+      hasPrefix(outline, translation, AffixList.getSharedInstance().prefixes)
+    ).toEqual(false);
+  });
+
+  it("returns false for single-stroke outlines", () => {
+    const outline = "TEFT";
+    const translation = "test";
+    expect(
+      hasPrefix(outline, translation, AffixList.getSharedInstance().prefixes)
+    ).toEqual(false);
+  });
+});
---
+++ src/utils/transformingDictionaries/hasPrefix.ts
@@ -0,0 +1,14 @@
+import type { AffixItem } from "../../types";
+
+const hasPrefix = (
+  outline: string,
+  translation: string,
+  prefixes: AffixItem[]
+) =>
+  prefixes.some(
+    ([prefixOutline, prefixTranslation]) =>
+      outline.startsWith(prefixOutline) &&
+      translation.startsWith(prefixTranslation)
+  );
+
+export default hasPrefix;
