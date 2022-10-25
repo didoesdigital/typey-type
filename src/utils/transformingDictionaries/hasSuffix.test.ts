50473113fba7b8f1077d5d1f653a513491a853d0
Di <didoesdigital+git@gmail.com>
2022-10-25 21:21:56 +1000

    Add hasSuffix TS and test

---
+++ src/utils/transformingDictionaries/hasSuffix.test.ts
@@ -0,0 +1,42 @@
+import hasSuffix from "./hasSuffix";
+import { AffixList } from "../affixList";
+
+describe("hasSuffix", () => {
+  beforeEach(() => {
+    const affixList = new AffixList(
+      new Map([
+        ["{^en}", [["*EPB", "typey:typey-type.json"]]],
+        ["{^ment}", [["*PLT", "typey:typey-type.json"]]],
+        ["{a^}", [["A", "typey:typey-type.json"]]],
+        ["{in^}", [["EUPB", "typey:typey-type.json"]]],
+        ["{^ly}", [["HREU", "typey:typey-type.json"]]],
+      ])
+    );
+    AffixList.setSharedInstance(affixList);
+  });
+
+  it("returns true for outline with suffix", () => {
+    const outline = "SWEUFT/HREU";
+    const translation = "swiftly";
+
+    expect(
+      hasSuffix(outline, translation, AffixList.getSharedInstance().suffixes)
+    ).toEqual(true);
+  });
+
+  it("returns false for outline that does not end with a suffix", () => {
+    const outline = "TKPWUT/*EPB/PWERG";
+    const translation = "Gutenberg";
+    expect(
+      hasSuffix(outline, translation, AffixList.getSharedInstance().suffixes)
+    ).toEqual(false);
+  });
+
+  it("returns false for single-stroke outlines", () => {
+    const outline = "TEFT";
+    const translation = "test";
+    expect(
+      hasSuffix(outline, translation, AffixList.getSharedInstance().suffixes)
+    ).toEqual(false);
+  });
+});
---
+++ src/utils/transformingDictionaries/hasSuffix.ts
@@ -0,0 +1,13 @@
+import type { AffixItem } from "../../types";
+
+const hasSuffix = (
+  outline: string,
+  translation: string,
+  suffixes: AffixItem[]
+) =>
+  suffixes.some(
+    ([suffixOutline, suffixTranslation]) =>
+      outline.endsWith(suffixOutline) && translation.endsWith(suffixTranslation)
+  );
+
+export default hasSuffix;
