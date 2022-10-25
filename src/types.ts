489bb3e505b8fdb3b5d78f8966751bff17c2a57f
Di <didoesdigital+git@gmail.com>
2022-10-25 21:03:38 +1000

    Add affix types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
 export type StenoDictionary = {
   [outline: string]: string;
 };
+
+export type AffixItem = [string, string];
+
+export type AffixObject = {
+  prefixes: AffixItem[];
+  suffixes: AffixItem[];
+};
+
