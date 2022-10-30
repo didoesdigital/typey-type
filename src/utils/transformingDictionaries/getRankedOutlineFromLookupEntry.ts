e9ab0b8b90fd91417b95068543c6dadbd7fd89b8
Di <didoesdigital+git@gmail.com>
2022-10-30 17:16:09 +1000

    Add TSDoc examples to params/returns

--- src/utils/transformingDictionaries/getRankedOutlineFromLookupEntry.ts
+++ src/utils/transformingDictionaries/getRankedOutlineFromLookupEntry.ts
@@ -6,6 +6,11 @@ import type { StrokeAndNamespacedDict, Translation } from "../../types";
 
 /**
  * Ranks outlines (and their source dictionaries) for a translation and returns the “best” outline and its source dictionary
+ *
+ * @param lookupEntries - Example: [ ["THAUT", "plover:main.json"], ["THAUGT", "typey:typey-type.json"] ],
+ * @param translation - Example: "gazed"
+ * @param affixList - Example: { prefixes: [["A*UT/", "auto"], ["TPHRAOUR/", "fluoro"]], suffixes: [["/WAL", "ual"], ["/AOEUBL", "izable"]] }
+ * @returns bestOutline - Example: "TEFT"
  */
 const getRankedOutlineFromLookupEntry = (
   lookupEntries: StrokeAndNamespacedDict[],
