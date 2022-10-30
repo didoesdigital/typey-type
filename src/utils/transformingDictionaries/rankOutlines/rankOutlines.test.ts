cf376b8054ecb6b5c92d52bc65f1224b2efb1280
Di <didoesdigital+git@gmail.com>
2022-10-30 10:39:43 +1000

    Move rankOutlines test

--- src/utils/transformingDictionaries/rankOutlines/rankOutlines.test.ts
+++ src/utils/transformingDictionaries/rankOutlines/rankOutlines.test.ts
@@ -1,4 +1,4 @@
-import rankOutlines from "./rankOutlines/rankOutlines";
+import rankOutlines from "./rankOutlines";
 
 describe("rankOutlines", () => {
   it("returns A user entry before B non-user entry", () => {
