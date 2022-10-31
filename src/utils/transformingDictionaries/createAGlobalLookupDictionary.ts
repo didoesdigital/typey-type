883a08f98a03dd1d7d01069fda83aa93ed20a0d9
Di <didoesdigital+git@gmail.com>
2022-10-31 12:17:00 +1000

    Fix types

--- src/utils/transformingDictionaries/createAGlobalLookupDictionary.ts
+++ src/utils/transformingDictionaries/createAGlobalLookupDictionary.ts
@@ -26,13 +26,13 @@ const createAGlobalLookupDictionary = (
 ): LookupDictWithNamespacedDictsAndConfig => {
   // TODO: one day, this could be the place we check for whether Typey Type dictionaries or the Plover dictionary are enabled and if so combineValidDictionaries with them and add to 'configuration'
 
-  let combinedLookupDictionary:
-    | LookupDictWithNamespacedDicts
-    | LookupDictWithNamespacedDictsAndConfig = combineValidDictionaries(
-    personalDictionariesNamesAndContents,
-    dictTypeyType,
-    ploverDict
-  );
+  let combinedLookupDictionary: LookupDictWithNamespacedDicts =
+    combineValidDictionaries(
+      personalDictionariesNamesAndContents,
+      dictTypeyType,
+      ploverDict
+    );
+
   // let sortedAndCombinedLookupDictionary = rankAllOutlinesInCombinedLookupDictionary(combinedLookupDictionary); // has a bug; instead of sorted entire dict, we sort per entry used within chooseOutlineForPhrase function
   let configuration = [
     `${SOURCE_NAMESPACES.get("typey")}:typey-type.json`,
@@ -40,6 +40,7 @@ const createAGlobalLookupDictionary = (
       (d) => `${SOURCE_NAMESPACES.get("user")}:${d[0]}`
     ),
   ];
+
   if (!!ploverDict) {
     configuration.push(
       `${SOURCE_NAMESPACES.get("plover")}:${LATEST_PLOVER_DICT_NAME}`
