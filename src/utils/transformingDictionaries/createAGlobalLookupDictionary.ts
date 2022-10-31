772200bc9c5e573d54936b66d32f6ca3fbc91366
Di <didoesdigital+git@gmail.com>
2022-10-31 11:46:34 +1000

    Add createAGlobalLookupDictionary

---
+++ src/utils/transformingDictionaries/createAGlobalLookupDictionary.ts
@@ -0,0 +1,47 @@
+import {
+  LATEST_PLOVER_DICT_NAME,
+  SOURCE_NAMESPACES,
+} from "../../constant/index.js";
+import { combineValidDictionaries } from "./transformingDictionaries";
+import {
+  LookupDictWithNamespacedDicts,
+  LookupDictWithNamespacedDictsAndConfig,
+  DictName,
+  StenoDictionary,
+} from "../../types";
+
+type PersonalDictionaryNameAndContents = [DictName, StenoDictionary];
+type PersonalDictionariesNamesAndContents = PersonalDictionaryNameAndContents[];
+
+const createAGlobalLookupDictionary = (
+  personalDictionariesNamesAndContents: PersonalDictionariesNamesAndContents,
+  dictTypeyType: StenoDictionary,
+  ploverDict: any = null
+): LookupDictWithNamespacedDictsAndConfig => {
+  // TODO: one day, this could be the place we check for whether Typey Type dictionaries or the Plover dictionary are enabled and if so combineValidDictionaries with them and add to 'configuration'
+
+  let combinedLookupDictionary:
+    | LookupDictWithNamespacedDicts
+    | LookupDictWithNamespacedDictsAndConfig = combineValidDictionaries(
+    personalDictionariesNamesAndContents,
+    dictTypeyType,
+    ploverDict
+  );
+  // let sortedAndCombinedLookupDictionary = rankAllOutlinesInCombinedLookupDictionary(combinedLookupDictionary); // has a bug; instead of sorted entire dict, we sort per entry used within chooseOutlineForPhrase function
+  let configuration = [
+    `${SOURCE_NAMESPACES.get("typey")}:typey-type.json`,
+    ...personalDictionariesNamesAndContents.map(
+      (d) => `${SOURCE_NAMESPACES.get("user")}:${d[0]}`
+    ),
+  ];
+  if (!!ploverDict) {
+    configuration.push(
+      `${SOURCE_NAMESPACES.get("plover")}:${LATEST_PLOVER_DICT_NAME}`
+    );
+  }
+  combinedLookupDictionary.configuration = configuration;
+
+  return combinedLookupDictionary as LookupDictWithNamespacedDictsAndConfig;
+};
+
+export default createAGlobalLookupDictionary;
