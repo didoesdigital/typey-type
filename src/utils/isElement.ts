11475d1c74ac5ef900de86b7904b187280ff58ea
Di <didoesdigital+git@gmail.com>
2022-11-16 19:52:26 +1000

    Housekeeping: format isElement

--- src/utils/isElement.ts
+++ src/utils/isElement.ts
@@ -1,11 +1,13 @@
 function isElement(obj: any): boolean {
   try {
     return obj instanceof HTMLElement;
-  }
-  catch(e){
-    return (typeof obj==="object") &&
-      (obj.nodeType===1) && (typeof obj.style === "object") &&
-      (typeof obj.ownerDocument ==="object");
+  } catch (e) {
+    return (
+      typeof obj === "object" &&
+      obj.nodeType === 1 &&
+      typeof obj.style === "object" &&
+      typeof obj.ownerDocument === "object"
+    );
   }
 }
 
