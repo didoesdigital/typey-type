b6252b9fd884b48a60d8e9f1c80cda22a75f65a9
Di <didoesdigital+git@gmail.com>
2022-11-16 22:17:37 +1000

    Format Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import GoogleAnalytics from 'react-ga';
-import { IconExternal } from './Icon';
-import { Tooltip } from 'react-tippy';
-import 'react-tippy/dist/tippy.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import GoogleAnalytics from "react-ga";
+import { IconExternal } from "./Icon";
+import { Tooltip } from "react-tippy";
+import "react-tippy/dist/tippy.css";
 
 class Footer extends Component {
   render() {
@@ -14,40 +14,93 @@ class Footer extends Component {
       fullscreen = "";
     }
     return (
-      <div className={"bg-slat bt b--brand-primary-tint--60 hide-in-fullscreen" + fullscreen}>
-        <footer role="contentinfo" className={"footer mx-auto mw-1920 flex flex-wrap items-center justify-between pt1 pb1 pl3 pr3 hide-in-fullscreen" + fullscreen}>
-          <p className="text-center mb0"><small><Link to="/support" className="no-underline">About</Link> and <Link to="/support#privacy" className="no-underline">Privacy</Link></small></p>
-          <p className="text-center mb0"><small><Link to="/contribute" className="no-underline">Contribute</Link></small></p>
-          <p className="text-center mb0"><small>Made with <span aria-label="love" role="img">❤️</span> by <a href="https://didoesdigital.com/" className="no-underline">DiDoesDigital</a></small></p>
-          <p className="text-center mb0"><small><Link to="/break" className="no-underline">Take a break</Link></small></p>
-          <p className="text-center mb0"><small><GoogleAnalytics.OutboundLink
-              className="no-underline"
-              eventLabel="DiDoesDigital: Typey Type updates and steno news (external link opens in new tab)"
-              aria-label="Typey Type updates and steno news (external link opens in new tab)"
-              to="https://didoesdigital.com/#newsletter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Steno&nbsp;news
-              <Tooltip
-                title="(external link opens in new tab)"
-                animation="shift"
-                arrow="true"
-                className=""
-                duration="200"
-                tabIndex="0"
-                tag="span"
-                theme="didoesdigital"
-                trigger="mouseenter focus click"
-                onShow={this.props.setAnnouncementMessage}
+      <div
+        className={
+          "bg-slat bt b--brand-primary-tint--60 hide-in-fullscreen" + fullscreen
+        }
+      >
+        <footer
+          role="contentinfo"
+          className={
+            "footer mx-auto mw-1920 flex flex-wrap items-center justify-between pt1 pb1 pl3 pr3 hide-in-fullscreen" +
+            fullscreen
+          }
+        >
+          <p className="text-center mb0">
+            <small>
+              <Link to="/support" className="no-underline">
+                About
+              </Link>{" "}
+              and{" "}
+              <Link to="/support#privacy" className="no-underline">
+                Privacy
+              </Link>
+            </small>
+          </p>
+          <p className="text-center mb0">
+            <small>
+              <Link to="/contribute" className="no-underline">
+                Contribute
+              </Link>
+            </small>
+          </p>
+          <p className="text-center mb0">
+            <small>
+              Made with{" "}
+              <span aria-label="love" role="img">
+                ❤️
+              </span>{" "}
+              by{" "}
+              <a href="https://didoesdigital.com/" className="no-underline">
+                DiDoesDigital
+              </a>
+            </small>
+          </p>
+          <p className="text-center mb0">
+            <small>
+              <Link to="/break" className="no-underline">
+                Take a break
+              </Link>
+            </small>
+          </p>
+          <p className="text-center mb0">
+            <small>
+              <GoogleAnalytics.OutboundLink
+                className="no-underline"
+                eventLabel="DiDoesDigital: Typey Type updates and steno news (external link opens in new tab)"
+                aria-label="Typey Type updates and steno news (external link opens in new tab)"
+                to="https://didoesdigital.com/#newsletter"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <IconExternal ariaHidden="true" role="presentation" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="" />
-              </Tooltip>
-            </GoogleAnalytics.OutboundLink></small>
+                Steno&nbsp;news
+                <Tooltip
+                  title="(external link opens in new tab)"
+                  animation="shift"
+                  arrow="true"
+                  className=""
+                  duration="200"
+                  tabIndex="0"
+                  tag="span"
+                  theme="didoesdigital"
+                  trigger="mouseenter focus click"
+                  onShow={this.props.setAnnouncementMessage}
+                >
+                  <IconExternal
+                    ariaHidden="true"
+                    role="presentation"
+                    iconWidth="24"
+                    iconHeight="24"
+                    className="ml1 svg-icon-wrapper svg-baseline"
+                    iconTitle=""
+                  />
+                </Tooltip>
+              </GoogleAnalytics.OutboundLink>
+            </small>
           </p>
         </footer>
       </div>
-    )
+    );
   }
 }
 
