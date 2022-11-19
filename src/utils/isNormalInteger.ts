5065c52f6781c4a3fb15a97cd37ac17dec9682be
Di <didoesdigital+git@gmail.com>
2022-11-19 13:57:06 +1000

    Convert let to const

--- src/utils/isNormalInteger.ts
+++ src/utils/isNormalInteger.ts
@@ -1,5 +1,5 @@
 function isNormalInteger(str: string): boolean {
-  let n = Math.floor(Number(str));
+  const n = Math.floor(Number(str));
   return n !== Infinity && String(n) === str && n >= 0;
 }
 
