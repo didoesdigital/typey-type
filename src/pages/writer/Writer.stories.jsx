daa7013d75bac49d6bd77e93e8eb0f28685d7667
Di <didoesdigital+git@gmail.com>
2022-11-19 15:51:16 +1000

    Add WriterStory

---
+++ src/pages/writer/Writer.stories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Writer from "./Writer";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Pages/Writer",
+  component: Writer,
+};
+
+const Template = (args) => {
+  return (
+    <Writer
+      changeStenoLayout={() => {
+        console.log("Change steno layout");
+      }}
+      changeWriterInput={() => {
+        console.log("Change writer input");
+      }}
+      setAnnouncementMessage={() => console.log("announce")}
+      setAnnouncementMessageString={() => console.log("announce string")}
+      globalUserSettings={{}}
+      userSettings={{ stenoLayout: "stenoLayoutAmericanSteno" }}
+      {...args}
+    />
+  );
+};
+
+export const WriterStory = Template.bind({});
