ff31cd69578548e8c93faafd4728d944ca80d7cf
Di <didoesdigital+git@gmail.com>
2022-11-19 15:32:56 +1000

    Add Home.stories.jsx

---
+++ src/pages/home/Home.stories.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Pages/Home",
+  component: Home,
+};
+
+const Template = (args) => {
+  return <Home {...args} />;
+};
+
+export const HomeStory = Template.bind({});
