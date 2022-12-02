2b1f8f7f7cb9951c34fa4303e6afe24d3d10ca53
Di <didoesdigital+git@gmail.com>
2022-12-02 19:48:32 +1000

    Add MetronomeStory

---
+++ src/pages/lessons/components/Metronome.stories.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Metronome from "./Metronome";
+import userSettings from "../../../stories/fixtures/userSettings";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Lessons/Metronome",
+  component: Metronome,
+  id: "metronome", // permalink
+};
+
+const Template = (args) => {
+  return (
+    <Metronome
+      setAnnouncementMessage={() => console.log("announce")}
+      userSettings={userSettings}
+      {...args}
+    />
+  );
+};
+
+export const MetronomeStory = Template.bind({});
+MetronomeStory.storyName = "Metronome";
