7260a167c763f05c6003178fa017f37ffe0cceb7
Di <didoesdigital+git@gmail.com>
2022-12-04 21:50:44 +1000

    Move EntireMaterial to TSX

--- src/components/Material/EntireMaterial.tsx
+++ src/components/Material/EntireMaterial.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+import type { PresentedMaterialItem, SpacePlacement } from "../../types";
+
+type Props = {
+  presentedMaterial: any;
+  spacePlacement: SpacePlacement;
+};
+
 export default React.memo(function EntireMaterial({
   presentedMaterial,
   spacePlacement,
-}) {
+}: Props) {
   const isSpaceBefore = spacePlacement === "spaceBeforeOutput";
   const isSpaceAfter = spacePlacement === "spaceAfterOutput";
 
@@ -15,18 +22,20 @@ export default React.memo(function EntireMaterial({
   const useSeparator =
     spacePlacement === "spaceOff" || spacePlacement === "spaceExact";
 
-  return presentedMaterial.map((phraseAndStroke, index) => (
-    <span
-      id={`presented-material-phrase-${index}`}
-      key={index}
-      className="hide-soft-hyphen"
-    >
-      {spaceBeforeOutput}
-      {phraseAndStroke.phrase.replaceAll(" ", " ")}
-      {spaceAfterOutput}
-      {useSeparator && (
-        <span className="separator o-0">&nbsp;&#8203;&shy;</span>
-      )}
-    </span>
-  ));
+  return presentedMaterial.map(
+    (phraseAndStroke: PresentedMaterialItem, index: number) => (
+      <span
+        id={`presented-material-phrase-${index}`}
+        key={index}
+        className="hide-soft-hyphen"
+      >
+        {spaceBeforeOutput}
+        {phraseAndStroke.phrase.replaceAll(" ", " ")}
+        {spaceAfterOutput}
+        {useSeparator && (
+          <span className="separator o-0">&nbsp;&#8203;&shy;</span>
+        )}
+      </span>
+    )
+  );
 });
