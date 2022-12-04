dc09dbddb3f90cfecf711dc1d85e7a25047f5960
Di <didoesdigital+git@gmail.com>
2022-12-04 22:44:02 +1000

    Make MainLessonView story work

---
+++ src/pages/lessons/MainLessonView.stories.jsx
@@ -0,0 +1,187 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import MainLessonView from "./MainLessonView";
+import userSettings from "../../stories/fixtures/userSettings";
+import Zipper from "../../utils/zipper";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Lessons/Main lesson view",
+  component: MainLessonView,
+  id: "main-lesson-view", // permalink
+};
+
+let userGoals = {
+  newWords: 5,
+  oldWords: 15,
+};
+const presentedMaterialCurrentItem = {
+  phrase: "test",
+  stroke: "TEFT",
+};
+
+const sourceMaterial = [
+  {
+    phrase: "test",
+    stroke: "TEFT",
+  },
+];
+
+const customLesson = {
+  sourceMaterial: sourceMaterial,
+  presentedMaterial: [
+    {
+      phrase: "test",
+      stroke: "TEFT",
+    },
+  ],
+  settings: {
+    ignoredChars: "",
+  },
+  title: "Test lesson",
+  subtitle: "",
+  newPresentedMaterial: new Zipper(sourceMaterial),
+  path: "/lessons/fundamentals/test/",
+};
+const lesson = {
+  sourceMaterial: sourceMaterial,
+  presentedMaterial: [
+    {
+      phrase: "test",
+      stroke: "TEFT",
+    },
+  ],
+  settings: {
+    ignoredChars: "",
+  },
+  title: "Test lesson",
+  subtitle: "",
+  newPresentedMaterial: new Zipper(sourceMaterial),
+  path: "/lessons/fundamentals/test/",
+};
+
+const globalLookupDictionary = new Map([
+  ["huh", [["H*U", "typey:typey-type.json"]]],
+  ["gonna", [["TKPW*G", "typey:typey-type.json"]]],
+]);
+
+const lessonIndex = [
+  {
+    title: "Test lesson",
+    subtitle: "",
+    category: "Fundamentals",
+    subcategory: "",
+    path: "/lessons/fundamentals/test/",
+  },
+];
+
+const handleLesson = (path) => undefined;
+const stopLesson = () => undefined;
+
+const Template = (args) => {
+  return (
+    <Switch>
+      <Route
+        render={(props) => (
+          <div>
+            <MainLessonView
+              customLesson={customLesson}
+              fetchAndSetupGlobalDict={() => Promise.resolve(true)}
+              globalLookupDictionary={globalLookupDictionary}
+              globalLookupDictionaryLoaded={true}
+              updateFlashcardsMetWords={() => undefined}
+              handleLesson={handleLesson}
+              lesson={lesson}
+              lessonIndex={lessonIndex}
+              stopLesson={stopLesson}
+              setAnnouncementMessage={() => console.log("announce")}
+              userSettings={userSettings}
+              updateFlashcardsProgress={() => undefined}
+              changeFullscreen={() => undefined}
+              flashcardsMetWords={{}}
+              customLessonMaterial={[]}
+              customLessonMaterialValidationState={[]}
+              customLessonMaterialValidationMessages={[]}
+              flashcardsProgress={{}}
+              globalUserSettings={{}}
+              personalDictionaries={{ dictionariesNamesAndContents: null }}
+              lessonsProgress={{}}
+              lessonNotFound={false}
+              fullscreen={false}
+              lessonSubTitle={""}
+              lessonTitle={"Test lesson"}
+              path={"/lessons/fundamentals/test/"}
+              actualText={""}
+              currentLessonStrokes={[]}
+              currentPhraseID={0}
+              disableUserSettings={false}
+              metWords={{}}
+              previousCompletedPhraseAsTyped={[]}
+              recommendationHistory={[]}
+              repetitionsRemaining={1}
+              revisionMaterial={[]}
+              revisionMode={false}
+              startTime={0}
+              settings={{ ignoredChars: "" }}
+              showStrokesInLesson={false}
+              targetStrokeCount={1}
+              timer={1}
+              topSpeedPersonalBest={40}
+              updateUserGoals={userGoals}
+              totalNumberOfMatchedWords={0}
+              totalNumberOfNewWordsMet={0}
+              totalNumberOfLowExposuresSeen={0}
+              totalNumberOfRetainedWords={0}
+              totalNumberOfMistypedWords={0}
+              totalNumberOfHintedWords={0}
+              updateGlobalLookupDictionary={() => undefined}
+              updatePersonalDictionaries={() => undefined}
+              restartLesson={() => undefined}
+              reviseLesson={() => undefined}
+              handleStopLesson={() => undefined}
+              changeShowStrokesInLesson={() => undefined}
+              changeShowStrokesOnMisstroke={() => undefined}
+              changeSortOrderUserSetting={() => undefined}
+              changeSpacePlacementUserSetting={() => undefined}
+              changeStenoLayout={() => undefined}
+              changeShowScoresWhileTyping={() => undefined}
+              changeShowStrokesAs={() => undefined}
+              changeUserSetting={() => undefined}
+              chooseStudy={() => undefined}
+              completedPhrases={[]}
+              createCustomLesson={() => undefined}
+              propsLesson={lesson}
+              currentPhrase={presentedMaterialCurrentItem.phrase}
+              currentStroke={presentedMaterialCurrentItem.stroke}
+              handleBeatsPerMinute={() => undefined}
+              handleDiagramSize={() => undefined}
+              handleLimitWordsChange={() => undefined}
+              handleStartFromWordChange={() => undefined}
+              handleRepetitionsChange={() => undefined}
+              handleUpcomingWordsLayout={() => undefined}
+              updateRevisionMaterial={() => undefined}
+              sayCurrentPhraseAgain={() => undefined}
+              setAnnouncementMessageString={() => undefined}
+              startFromWordOne={() => undefined}
+              stenoHintsOnTheFly={true}
+              startCustomLesson={() => undefined}
+              setUpProgressRevisionLesson={() => undefined}
+              setupLesson={() => undefined}
+              charsPerWord={() => undefined}
+              totalWordCount={1}
+              upcomingPhrases={[]}
+              updateRecommendationHistory={() => undefined}
+              updateMarkup={() => undefined}
+              updateTopSpeedPersonalBest={() => undefined}
+              {...props}
+              {...args}
+            />
+          </div>
+        )}
+      />
+    </Switch>
+  );
+};
+
+export const MainLessonViewStory = Template.bind({});
+MainLessonViewStory.storyName = "Main lesson view";
