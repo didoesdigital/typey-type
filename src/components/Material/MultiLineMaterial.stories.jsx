4c8a13fdd1c62c553d53b1a36f5aa3afb94223a9
Di <didoesdigital+git@gmail.com>
2022-12-04 23:30:19 +1000

    Add Chromatic param for unstable story

--- src/components/Material/MultiLineMaterial.stories.jsx
+++ src/components/Material/MultiLineMaterial.stories.jsx
@@ -29,3 +29,12 @@ MultiLineMaterial.args = {
   userSettings: userSettings,
   currentPhraseID: 1,
 };
+MultiLineMaterial.parameters = {
+  chromatic: { delay: 300 },
+  // If the delay ☝️ doesn't work, try tweaking the diffThreshold:
+  // Chromatic’s default threshold is .063, which balances high visual accuracy with low false
+  // positives (for example, from artifacts like anti-aliasing).
+  // 0 is the most accurate. 1 is the least accurate.
+  // https://6262c53f521620003ac2ff49-ukmsdlppcb.chromatic.com/?path=/story/stories-diff-threshold-check--test-yours-out
+  // chromatic: { diffThreshold: 0.78 },
+};
