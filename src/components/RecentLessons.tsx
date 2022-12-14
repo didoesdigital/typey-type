9a477e0f42703cb3d5f019ded12f937333a0a9fa
Di <didoesdigital+git@gmail.com>
2022-12-14 20:40:18 +1000

    Refactor RecentLessons

--- src/components/RecentLessons.tsx
+++ src/components/RecentLessons.tsx
@@ -26,79 +26,77 @@ const RecentLessons = ({ recentLessonHistory, lessonIndex }: Props) => {
     recentLessonHistory[0] &&
     recentLessonHistory[0].path;
 
-  let recentLessons = null;
-
-  if (hasRecentLessons) {
-    const linkList = recentLessonHistory
-      .filter((recentLesson: any) =>
-        lessonIndex.find(
-          (lesson) =>
-            recentLesson.path.includes("/lessons/progress") ||
-            lesson.path ===
-              recentLesson.path.replace("/lessons", "") + "lesson.txt"
+  const linkList = hasRecentLessons
+    ? recentLessonHistory
+        .filter((recentLesson: any) =>
+          lessonIndex.find(
+            (lesson) =>
+              recentLesson.path.includes("/lessons/progress") ||
+              lesson.path ===
+                recentLesson.path.replace("/lessons", "") + "lesson.txt"
+          )
         )
-      )
-      .map((recentLesson: any) => {
-        let lesson = lessonIndex.find(
-          (lesson) =>
-            lesson.path ===
-            recentLesson.path.replace("/lessons", "") + "lesson.txt"
-        );
+        .map((recentLesson: any) => {
+          let lesson = lessonIndex.find(
+            (lesson) =>
+              lesson.path ===
+              recentLesson.path.replace("/lessons", "") + "lesson.txt"
+          );
 
-        if (recentLesson.path.includes("/lessons/progress")) {
-          let path = recentLesson.path.replace("/lessons", "");
-          let title = "Your words";
-          if (path.includes("memorised")) {
-            title = "Your memorised words";
+          if (recentLesson.path.includes("/lessons/progress")) {
+            let path = recentLesson.path.replace("/lessons", "");
+            let title = "Your words";
+            if (path.includes("memorised")) {
+              title = "Your memorised words";
+            }
+            if (path.includes("seen")) {
+              title = "Your revision words";
+            }
+
+            lesson = {
+              path: path,
+              title: title,
+            };
           }
-          if (path.includes("seen")) {
-            title = "Your revision words";
+
+          let studyType: Study = "practice";
+          // NOTE: does not check if studyType is legit
+          if (recentLesson && recentLesson.studyType) {
+            studyType = recentLesson.studyType;
           }
 
-          lesson = {
-            path: path,
-            title: title,
-          };
-        }
+          const studyTypeKey: keyof typeof PARAMS = `${studyType}Params`;
 
-        let studyType: Study = "practice";
-        // NOTE: does not check if studyType is legit
-        if (recentLesson && recentLesson.studyType) {
-          studyType = recentLesson.studyType;
-        }
+          return lesson ? (
+            <li className="unstyled-list-item mb1" key={lesson.path}>
+              <Link
+                to={
+                  "/lessons" +
+                  lesson.path
+                    .replace(/lesson\.txt$/, "")
+                    .replace(/\/{2,}/g, "/") +
+                  "?recent=1&" +
+                  PARAMS[studyTypeKey]
+                }
+                id={"ga--recent-lessons--" + lesson.path.replace(/[/.]/g, "-")}
+              >
+                {lesson.title}
+              </Link>
+            </li>
+          ) : undefined;
+        })
+        .filter(Boolean) // guard against undefined result from find
+        .reverse()
+    : undefined;
 
-        const studyTypeKey: keyof typeof PARAMS = `${studyType}Params`;
+  const recentLessons = hasRecentLessons ? (
+    <React.Fragment>
+      <h3>Recent lessons</h3>
+      <ul className="unstyled-list">{linkList}</ul>
+    </React.Fragment>
+  ) : undefined;
 
-        return lesson ? (
-          <li className="unstyled-list-item mb1" key={lesson.path}>
-            <Link
-              to={
-                "/lessons" +
-                lesson.path
-                  .replace(/lesson\.txt$/, "")
-                  .replace(/\/{2,}/g, "/") +
-                "?recent=1&" +
-                PARAMS[studyTypeKey]
-              }
-              id={"ga--recent-lessons--" + lesson.path.replace(/[/.]/g, "-")}
-            >
-              {lesson.title}
-            </Link>
-          </li>
-        ) : undefined;
-      })
-      .filter(Boolean) // guard against undefined result from find
-      .reverse();
-
-    recentLessons = (
-      <React.Fragment>
-        <h3>Recent lessons</h3>
-        <ul className="unstyled-list">{linkList}</ul>
-      </React.Fragment>
-    );
-  }
-
-  return hasRecentLessons ? recentLessons : null;
+  return recentLessons;
 };
 
 export default RecentLessons;
