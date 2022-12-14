3fa4f4901fb005d26c72dda47e70b48faf7220d0
Di <didoesdigital+git@gmail.com>
2022-12-14 19:54:04 +1000

    Convert class component to functional

--- src/components/RecentLessons.jsx
+++ src/components/RecentLessons.jsx
@@ -1,76 +1,91 @@
-import React, { Component } from 'react';
-import PARAMS from '../utils/params.js';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import PARAMS from "../utils/params.js";
+import { Link } from "react-router-dom";
 
-class RecentLessons extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasRecentLessons: false
+const RecentLessons = ({ recentLessonHistory, lessonIndex }) => {
+  const [hasRecentLessons, setHasRecentLessons] = useState(false);
+
+  useEffect(() => {
+    if (
+      recentLessonHistory &&
+      recentLessonHistory.length > 0 &&
+      recentLessonHistory[0] &&
+      recentLessonHistory[0].path
+    ) {
+      setHasRecentLessons(true);
     }
-  }
+  }, [recentLessonHistory]);
 
-  componentDidMount() {
-    let hasRecentLessons = false;
-    if (this.props.recentLessonHistory && this.props.recentLessonHistory.length > 0 && this.props.recentLessonHistory[0] && this.props.recentLessonHistory[0].path) {
-      hasRecentLessons = true;
-    }
-    this.setState({
-      hasRecentLessons: hasRecentLessons
-    });
-  }
+  let recentLessons = null;
 
-  render() {
-    let recentLessons = null;
+  if (hasRecentLessons) {
+    const linkList = recentLessonHistory
+      .filter((recentLesson) =>
+        lessonIndex.find(
+          (lesson) =>
+            recentLesson.path.includes("/lessons/progress") ||
+            lesson.path ===
+              recentLesson.path.replace("/lessons", "") + "lesson.txt"
+        )
+      )
+      .map((recentLesson) => {
+        let lesson = lessonIndex.find(
+          (lesson) =>
+            lesson.path ===
+            recentLesson.path.replace("/lessons", "") + "lesson.txt"
+        );
 
-    if (this.state.hasRecentLessons) {
-      const linkList = this.props.recentLessonHistory
-        .filter( recentLesson => this.props.lessonIndex.find(lesson => (recentLesson.path.includes("/lessons/progress") || lesson.path === recentLesson.path.replace("/lessons", "") + 'lesson.txt')))
-        .map( recentLesson => {
-          let lesson = this.props.lessonIndex.find(lesson => lesson.path === recentLesson.path.replace("/lessons", "") + 'lesson.txt');
-
-          if (recentLesson.path.includes("/lessons/progress")) {
-            let path = recentLesson.path.replace("/lessons", '');
-            let title = "Your words";
-            if (path.includes("memorised")) { title = "Your memorised words"; }
-            if (path.includes("seen")) { title = "Your revision words"; }
-
-            lesson = {
-              path: path,
-              title: title
-            }
+        if (recentLesson.path.includes("/lessons/progress")) {
+          let path = recentLesson.path.replace("/lessons", "");
+          let title = "Your words";
+          if (path.includes("memorised")) {
+            title = "Your memorised words";
+          }
+          if (path.includes("seen")) {
+            title = "Your revision words";
           }
 
-          let studyType = "practice";
-          // NOTE: does not check if studyType is legit
-          if (recentLesson && recentLesson.studyType) { studyType = recentLesson.studyType; }
+          lesson = {
+            path: path,
+            title: title,
+          };
+        }
 
-          return(
-            <li className="unstyled-list-item mb1" key={ lesson.path }>
-              <Link to={
-                "/lessons"
-                  + lesson.path
-                  .replace(/lesson\.txt$/,'')
-                  .replace(/\/{2,}/g,'/')
-                  + "?recent=1&"
-                  + PARAMS[studyType + "Params"]
-                  } id={'ga--recent-lessons--' + lesson.path.replace(/[/.]/g,'-')}>{lesson.title}</Link>
-            </li>
-          )
-        }).reverse();
+        let studyType = "practice";
+        // NOTE: does not check if studyType is legit
+        if (recentLesson && recentLesson.studyType) {
+          studyType = recentLesson.studyType;
+        }
 
-      recentLessons = (
-        <React.Fragment>
-          <h3>Recent lessons</h3>
-          <ul className="unstyled-list">{linkList}</ul>
-        </React.Fragment>
-      );
-    }
+        return (
+          <li className="unstyled-list-item mb1" key={lesson.path}>
+            <Link
+              to={
+                "/lessons" +
+                lesson.path
+                  .replace(/lesson\.txt$/, "")
+                  .replace(/\/{2,}/g, "/") +
+                "?recent=1&" +
+                PARAMS[studyType + "Params"]
+              }
+              id={"ga--recent-lessons--" + lesson.path.replace(/[/.]/g, "-")}
+            >
+              {lesson.title}
+            </Link>
+          </li>
+        );
+      })
+      .reverse();
 
-    return (
-      this.state.hasRecentLessons ? recentLessons : null
+    recentLessons = (
+      <React.Fragment>
+        <h3>Recent lessons</h3>
+        <ul className="unstyled-list">{linkList}</ul>
+      </React.Fragment>
     );
   }
-}
+
+  return hasRecentLessons ? recentLessons : null;
+};
 
 export default RecentLessons;
