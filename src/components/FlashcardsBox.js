4b073e2055622c72d0c4fce25a7e0106ba59b6c4
Di <didoesdigital+git@gmail.com>
2022-12-15 21:21:55 +1000

    Remove useless fragments

--- src/components/FlashcardsBox.js
+++ src/components/FlashcardsBox.js
@@ -28,80 +28,76 @@ class FlashcardsBox extends Component {
 
     if (flashcardsNextLesson !== undefined && !loadingLessonIndex) {
       flashcardsBoxContent = (
-        <React.Fragment>
-          <div className="bw-12 br-4 b--solid b--brand-primary p3 mb3">
-            <p className="text-right">
-              <strong>
-                {flashcardsNextLesson.linkTitle === "Error"
-                  ? "Unable to load flashcards"
-                  : flashcardsNextLesson.linkTitle}
-              </strong>
-            </p>
-            <p className="text-right de-emphasized">
-              {flashcardsNextLesson.lastSeen === defaultTimestamp
-                ? "New flashcards"
-                : `Seen ${flashcardsTimeAgo} ago`}
-            </p>
-            <div className="flex flex-wrap justify-end">
-              <button
-                onClick={this.moreFlashcards.bind(this)}
-                id={skipButtonId || "js-flashcards-skip-button"}
-                className="button button--secondary mr2 pl3 pr3"
-              >
-                Skip
-              </button>
-              <div className="text-right">
-                {flashcardsNextLesson.linkTitle === "Error" ? (
-                  <a
-                    href="."
-                    className="link-button dib"
-                    style={{ lineHeight: 2 }}
-                  >
-                    Refresh
-                  </a>
-                ) : (
-                  <Link
-                    onClick={startFlashcards}
-                    to={flashcardsNextLesson.link}
-                    className="link-button dib"
-                    style={{ lineHeight: 2 }}
-                  >
-                    Study
-                  </Link>
-                )}
-              </div>
-            </div>
-          </div>
-        </React.Fragment>
-      );
-    } else {
-      flashcardsBoxContent = (
-        <React.Fragment>
-          <div className="bw-12 br-4 b--solid b--brand-primary p3 mb3">
-            <p className="text-right">
-              <strong>Loading…</strong>
-            </p>
-            <p className="text-right de-emphasized"></p>
-            <div className="flex flex-wrap justify-end">
-              <button
-                onClick={this.moreFlashcards.bind(this)}
-                id={skipButtonId || "js-flashcards-skip-button"}
-                className="button button--secondary mr2 pl3 pr3"
-              >
-                Skip
-              </button>
-              <div className="text-right">
-                <button
-                  disabled
+        <div className="bw-12 br-4 b--solid b--brand-primary p3 mb3">
+          <p className="text-right">
+            <strong>
+              {flashcardsNextLesson.linkTitle === "Error"
+                ? "Unable to load flashcards"
+                : flashcardsNextLesson.linkTitle}
+            </strong>
+          </p>
+          <p className="text-right de-emphasized">
+            {flashcardsNextLesson.lastSeen === defaultTimestamp
+              ? "New flashcards"
+              : `Seen ${flashcardsTimeAgo} ago`}
+          </p>
+          <div className="flex flex-wrap justify-end">
+            <button
+              onClick={this.moreFlashcards.bind(this)}
+              id={skipButtonId || "js-flashcards-skip-button"}
+              className="button button--secondary mr2 pl3 pr3"
+            >
+              Skip
+            </button>
+            <div className="text-right">
+              {flashcardsNextLesson.linkTitle === "Error" ? (
+                <a
+                  href="."
                   className="link-button dib"
                   style={{ lineHeight: 2 }}
                 >
-                  Loading…
-                </button>
-              </div>
+                  Refresh
+                </a>
+              ) : (
+                <Link
+                  onClick={startFlashcards}
+                  to={flashcardsNextLesson.link}
+                  className="link-button dib"
+                  style={{ lineHeight: 2 }}
+                >
+                  Study
+                </Link>
+              )}
             </div>
           </div>
-        </React.Fragment>
+        </div>
+      );
+    } else {
+      flashcardsBoxContent = (
+        <div className="bw-12 br-4 b--solid b--brand-primary p3 mb3">
+          <p className="text-right">
+            <strong>Loading…</strong>
+          </p>
+          <p className="text-right de-emphasized"></p>
+          <div className="flex flex-wrap justify-end">
+            <button
+              onClick={this.moreFlashcards.bind(this)}
+              id={skipButtonId || "js-flashcards-skip-button"}
+              className="button button--secondary mr2 pl3 pr3"
+            >
+              Skip
+            </button>
+            <div className="text-right">
+              <button
+                disabled
+                className="link-button dib"
+                style={{ lineHeight: 2 }}
+              >
+                Loading…
+              </button>
+            </div>
+          </div>
+        </div>
       );
     }
 
