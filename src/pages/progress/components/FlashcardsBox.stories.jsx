5ac4d05f75fd32465e8e5fcb469ec129691952b1
Di <didoesdigital+git@gmail.com>
2022-12-15 21:39:41 +1000

    Add FlashcardsBox.stories.jsx

---
+++ src/pages/progress/components/FlashcardsBox.stories.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import FlashcardsBox from "./FlashcardsBox";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Progress/FlashcardsBox",
+  component: FlashcardsBox,
+};
+
+const Template = (args) => {
+  return (
+    <div className="mw-368">
+      <FlashcardsBox {...args} />
+    </div>
+  );
+};
+
+export const FlashcardsBoxStory = Template.bind({});
+FlashcardsBoxStory.args = {
+  flashcardsNextLesson: {
+    linkTitle: "Top 1000 words flashcards",
+    lastSeen: 1558144862000,
+    link: "http://localhost:3000/typey-type/lessons/drills/top-1000-words/flashcards",
+  },
+  loadingLessonIndex: false,
+  onSkip: () => undefined,
+  skipButtonId: "js-test-flashcards-button-in-storybook",
+  startFlashcards: () => undefined,
+};
