d133340b0a4857365133de614c137449cf11e470
Di <didoesdigital+git@gmail.com>
2022-12-16 22:39:27 +1000

    Add TodaysEffortsOrGoals.tsx

---
+++ src/pages/progress/components/TodaysEffortsOrGoals.tsx
@@ -0,0 +1,258 @@
+import React from "react";
+import NumericInput from "react-numeric-input";
+import { ReactComponent as AlertRobot } from "../../../images/AlertRobot.svg";
+import { ReactComponent as BoredRobot } from "../../../images/BoredRobot.svg";
+import { ReactComponent as HappyRobot } from "../../../images/HappyRobot.svg";
+import type { MetWords } from "../../../types";
+
+type Props = {
+  cancelSetGoals: () => void;
+  handleNewWordsGoalInputChange: () => void;
+  handleOldWordsGoalInputChange: () => void;
+  newWordsGoalMet: boolean;
+  newWordsGoalUnveiled: boolean;
+  oldWordsGoalMet: boolean;
+  oldWordsGoalUnveiled: boolean;
+  revealCompletedGoals: () => void;
+  saveGoals: () => void;
+  showSetGoalsForm: boolean;
+  showSetGoalsFormFn: () => void;
+  startingMetWordsToday: MetWords;
+  todayNewWordCount: number;
+  todayOldWordCount: number;
+  userGoalInputNewWords: number;
+  userGoalInputOldWords: number;
+  userGoals: {
+    oldWords: number;
+    newWords: number;
+  };
+};
+
+const renderTodaysEffortsGoals = (
+  userGoalsWords: number,
+  todayWordCount: number
+) => {
+  return (
+    <React.Fragment>
+      {userGoalsWords}
+      {userGoalsWords <= todayWordCount ? (
+        <>
+          <span aria-hidden="true"> •</span> Done!
+        </>
+      ) : null}
+    </React.Fragment>
+  );
+};
+
+const TodaysEffortsOrGoals = ({
+  cancelSetGoals,
+  handleNewWordsGoalInputChange,
+  handleOldWordsGoalInputChange,
+  newWordsGoalMet,
+  newWordsGoalUnveiled,
+  oldWordsGoalMet,
+  oldWordsGoalUnveiled,
+  revealCompletedGoals,
+  saveGoals,
+  showSetGoalsForm,
+  showSetGoalsFormFn,
+  startingMetWordsToday,
+  todayNewWordCount,
+  todayOldWordCount,
+  userGoalInputNewWords,
+  userGoalInputOldWords,
+  userGoals,
+}: Props) => {
+  const grabStyle = function () {
+    return false;
+  };
+
+  let todaysEffortsOrGoals;
+  if (showSetGoalsForm) {
+    todaysEffortsOrGoals = (
+      <React.Fragment>
+        <form onSubmit={saveGoals}>
+          <div className="pt4 pb4">
+            <div className="mb3">
+              <label
+                className="pb1"
+                id="js-first-interactive-form-field-element"
+                htmlFor="userGoalInputOldWords"
+              >
+                Old words goal
+              </label>
+              <NumericInput
+                autoCapitalize="off"
+                autoComplete="off"
+                autoCorrect="off"
+                autoFocus={false}
+                className="form-control w-100"
+                disabled={!showSetGoalsForm}
+                id="userGoalInputOldWords"
+                max={10000}
+                min={1}
+                name="userGoalInputOldWords"
+                onChange={handleOldWordsGoalInputChange}
+                precision={0}
+                spellCheck="false"
+                step={1}
+                style={grabStyle()}
+                type="number"
+                value={userGoalInputOldWords}
+                snap
+              />
+              <div className="mt1 text-small de-emphasized">
+                (50–200 recommended)
+              </div>
+            </div>
+            <div className="mb3">
+              <label className="pb1" htmlFor="userGoalInputNewWords">
+                New words goal
+              </label>
+              <NumericInput
+                autoCapitalize="off"
+                autoComplete="off"
+                autoCorrect="off"
+                autoFocus={false}
+                className="form-control w-100"
+                disabled={!showSetGoalsForm}
+                id="userGoalInputNewWords"
+                max={10000}
+                min={1}
+                name="userGoalInputNewWords"
+                onChange={handleNewWordsGoalInputChange}
+                precision={0}
+                spellCheck="false"
+                step={1}
+                style={grabStyle()}
+                type="number"
+                value={userGoalInputNewWords}
+                snap
+              />
+              <div className="mt1 text-small de-emphasized">
+                (5–40 recommended)
+              </div>
+            </div>
+            <div className="flex flex-wrap justify-end">
+              <button
+                onClick={cancelSetGoals}
+                className="button button--secondary mr2 dib"
+              >
+                Cancel
+              </button>
+              <button onClick={saveGoals} className="button mr2 dib">
+                Save goals
+              </button>
+            </div>
+          </div>
+        </form>
+      </React.Fragment>
+    );
+  } else if (
+    (oldWordsGoalMet && !oldWordsGoalUnveiled) ||
+    (newWordsGoalMet && !newWordsGoalUnveiled)
+  ) {
+    todaysEffortsOrGoals = (
+      <React.Fragment>
+        <div className="inline-flex flex-column items-center pt4 pb4 bb b--brand-primary-tint w-100">
+          <div className="todays-effort-reveal-robot">
+            <AlertRobot />
+          </div>
+          You completed a goal!
+          <button
+            onClick={revealCompletedGoals.bind(
+              this,
+              oldWordsGoalMet && !oldWordsGoalUnveiled,
+              newWordsGoalMet && !newWordsGoalUnveiled
+            )}
+            className="button button--secondary mt3 dib"
+          >
+            Reveal
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  } else {
+    let yourOldWordsGoal = renderTodaysEffortsGoals(
+      userGoals.oldWords,
+      todayOldWordCount
+    );
+    let yourNewWordsGoal = renderTodaysEffortsGoals(
+      userGoals.newWords,
+      todayNewWordCount
+    );
+    let todaysEffortsOldGoalsRow: JSX.Element | null = (
+      <div className="inline-flex items-center pt4 pb4 bb b--brand-primary-tint w-100">
+        <div className="flex todays-effort-goal-robot">
+          {userGoals.oldWords <= todayOldWordCount ? (
+            <HappyRobot />
+          ) : (
+            <BoredRobot />
+          )}
+        </div>
+        <div className="stat__number stat__number--display mr1">
+          {todayOldWordCount}
+        </div>
+        <div>
+          Old {todayOldWordCount !== 1 ? "words" : "word"}
+          <br />
+          <span className="text-small">Your goal: {yourOldWordsGoal}</span>
+        </div>
+      </div>
+    );
+
+    if (
+      !startingMetWordsToday ||
+      Object.keys(startingMetWordsToday).length < 15
+    ) {
+      todaysEffortsOldGoalsRow = null;
+    }
+
+    todaysEffortsOrGoals = (
+      <React.Fragment>
+        {todaysEffortsOldGoalsRow}
+        <div className="inline-flex items-center pt4 pb4 bb b--brand-primary-tint w-100">
+          <div className="flex todays-effort-goal-robot">
+            {userGoals.newWords <= todayNewWordCount ? (
+              <HappyRobot />
+            ) : (
+              <BoredRobot />
+            )}
+          </div>
+          <div className="stat__number stat__number--display mr1">
+            {todayNewWordCount}
+          </div>
+          <div>
+            New {todayNewWordCount !== 1 ? "words" : "word"}
+            <br />
+            <span className="text-small">Your goal: {yourNewWordsGoal}</span>
+          </div>
+        </div>
+        <div className="text-right">
+          <button
+            id="js-set-goals-button"
+            onClick={showSetGoalsFormFn}
+            className="button button--secondary mt3 dib"
+          >
+            Set goals
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <>
+      <h3
+        className="mt0 mb0 pt5 pb1 bb b--brand-primary-tint"
+        id="js-todays-efforts"
+        tabIndex={-1}
+      >
+        Today’s efforts
+      </h3>
+      {todaysEffortsOrGoals}
+    </>
+  );
+};
+
+export default TodaysEffortsOrGoals;
