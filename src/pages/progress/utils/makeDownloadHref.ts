990bd57477fe55bb4f6b682ef8582c7eab325e4e
Di <didoesdigital+git@gmail.com>
2022-12-17 16:57:48 +1000

    Reduce makeDownloadHref

--- src/pages/progress/utils/makeDownloadHref.ts
+++ src/pages/progress/utils/makeDownloadHref.ts
@@ -1,13 +1,9 @@
-const makeDownloadHref = (json: JSON) => {
-  if (Blob !== undefined) {
-    return URL.createObjectURL(
-      new Blob([JSON.stringify(json)], { type: "text/json" })
-    );
-  } else {
-    return (
-      "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json))
-    );
-  }
-};
+const makeDownloadHref = (json: JSON) =>
+  Blob !== undefined
+    ? URL.createObjectURL(
+        new Blob([JSON.stringify(json)], { type: "text/json" })
+      )
+    : "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(json));
 
 export default makeDownloadHref;
