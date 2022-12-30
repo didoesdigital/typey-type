f49ae51062da74ac0e8b5c58a766b2bccf470130
Di <didoesdigital+git@gmail.com>
2022-12-31 09:31:02 +1000

    Start building ProgressSummaryAndLinks component

---
+++ src/pages/progress/components/ProgressSummaryAndLinks.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  yourWordCount: number;
+  yourMemorisedWordCount: number;
+  yourSeenWordCount: number;
+};
+
+const ProgressSummaryAndLinks = ({
+  yourSeenWordCount,
+  yourMemorisedWordCount,
+  yourWordCount,
+}: Props) => {
+  return <></>;
+};
+
+export default ProgressSummaryAndLinks;
