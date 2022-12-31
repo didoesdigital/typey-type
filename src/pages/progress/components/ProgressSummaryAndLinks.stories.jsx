9ec7d4dc8ffd20c233c56f47e7a8eed77e6aa689
Di <didoesdigital+git@gmail.com>
2022-12-31 11:04:29 +1000

    Add ProgressSummaryAndLinks.stories.jsx

---
+++ src/pages/progress/components/ProgressSummaryAndLinks.stories.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProgressSummaryAndLinks from "./ProgressSummaryAndLinks";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Progress/SummaryAndLinks",
+  component: ProgressSummaryAndLinks,
+  id: "progress-summary", // permalink
+};
+
+const Template = (args) => {
+  return (
+    <div className="p3 mx-auto mw-1024">
+      <ProgressSummaryAndLinks {...args} />
+    </div>
+  );
+};
+
+export const ProgressSummaryAndLinksStory = Template.bind({});
+ProgressSummaryAndLinksStory.storyName = "Summary and links";
+ProgressSummaryAndLinksStory.args = {
+  yourMemorisedWordCount: 1,
+  yourSeenWordCount: 1,
+  yourWordCount: 2,
+};
