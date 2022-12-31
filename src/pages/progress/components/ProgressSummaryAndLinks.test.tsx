f70a8d3fb6f47f2f57d15cebb56842e94e16f0f4
Di <didoesdigital+git@gmail.com>
2022-12-31 16:02:59 +1000

    Expand tests for ProgressSummaryAndLinks

--- src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
+++ src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
@@ -81,4 +81,110 @@ describe("progress summary and links", () => {
     expect(screen.getByText(/Revise/i)).toBeInTheDocument();
     expect(screen.getByTestId("test-wrapper")).not.toHaveTextContent("Drill");
   });
+
+  it("has 1 seen and 0 memorised", () => {
+    render(
+      <Router basename="/typey-type">
+        <Route>
+          <div data-testid="test-wrapper">
+            <ProgressSummaryAndLinks
+              metWords={{ "one": 1 }}
+              restartConfetti={() => undefined}
+              yourMemorisedWordCount={0}
+              yourSeenWordCount={1}
+            />
+          </div>
+        </Route>
+      </Router>
+    );
+
+    expect(screen.getByTestId("test-wrapper")).not.toHaveTextContent(
+      "Practice"
+    );
+    expect(screen.getByTestId("test-wrapper")).not.toHaveTextContent("Drill");
+    expect(screen.getByText(/Revise/i)).toBeInTheDocument();
+    expect(screen.getByText(/Discover/i)).toBeInTheDocument();
+  });
+
+  it("has 1 seen and 1 memorised", () => {
+    render(
+      <Router basename="/typey-type">
+        <Route>
+          <div data-testid="test-wrapper">
+            <ProgressSummaryAndLinks
+              metWords={{ "one": 1, "two": 30 }}
+              restartConfetti={() => undefined}
+              yourMemorisedWordCount={1}
+              yourSeenWordCount={1}
+            />
+          </div>
+        </Route>
+      </Router>
+    );
+
+    expect(screen.getByTestId("test-wrapper")).not.toHaveTextContent(
+      "Practice"
+    );
+    expect(screen.getByText(/Drill/i)).toBeInTheDocument();
+    expect(screen.getByText(/Revise/i)).toBeInTheDocument();
+    expect(screen.getByText(/Discover/i)).toBeInTheDocument();
+  });
+
+  it("has 0 seen and 1 memorised", () => {
+    render(
+      <Router basename="/typey-type">
+        <Route>
+          <div data-testid="test-wrapper">
+            <ProgressSummaryAndLinks
+              metWords={{ "memorised": 30 }}
+              restartConfetti={() => undefined}
+              yourMemorisedWordCount={30}
+              yourSeenWordCount={0}
+            />
+          </div>
+        </Route>
+      </Router>
+    );
+
+    expect(screen.getByTestId("test-wrapper")).not.toHaveTextContent(
+      "Practice"
+    );
+    expect(screen.getByText(/Drill/i)).toBeInTheDocument();
+    expect(screen.getByTestId("test-wrapper")).not.toHaveTextContent("Revise");
+    expect(screen.getByText(/Discover/i)).toBeInTheDocument();
+  });
+
+  it("has 10 seen and 1 memorised", () => {
+    render(
+      <Router basename="/typey-type">
+        <Route>
+          <div data-testid="test-wrapper">
+            <ProgressSummaryAndLinks
+              metWords={{
+                "seen1": 1,
+                "seen2": 1,
+                "seen3": 1,
+                "seen4": 1,
+                "seen5": 1,
+                "seen6": 1,
+                "seen7": 1,
+                "seen8": 1,
+                "seen9": 1,
+                "seen10": 1,
+                "memorised": 30,
+              }}
+              restartConfetti={() => undefined}
+              yourMemorisedWordCount={1}
+              yourSeenWordCount={10}
+            />
+          </div>
+        </Route>
+      </Router>
+    );
+
+    expect(screen.getByText(/Practice/i)).toBeInTheDocument();
+    expect(screen.getByText(/Drill/i)).toBeInTheDocument();
+    expect(screen.getByText(/Revise/i)).toBeInTheDocument();
+    expect(screen.getByText(/Discover/i)).toBeInTheDocument();
+  });
 });
