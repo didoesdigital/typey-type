bd2414e7febb42cf0b726c828a2f8184d6052cb2
Di <didoesdigital+git@gmail.com>
2022-12-31 11:30:35 +1000

    Fix test

--- src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
+++ src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import ProgressSummaryAndLinks from "./ProgressSummaryAndLinks";
 
 test("renders progress summary and links", () => {
   render(
-    <ProgressSummaryAndLinks
-      yourMemorisedWordCount={1}
-      yourSeenWordCount={1}
-      yourWordCount={2}
-    />
+    <Router basename="/typey-type">
+      <Route>
+        <ProgressSummaryAndLinks
+          yourMemorisedWordCount={1}
+          yourSeenWordCount={1}
+          yourWordCount={2}
+        />
+      </Route>
+    </Router>
   );
-  const linkElement = screen.getByText(/test progress summary and links/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const textElement = screen.getByText(/successfully typed/i);
+  expect(textElement).toBeInTheDocument();
 });
