599d72eddd521755da3cabb47b8aad539c569d61
Di <didoesdigital+git@gmail.com>
2022-12-31 19:34:37 +1000

    Format progress summary with thousands separator

--- src/pages/progress/components/ProgressSummaryCompleted.tsx
+++ src/pages/progress/components/ProgressSummaryCompleted.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProgressLessonLinks from "./ProgressLessonLinks";
+import { format } from "d3-format";
 
 type CompletedProps = {
   restartConfetti: (
@@ -25,13 +26,14 @@ const ProgressSummaryCompleted = ({
           <>
             Woohoo! You rock! What a magnificent effort to memorise 10,000
             words. You are an expert stenographer now! You’ve successfully typed{" "}
-            {yourWordCount} words without misstrokes. It’s time to{" "}
+            {format(",")(yourWordCount)} words. It’s time to{" "}
           </>
         ) : (
           <>
-            Woohoo! You rock! You’ve successfully typed {yourWordCount} words
-            without misstrokes. You are an accomplished stenographer now! You’ve
-            completed 100% of 10,000 words. It’s time to{" "}
+            Woohoo! You rock! You’ve successfully typed{" "}
+            {format(",")(yourWordCount)} words. You are an accomplished
+            stenographer now! You’ve completed 100% of 10,000 words. It’s time
+            to{" "}
           </>
         )}
         <button
