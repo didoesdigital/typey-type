fd51d9699b3f6047dd2bb5068057393f4da93461
Di <didoesdigital+git@gmail.com>
2022-12-31 11:46:19 +1000

    Expand ProgressSummaryAndLinks tests

--- src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
+++ src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
@@ -3,19 +3,57 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import ProgressSummaryAndLinks from "./ProgressSummaryAndLinks";
 
-test("renders progress summary and links", () => {
-  render(
-    <Router basename="/typey-type">
-      <Route>
-        <ProgressSummaryAndLinks
-          yourMemorisedWordCount={1}
-          yourSeenWordCount={1}
-          yourWordCount={2}
-        />
-      </Route>
-    </Router>
-  );
+describe("progress summary and links", () => {
+  it("renders", () => {
+    render(
+      <Router basename="/typey-type">
+        <Route>
+          <ProgressSummaryAndLinks
+            yourMemorisedWordCount={1}
+            yourSeenWordCount={1}
+            yourWordCount={2}
+          />
+        </Route>
+      </Router>
+    );
+    const textElement = screen.getByText(/successfully typed/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-  const textElement = screen.getByText(/successfully typed/i);
-  expect(textElement).toBeInTheDocument();
+  it("celebrates finishing Typey Type", () => {
+    render(
+      <Router basename="/typey-type">
+        <Route>
+          <ProgressSummaryAndLinks
+            yourMemorisedWordCount={10000}
+            yourSeenWordCount={10000}
+            yourWordCount={20000}
+          />
+        </Route>
+      </Router>
+    );
+    const textElement = screen.getByText(/You rock!/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  it("hides Discover link when Typey Type is finished", () => {
+    render(
+      <Router basename="/typey-type">
+        <Route>
+          <div data-testid="test-wrapper">
+            <ProgressSummaryAndLinks
+              yourMemorisedWordCount={10000}
+              yourSeenWordCount={10000}
+              yourWordCount={20000}
+            />
+          </div>
+        </Route>
+      </Router>
+    );
+
+    expect(screen.getByText(/Practice/i)).toBeInTheDocument();
+    expect(screen.getByTestId("test-wrapper")).not.toHaveTextContent(
+      "Discover"
+    );
+  });
 });
