344b2324eb81cd03878b32d5d89b89e961ba539e
Di <didoesdigital+git@gmail.com>
2023-01-08 11:31:21 +1000

    Add LessonsIndex story

---
+++ src/pages/lessons/LessonsIndex.stories.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import LessonsIndex from "./LessonsIndex";
+import Zipper from "../../utils/zipper";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Pages/LessonsIndex",
+  component: LessonsIndex,
+};
+
+const lessonIndex = [
+  {
+    title: "Test lesson",
+    subtitle: "",
+    category: "Fundamentals",
+    subcategory: "",
+    path: "/lessons/fundamentals/test/",
+  },
+];
+
+const sourceMaterial = [
+  {
+    phrase: "test",
+    stroke: "TEFT",
+  },
+];
+
+const customLesson = {
+  sourceMaterial: sourceMaterial,
+  presentedMaterial: [
+    {
+      phrase: "test",
+      stroke: "TEFT",
+    },
+  ],
+  settings: {
+    ignoredChars: "",
+  },
+  title: "Test lesson",
+  subtitle: "",
+  newPresentedMaterial: new Zipper(sourceMaterial),
+  path: "/lessons/fundamentals/test/",
+};
+
+const Template = (args) => {
+  return (
+    <LessonsIndex
+      lessonIndex={lessonIndex}
+      customLesson={customLesson}
+      setAnnouncementMessage={() => console.log("announce")}
+      stopLesson={() => console.log("stop lesson")}
+      match={{
+        isExact: true,
+        params: {},
+        path: "/dictionaries",
+        url: "/dictionaries/",
+      }}
+      {...args}
+    />
+  );
+};
+
+export const LessonsIndexStory = Template.bind({});
