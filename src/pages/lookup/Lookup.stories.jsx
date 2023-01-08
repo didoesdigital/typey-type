c3ba50f0cfe8ef4adca08fe14288f980cdab10a7
Di <didoesdigital+git@gmail.com>
2023-01-08 10:27:44 +1000

    Add Lookup stories

---
+++ src/pages/lookup/Lookup.stories.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Lookup from "./Lookup";
+import userSettings from "../../stories/fixtures/userSettings";
+import { within, userEvent } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Pages/Lookup",
+  component: Lookup,
+};
+
+const globalLookupDictionary = new Map([
+  ["huh", [["H*U", "typey:typey-type.json"]]],
+  ["gonna", [["TKPW*G", "typey:typey-type.json"]]],
+]);
+
+const Template = (args) => (
+  <Lookup
+    fetchAndSetupGlobalDict={() => Promise.resolve(true)}
+    globalLookupDictionary={globalLookupDictionary}
+    globalLookupDictionaryLoaded={true}
+    setAnnouncementMessage={() => console.log("announce")}
+    lookupTerm={undefined}
+    userSettings={userSettings}
+    globalUserSettings={{}}
+    personalDictionaries={{ dictionariesNamesAndContents: null }}
+    updateGlobalLookupDictionary={() => undefined}
+    updatePersonalDictionaries={() => undefined}
+    stenohintsonthefly={true}
+    {...args}
+  />
+);
+
+export const LookupStory = Template.bind({});
+
+export const LookupFromURLStory = Template.bind({});
+LookupFromURLStory.args = {
+  lookupTerm: "a phrase that is not in any dictionary",
+};
+
+export const LookupSearchStory = Template.bind({});
+LookupSearchStory.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  userEvent.type(
+    await within(canvasElement).getByLabelText("Enter words to look up"),
+    "huh"
+  );
+
+  await expect(canvas.getByTestId("lookup-page-contents")).not.toHaveClass(
+    "No results found"
+  );
+
+  await expect(canvas.getByTestId("lookup-page-contents")).toHaveTextContent(
+    "typey-type.json"
+  );
+};
