5171ffd4769a097264cebfcf6e5243e798f37b53
Di <didoesdigital+git@gmail.com>
2023-01-08 11:57:31 +1000

    Extract fixture from story

--- src/pages/progress/components/LessonsProgress.stories.jsx
+++ src/pages/progress/components/LessonsProgress.stories.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LessonsProgress from "./LessonsProgress";
+import lessonIndex from "../../../stories/fixtures/lessonIndex";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -19,87 +20,6 @@ export default {
     ),
   ],
 };
-
-const testLessonIndex = [
-  {
-    title: "Introduction",
-    subtitle: "",
-    category: "Fundamentals",
-    subcategory: "",
-    path: "/fundamentals/introduction/lesson.txt",
-    wordCount: 52,
-    suggestedNext:
-      "/fundamentals/one-syllable-words-with-simple-keys/lesson.txt",
-    overview: "/fundamentals/introduction/lesson-overview.html",
-  },
-  {
-    category: "Fundamentals",
-    overview:
-      "/fundamentals/one-syllable-words-with-simple-keys/lesson-overview.html",
-    path: "/fundamentals/one-syllable-words-with-simple-keys/lesson.txt",
-    subcategory: "",
-    subtitle: "",
-    suggestedNext:
-      "/fundamentals/one-syllable-words-with-more-consonants/lesson.txt",
-    title: "One-syllable words with simple keys",
-    wordCount: 164,
-  },
-  {
-    category: "Fundamentals",
-    overview:
-      "/fundamentals/one-syllable-words-with-more-consonants/lesson-overview.html",
-    path: "/fundamentals/one-syllable-words-with-more-consonants/lesson.txt",
-    subcategory: "",
-    subtitle: "",
-    suggestedNext:
-      "/fundamentals/one-syllable-words-with-f-as-v-or-s/lesson.txt",
-    title: "One-syllable words with more consonants",
-    wordCount: 96,
-  },
-  {
-    category: "Fundamentals",
-    overview:
-      "/fundamentals/one-syllable-words-with-f-as-v-or-s/lesson-overview.html",
-    path: "/fundamentals/one-syllable-words-with-f-as-v-or-s/lesson.txt",
-    subcategory: "",
-    subtitle: "",
-    suggestedNext:
-      "/fundamentals/one-syllable-words-with-unstressed-vowels/lesson.txt",
-    title: "One-syllable words with F as V or S",
-    wordCount: 11,
-  },
-  {
-    category: "Fundamentals",
-    overview:
-      "/fundamentals/multi-syllable-words-with-suffixes/lesson-overview.html",
-    path: "/fundamentals/multi-syllable-words-with-suffixes/lesson.txt",
-    subcategory: "",
-    subtitle: "",
-    suggestedNext:
-      "/fundamentals/multi-syllable-words-with-doubled-consonant-letters/lesson.txt",
-    title: "Multi-syllable words with suffixes",
-    wordCount: 1375,
-  },
-  {
-    title: "Proverbial phrases starting with E",
-    subtitle: "",
-    category: "Stories",
-    subcategory: "Proverbial phrases",
-    path: "/stories/proverbial-phrases/proverbial-phrases-starting-with-e/lesson.txt",
-    wordCount: 165,
-    suggestedNext:
-      "/stories/proverbial-phrases/proverbial-phrases-starting-with-f/lesson.txt",
-  },
-  {
-    title: "The Two Crabs",
-    subtitle: "",
-    category: "Stories",
-    subcategory: "Aesop’s Fables",
-    path: "/stories/fables/the-two-crabs/lesson.txt",
-    wordCount: 62,
-    suggestedNext: "/stories/fables/the-two-fellows-and-the-bear/lesson.txt",
-  },
-];
 
 const fakePUBLIC_URL = "."; // should be '/typey-type' but that requires setting the Storybook environment variable for PUBLIC_URL and that in turn breaks other stories
 
@@ -144,7 +64,7 @@ const Template = (args) => (
 export const LessonsProgressStory = Template.bind({});
 LessonsProgressStory.storyName = "Lessons progress";
 LessonsProgressStory.args = {
-  lessonIndex: testLessonIndex,
+  lessonIndex: lessonIndex,
   lessonsProgress: testLessonsProgress,
   setAnnouncementMessage: () => undefined,
 };
---
+++ src/stories/fixtures/lessonIndex.js
@@ -0,0 +1,82 @@
+const lessonIndex = [
+  {
+    title: "Introduction",
+    subtitle: "",
+    category: "Fundamentals",
+    subcategory: "",
+    path: "/fundamentals/introduction/lesson.txt",
+    wordCount: 52,
+    suggestedNext:
+      "/fundamentals/one-syllable-words-with-simple-keys/lesson.txt",
+    overview: "/fundamentals/introduction/lesson-overview.html",
+  },
+  {
+    category: "Fundamentals",
+    overview:
+      "/fundamentals/one-syllable-words-with-simple-keys/lesson-overview.html",
+    path: "/fundamentals/one-syllable-words-with-simple-keys/lesson.txt",
+    subcategory: "",
+    subtitle: "",
+    suggestedNext:
+      "/fundamentals/one-syllable-words-with-more-consonants/lesson.txt",
+    title: "One-syllable words with simple keys",
+    wordCount: 164,
+  },
+  {
+    category: "Fundamentals",
+    overview:
+      "/fundamentals/one-syllable-words-with-more-consonants/lesson-overview.html",
+    path: "/fundamentals/one-syllable-words-with-more-consonants/lesson.txt",
+    subcategory: "",
+    subtitle: "",
+    suggestedNext:
+      "/fundamentals/one-syllable-words-with-f-as-v-or-s/lesson.txt",
+    title: "One-syllable words with more consonants",
+    wordCount: 96,
+  },
+  {
+    category: "Fundamentals",
+    overview:
+      "/fundamentals/one-syllable-words-with-f-as-v-or-s/lesson-overview.html",
+    path: "/fundamentals/one-syllable-words-with-f-as-v-or-s/lesson.txt",
+    subcategory: "",
+    subtitle: "",
+    suggestedNext:
+      "/fundamentals/one-syllable-words-with-unstressed-vowels/lesson.txt",
+    title: "One-syllable words with F as V or S",
+    wordCount: 11,
+  },
+  {
+    category: "Fundamentals",
+    overview:
+      "/fundamentals/multi-syllable-words-with-suffixes/lesson-overview.html",
+    path: "/fundamentals/multi-syllable-words-with-suffixes/lesson.txt",
+    subcategory: "",
+    subtitle: "",
+    suggestedNext:
+      "/fundamentals/multi-syllable-words-with-doubled-consonant-letters/lesson.txt",
+    title: "Multi-syllable words with suffixes",
+    wordCount: 1375,
+  },
+  {
+    title: "Proverbial phrases starting with E",
+    subtitle: "",
+    category: "Stories",
+    subcategory: "Proverbial phrases",
+    path: "/stories/proverbial-phrases/proverbial-phrases-starting-with-e/lesson.txt",
+    wordCount: 165,
+    suggestedNext:
+      "/stories/proverbial-phrases/proverbial-phrases-starting-with-f/lesson.txt",
+  },
+  {
+    title: "The Two Crabs",
+    subtitle: "",
+    category: "Stories",
+    subcategory: "Aesop’s Fables",
+    path: "/stories/fables/the-two-crabs/lesson.txt",
+    wordCount: 62,
+    suggestedNext: "/stories/fables/the-two-fellows-and-the-bear/lesson.txt",
+  },
+];
+
+export default lessonIndex;
