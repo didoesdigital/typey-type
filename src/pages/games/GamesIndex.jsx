29bb22185b3f026cb50d54ee53d138fc95c936f4
Di <didoesdigital+git@gmail.com>
2023-01-09 15:24:02 +1000

    Add Subheader to GamesIndex

--- src/pages/games/GamesIndex.jsx
+++ src/pages/games/GamesIndex.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as RaverRobot } from "../../images/RaverRobot.svg";
 import { ReactComponent as AussieRobot } from "../../images/AussieRobot.svg";
 import { ReactComponent as ThinkingRobot } from "../../images/ThinkingRobot.svg";
 import { ReactComponent as MischievousRobot } from "../../images/MischievousRobot.svg";
+import Subheader from "../../components/Subheader";
 
 export default function GamesIndex() {
   const mainHeading = useRef(null);
@@ -15,17 +16,15 @@ export default function GamesIndex() {
 
   return (
     <main id="main">
-      <div className="subheader">
-        <div className="flex items-baseline mx-auto mw-1920 justify-between px3 py2">
-          <div className="flex mr1 self-center">
-            <header className="flex items-center min-h-40">
-              <h2 ref={mainHeading} tabIndex="-1">
-                Games
-              </h2>
-            </header>
-          </div>
+      <Subheader>
+        <div className="flex mr1 self-center">
+          <header className="flex items-center min-h-40">
+            <h2 ref={mainHeading} tabIndex="-1">
+              Games
+            </h2>
+          </header>
         </div>
-      </div>
+      </Subheader>
       <div className="p3 mx-auto mw-1024">
         <div className="flex flex-wrap justify-between">
           <h3 id="typey-type-games" className="mx-auto">
