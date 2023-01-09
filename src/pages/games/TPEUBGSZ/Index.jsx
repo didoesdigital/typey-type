c7dcc3f701122c034157da8323841b42c28020c0
Di <didoesdigital+git@gmail.com>
2023-01-09 15:29:09 +1000

    Fix tabIndex

--- src/pages/games/TPEUBGSZ/Index.jsx
+++ src/pages/games/TPEUBGSZ/Index.jsx
@@ -15,7 +15,7 @@ export default function Index({ startingMetWordsToday }) {
       <Subheader>
         <div className="flex mr1 self-center">
           <header className="flex items-center min-h-40">
-            <h2 ref={mainHeading} tabIndex="-1">
+            <h2 ref={mainHeading} tabIndex={-1}>
               TPEUBGSZ
             </h2>
           </header>
