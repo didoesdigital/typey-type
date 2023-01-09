0bbfad66ba1ed517a98b44df0b6f972a40894332
Di <didoesdigital+git@gmail.com>
2023-01-09 15:17:48 +1000

    Use new Subheader in LessonsIndex

--- src/pages/lessons/LessonsIndex.tsx
+++ src/pages/lessons/LessonsIndex.tsx
@@ -4,6 +4,7 @@ import GoogleAnalytics from "react-ga";
 import { IconExternal } from "../../components/Icon";
 import LessonList from "./components/LessonList";
 import { Tooltip } from "react-tippy";
+import Subheader from "../../components/Subheader";
 
 type LessonsIndexProps = {
   customLesson: any;
@@ -28,39 +29,37 @@ const LessonsIndex = ({
 
   return (
     <main id="main">
-      <div className="subheader">
-        <div className="flex flex-wrap items-baseline mx-auto mw-1920 justify-between px3 py2">
-          <div className="flex flex-wrap mr1 self-center">
-            <header className="flex items-center min-h-40">
-              <h2 ref={mainHeading} tabIndex={-1}>
-                Lessons
-              </h2>
-            </header>
-          </div>
-          <div className="flex flex-wrap mxn2">
-            {customLesson.title !== "Steno" ? (
-              <Link
-                to={`${match.url}/custom?study=discover&newWords=1&seenWords=1&retainedWords=1&sortOrder=sortOff&startFromWord=1`.replace(
-                  /\/{2,}/g,
-                  "/"
-                )}
-                onClick={stopLesson}
-                className="dib ml1 link-button link-button-ghost table-cell mr1"
-              >
-                Start custom lesson
-              </Link>
-            ) : null}
-            <Link
-              to={`${match.url}/custom/setup`.replace(/\/{2,}/g, "/")}
-              className="link-button button button--secondary table-cell mr2 ml1"
-              style={{ lineHeight: 2 }}
-              id="ga--lesson-index--create-a-custom-lesson"
-            >
-              Create a custom lesson
-            </Link>
-          </div>
+      <Subheader>
+        <div className="flex flex-wrap mr1 self-center">
+          <header className="flex items-center min-h-40">
+            <h2 ref={mainHeading} tabIndex={-1}>
+              Lessons
+            </h2>
+          </header>
         </div>
-      </div>
+        <div className="flex flex-wrap mxn2">
+          {customLesson.title !== "Steno" ? (
+            <Link
+              to={`${match.url}/custom?study=discover&newWords=1&seenWords=1&retainedWords=1&sortOrder=sortOff&startFromWord=1`.replace(
+                /\/{2,}/g,
+                "/"
+              )}
+              onClick={stopLesson}
+              className="dib ml1 link-button link-button-ghost table-cell mr1"
+            >
+              Start custom lesson
+            </Link>
+          ) : null}
+          <Link
+            to={`${match.url}/custom/setup`.replace(/\/{2,}/g, "/")}
+            className="link-button button button--secondary table-cell mr2 ml1"
+            style={{ lineHeight: 2 }}
+            id="ga--lesson-index--create-a-custom-lesson"
+          >
+            Create a custom lesson
+          </Link>
+        </div>
+      </Subheader>
       <div className="p3 mx-auto mw-1024">
         <h3>Typey&nbsp;Type lessons</h3>
         <LessonList lessonIndex={lessonIndex} url={match.url} />
