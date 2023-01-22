182b8f141a0140b0718beae4da081690d6184d81
Di <didoesdigital+git@gmail.com>
2023-01-22 19:31:58 +1000

    Add more phrases to hasMedical

--- src/pages/lessons/custom/generator/rules/hasMedical.ts
+++ src/pages/lessons/custom/generator/rules/hasMedical.ts
@@ -1,3 +1,60 @@
+const MEDICAL_PHRASES = [
+  "methicillin resistant staphylococcus aureus",
+  "antiepileptic hypersensitivity syndrome",
+  "biologically active intermediary",
+  "superficial punctate keratitis",
+  "benign prostatic hyperplasia",
+  "peripheral arterial disease",
+  "meibomian gland dysfunction",
+  "proximal convoluted tubule",
+  "peripheral nervous system",
+  "primary convoluted tubule",
+  "medullary collecting duct",
+  "acute coronary syndromes",
+  "ventricular fibrillation",
+  "community acquired MRSA",
+  "central venous pressure",
+  "urinary tract infection",
+  "suprachiasmatic nucleus",
+  "mean arterial pressure",
+  "sarcoplasmic reticulum",
+  "chronic heart failure",
+  "cystoid macular edema",
+  "premenstrual syndrome",
+  "endoplasmic reticulum",
+  "hematoxylin and eosin",
+  "staphylococcus aureus",
+  "creatinine clearance",
+  "peptic ulcer disease",
+  "macular degeneration",
+  "metered dose inhaler",
+  "bacterial meningitis",
+  "rheumatoid arthritis",
+  "atrial fibrillation",
+  "upper motor neurons",
+  "lower motor neurons",
+  "cerebrospinal fluid",
+  "reticular formation",
+  "sickle cell disease",
+  "spectral domain OCT",
+  "inferior vena cava",
+  "upper motor neuron",
+  "lower motor neuron",
+  "connective tissues",
+  "multiple sclerosis",
+  "ejection fraction",
+  "internal carotid",
+  "cochlear implant",
+  "anxiety disorder",
+  "corneal ectasia",
+  "plasma membrane",
+  "peak flow meter",
+  "cardinal veins",
+  "red blood cell",
+  "ACE inhibitor",
+  "carpal tunnel",
+];
+
 const MEDICAL_WORDS = [
   "pharmacotherapeutics",
   "dihydrotestosterone",
@@ -69,6 +126,6 @@ const MEDICAL_WORDS = [
 ];
 
 const hasMedical = (_outline: string, translation: string) =>
-  MEDICAL_WORDS.includes(translation);
+  MEDICAL_WORDS.includes(translation) || MEDICAL_PHRASES.includes(translation);
 
 export default hasMedical;
