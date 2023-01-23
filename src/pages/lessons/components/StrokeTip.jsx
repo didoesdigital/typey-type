e1ca43a23091003dca7cc5e04c1b83b156d515d8
Di <didoesdigital+git@gmail.com>
2023-01-23 15:01:41 +1000

    Simplify conditional

--- src/pages/lessons/components/StrokeTip.jsx
+++ src/pages/lessons/components/StrokeTip.jsx
@@ -24,98 +24,96 @@ export default function StrokeTip({
   );
 
   let strokeTip;
-  if (showStroke) {
-    if (currentStroke) {
-      const [StenoLayoutDiagram, mapBriefsFunction] =
-        getStenoDiagramAndMapBriefsFn(userSettings.stenoLayout);
+  if (showStroke && currentStroke) {
+    const [StenoLayoutDiagram, mapBriefsFunction] =
+      getStenoDiagramAndMapBriefsFn(userSettings.stenoLayout);
 
-      let layoutTypeStyle = "";
-      if (userSettings.stenoLayout === "stenoLayoutKoreanModernCSteno") {
-        layoutTypeStyle = " heavy-type-face--korean";
-      }
-      if (userSettings.stenoLayout === "stenoLayoutJapaneseSteno") {
-        layoutTypeStyle = " type-face--japanese";
-      }
-
-      const diagramWidth = (userSettings.diagramSize || 1) * 140;
-
-      strokeTip = (
-        <div
-          className={`stroke-tip${isMultiline ? " flex justify-center" : ""}`}
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          <span
-            className="visually-hidden"
-            aria-hidden={userSettings.showStrokesAsDiagrams ? "true" : "false"}
-          >
-            Hint:{" "}
-          </span>
-          {userSettings.showStrokesAsDiagrams ? (
-            <div className={`flex flex-wrap mr05${isMultiline ? " ml1" : ""}`}>
-              {splitBriefsIntoStrokes(currentStroke).map(
-                (strokeToDraw, index) => (
-                  <React.Fragment key={index}>
-                    {Object.values(mapBriefsFunction(strokeToDraw)).some(
-                      (item) => item
-                    ) && (
-                      <div
-                        className={`mt1 mr3${
-                          isMultiline ? " flex flex-grow justify-center" : ""
-                        }`}
-                      >
-                        <StenoLayoutDiagram
-                          classes="steno-diagram-svg"
-                          id={"diagramID-" + index + "-" + strokeToDraw}
-                          {...mapBriefsFunction(strokeToDraw)}
-                          brief={strokeToDraw}
-                          diagramWidth={diagramWidth}
-                        />
-                      </div>
-                    )}
-                    {Object.values(mapBriefsFunction(strokeToDraw)).every(
-                      (item) => !item
-                    ) && (
-                      <div
-                        className={`mt1 mr3 relative unknown-steno-diagram${
-                          isMultiline ? " flex flex-grow justify-center" : ""
-                        }`}
-                        aria-hidden={true}
-                      >
-                        <StenoLayoutDiagram
-                          classes="steno-diagram-svg"
-                          id={"diagramID-" + index + "-" + strokeToDraw}
-                          {...mapBriefsFunction("")}
-                          brief=""
-                          diagramWidth={diagramWidth}
-                        />
-                      </div>
-                    )}
-                  </React.Fragment>
-                )
-              )}
-            </div>
-          ) : (
-            <div className={"db" + layoutTypeStyle}>
-              <pre
-                className={`overflow-auto mw-408 text-small${
-                  isMultiline ? " flex" : ""
-                }`}
-              >
-                <span
-                  className="steno-stroke pa05 text-small"
-                  aria-label={[...currentStroke].join(" ").replace("-", "dash")}
-                >
-                  {[...currentStroke].map((item, i) => (
-                    <React.Fragment key={i}>{item}</React.Fragment>
-                  ))}
-                </span>
-              </pre>
-            </div>
-          )}
-        </div>
-      );
+    let layoutTypeStyle = "";
+    if (userSettings.stenoLayout === "stenoLayoutKoreanModernCSteno") {
+      layoutTypeStyle = " heavy-type-face--korean";
     }
+    if (userSettings.stenoLayout === "stenoLayoutJapaneseSteno") {
+      layoutTypeStyle = " type-face--japanese";
+    }
+
+    const diagramWidth = (userSettings.diagramSize || 1) * 140;
+
+    strokeTip = (
+      <div
+        className={`stroke-tip${isMultiline ? " flex justify-center" : ""}`}
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        <span
+          className="visually-hidden"
+          aria-hidden={userSettings.showStrokesAsDiagrams ? "true" : "false"}
+        >
+          Hint:{" "}
+        </span>
+        {userSettings.showStrokesAsDiagrams ? (
+          <div className={`flex flex-wrap mr05${isMultiline ? " ml1" : ""}`}>
+            {splitBriefsIntoStrokes(currentStroke).map(
+              (strokeToDraw, index) => (
+                <React.Fragment key={index}>
+                  {Object.values(mapBriefsFunction(strokeToDraw)).some(
+                    (item) => item
+                  ) && (
+                    <div
+                      className={`mt1 mr3${
+                        isMultiline ? " flex flex-grow justify-center" : ""
+                      }`}
+                    >
+                      <StenoLayoutDiagram
+                        classes="steno-diagram-svg"
+                        id={"diagramID-" + index + "-" + strokeToDraw}
+                        {...mapBriefsFunction(strokeToDraw)}
+                        brief={strokeToDraw}
+                        diagramWidth={diagramWidth}
+                      />
+                    </div>
+                  )}
+                  {Object.values(mapBriefsFunction(strokeToDraw)).every(
+                    (item) => !item
+                  ) && (
+                    <div
+                      className={`mt1 mr3 relative unknown-steno-diagram${
+                        isMultiline ? " flex flex-grow justify-center" : ""
+                      }`}
+                      aria-hidden={true}
+                    >
+                      <StenoLayoutDiagram
+                        classes="steno-diagram-svg"
+                        id={"diagramID-" + index + "-" + strokeToDraw}
+                        {...mapBriefsFunction("")}
+                        brief=""
+                        diagramWidth={diagramWidth}
+                      />
+                    </div>
+                  )}
+                </React.Fragment>
+              )
+            )}
+          </div>
+        ) : (
+          <div className={"db" + layoutTypeStyle}>
+            <pre
+              className={`overflow-auto mw-408 text-small${
+                isMultiline ? " flex" : ""
+              }`}
+            >
+              <span
+                className="steno-stroke pa05 text-small"
+                aria-label={[...currentStroke].join(" ").replace("-", "dash")}
+              >
+                {[...currentStroke].map((item, i) => (
+                  <React.Fragment key={i}>{item}</React.Fragment>
+                ))}
+              </span>
+            </pre>
+          </div>
+        )}
+      </div>
+    );
   } else {
     strokeTip = (
       <StrokeTipHidden
