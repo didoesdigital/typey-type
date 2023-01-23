7d9b840474102233b37fe177f2287a8eeec5cdea
Di <didoesdigital+git@gmail.com>
2023-01-23 15:42:47 +1000

    Use new StrokeTipDiagram

--- src/pages/lessons/components/StrokeTip.jsx
+++ src/pages/lessons/components/StrokeTip.jsx
@@ -7,6 +7,7 @@ import {
 import getStenoDiagram from "../utilities/getStenoDiagram";
 import getMapBriefsFn from "../utilities/getMapBriefsFn";
 import StrokeTipHidden from "./StrokeTipHidden";
+import StrokeTipDiagram from "./StrokeTipDiagram";
 
 export default function StrokeTip({
   changeShowStrokesInLesson,
@@ -34,8 +35,6 @@ export default function StrokeTip({
       ? " type-face--japanese"
       : "";
 
-  const diagramWidth = (userSettings.diagramSize || 1) * 140;
-
   return (
     <div className="mb6">
       {showStroke && currentStroke ? (
@@ -51,49 +50,14 @@ export default function StrokeTip({
             Hint:{" "}
           </span>
           {userSettings.showStrokesAsDiagrams ? (
-            <div className={`flex flex-wrap mr05${isMultiline ? " ml1" : ""}`}>
-              {splitBriefsIntoStrokes(currentStroke).map(
-                (strokeToDraw, index) => (
-                  <React.Fragment key={index}>
-                    {Object.values(mapBriefsFunction(strokeToDraw)).some(
-                      (item) => item
-                    ) && (
-                      <div
-                        className={`mt1 mr3${
-                          isMultiline ? " flex flex-grow justify-center" : ""
-                        }`}
-                      >
-                        <StenoLayoutDiagram
-                          classes="steno-diagram-svg"
-                          id={"diagramID-" + index + "-" + strokeToDraw}
-                          {...mapBriefsFunction(strokeToDraw)}
-                          brief={strokeToDraw}
-                          diagramWidth={diagramWidth}
-                        />
-                      </div>
-                    )}
-                    {Object.values(mapBriefsFunction(strokeToDraw)).every(
-                      (item) => !item
-                    ) && (
-                      <div
-                        className={`mt1 mr3 relative unknown-steno-diagram${
-                          isMultiline ? " flex flex-grow justify-center" : ""
-                        }`}
-                        aria-hidden={true}
-                      >
-                        <StenoLayoutDiagram
-                          classes="steno-diagram-svg"
-                          id={"diagramID-" + index + "-" + strokeToDraw}
-                          {...mapBriefsFunction("")}
-                          brief=""
-                          diagramWidth={diagramWidth}
-                        />
-                      </div>
-                    )}
-                  </React.Fragment>
-                )
-              )}
-            </div>
+            <StrokeTipDiagram
+              isMultiline={isMultiline}
+              splitBriefsIntoStrokes={splitBriefsIntoStrokes}
+              currentStroke={currentStroke}
+              mapBriefsFunction={mapBriefsFunction}
+              StenoLayoutDiagram={StenoLayoutDiagram}
+              diagramSize={userSettings.diagramSize}
+            />
           ) : (
             <div className={"db" + layoutTypeStyle}>
               <pre
