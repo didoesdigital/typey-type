ada4a67b0352cdb00b5a144891de7f5db53d98f0
Di <didoesdigital+git@gmail.com>
2023-01-26 20:53:51 +1000

    Add UserSettings story

---
+++ src/pages/lessons/components/UserSettings.stories.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import UserSettings from "./UserSettings";
+import userSettingsFixture from "../../../stories/fixtures/userSettings";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Pages/UserSettings",
+  component: UserSettings,
+};
+
+const Template = (args) => {
+  return (
+    <div
+      id="lesson-page"
+      className="flex-wrap-md flex mx-auto mw-1920"
+      style={{ justifyContent: "flex-end" }}
+    >
+      <UserSettings
+        changeSortOrderUserSetting={() => undefined}
+        changeSpacePlacementUserSetting={() => undefined}
+        changeStenoLayout={() => undefined}
+        changeShowStrokesAs={() => undefined}
+        changeShowStrokesAsList={() => undefined}
+        changeShowStrokesOnMisstroke={() => undefined}
+        changeUserSetting={() => undefined}
+        chooseStudy={() => undefined}
+        disableUserSettings={false}
+        handleDiagramSize={() => undefined}
+        handleBeatsPerMinute={() => undefined}
+        handleLimitWordsChange={() => undefined}
+        handleStartFromWordChange={() => undefined}
+        handleRepetitionsChange={() => undefined}
+        handleUpcomingWordsLayout={() => undefined}
+        hideOtherSettings={false}
+        maxStartFromWord={100}
+        revisionMode={false}
+        setAnnouncementMessage={() => undefined}
+        totalWordCount={50}
+        userSettings={userSettingsFixture}
+        {...args}
+      />
+    </div>
+  );
+};
+
+export const UserSettingsStory = Template.bind({});
