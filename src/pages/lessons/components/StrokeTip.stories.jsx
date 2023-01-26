f678dca2b7b10f5c6192a3582e3c902285482d86
Di <didoesdigital+git@gmail.com>
2023-01-26 21:18:21 +1000

    Fix stroke tip stories

--- src/pages/lessons/components/StrokeTip.stories.jsx
+++ src/pages/lessons/components/StrokeTip.stories.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import StrokeTip from "./StrokeTip";
 import userSettings from "../../../stories/fixtures/userSettings";
+import globalLookupDictionaryMinimalJSON from "../../../stories/fixtures/globalLookupDictionaryMinimal.json";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: "Lessons/StrokeTip",
   component: StrokeTip,
-  id: "metronome", // permalink
+  id: "stroke-tip", // permalink
 };
+
+const globalLookupDictionaryMinimal = new Map(
+  // @ts-ignore
+  globalLookupDictionaryMinimalJSON
+);
 
 const Template = (args) => {
   return (
     <StrokeTip
       changeShowStrokesInLesson={() => undefined}
+      currentPhrase={"test"}
       currentStroke={"TEFT"}
+      globalLookupDictionary={globalLookupDictionaryMinimal}
       showStrokesInLesson={false}
       targetStrokeCount={1}
       userSettings={userSettings}
@@ -27,7 +35,9 @@ export const StrokeTipDiagramStory = Template.bind({});
 StrokeTipDiagramStory.storyName = "Stroke tip diagram";
 StrokeTipDiagramStory.args = {
   changeShowStrokesInLesson: () => undefined,
+  currentPhrase: "alone",
   currentStroke: "A/PHOEPB",
+  globalLookupDictionary: globalLookupDictionaryMinimal,
   showStrokesInLesson: true,
   targetStrokeCount: 1,
   userSettings: userSettings,
@@ -38,7 +48,9 @@ export const StrokeTipPalantypeStory = Template.bind({});
 StrokeTipPalantypeStory.storyName = "Stroke tip diagram Palantype";
 StrokeTipPalantypeStory.args = {
   changeShowStrokesInLesson: () => undefined,
+  currentPhrase: "alone",
   currentStroke: "SCPTH+MFRNLYOEAU-I^NLCMFRPT+SH",
+  globalLookupDictionary: globalLookupDictionaryMinimal,
   showStrokesInLesson: true,
   targetStrokeCount: 1,
   userSettings: {
@@ -53,7 +65,9 @@ export const StrokeTipTextStory = Template.bind({});
 StrokeTipTextStory.storyName = "Stroke tip text";
 StrokeTipTextStory.args = {
   changeShowStrokesInLesson: () => undefined,
+  currentPhrase: "alone",
   currentStroke: "A/PHOEPB",
+  globalLookupDictionary: globalLookupDictionaryMinimal,
   showStrokesInLesson: true,
   targetStrokeCount: 2,
   userSettings: { ...userSettings, showStrokesAsDiagrams: false },
@@ -64,7 +78,9 @@ export const StrokeTipHiddenStory = Template.bind({});
 StrokeTipHiddenStory.storyName = "Stroke tip hidden";
 StrokeTipHiddenStory.args = {
   changeShowStrokesInLesson: () => undefined,
+  currentPhrase: "alone",
   currentStroke: "A/PHOEPB",
+  globalLookupDictionary: globalLookupDictionaryMinimal,
   showStrokesInLesson: false,
   targetStrokeCount: 2,
   userSettings: { ...userSettings, showStrokesAsDiagrams: false },
