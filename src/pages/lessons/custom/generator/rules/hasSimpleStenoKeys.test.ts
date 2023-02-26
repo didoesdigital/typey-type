22026ec9aff1839380260e523f1a573723266cfc
Di <didoesdigital+git@gmail.com>
2023-02-26 17:17:09 +1000

    Add hasSimpleStenoKeys test

--- src/pages/lessons/custom/generator/rules/hasSimpleStenoKeys.test.ts
+++ src/pages/lessons/custom/generator/rules/hasSimpleStenoKeys.test.ts
@@ -8,4 +8,9 @@ describe("hasSimpleStenoKeys", () => {
   it("returns false for letters that need two or more keys", async () => {
     expect(hasSimpleStenoKeys("TKEUD", "did")).toEqual(false);
   });
+
+  // Not sure if this is good choice or not but this test at least makes it visible
+  it("returns true for EU even though it needs two keys to make a short i sound", async () => {
+    expect(hasSimpleStenoKeys("EU", "I")).toEqual(true);
+  });
 });
