cf6ec1a196ba5b2d7b16ca30dffc25a3ac76457d
Di <didoesdigital+git@gmail.com>
2023-03-11 16:07:50 +1000

    Remove useless isCustom check

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -153,10 +153,6 @@ class Dictionary extends Component {
       });
   }
 
-  isCustom() {
-    return this.props.location.pathname === "/dictionaries/custom";
-  }
-
   prefillSurveyLink() {
     // fullURL = "https://docs.google.com/forms/d/e/1FAIpQLSfqBBEs5Fl8vgay03fEXzSU7Ey_pms6Y6Nt2Yk8gFftGhAWQA/viewform?usp=pp_url&entry.1884511690=Example";
     let googleFormURL =
@@ -203,193 +199,182 @@ class Dictionary extends Component {
     }
 
     if (this.state.dictionary) {
-      if (this.isCustom()) {
-        // console.log("custom");
-        return (
-          <DocumentTitle title="Typey Type | Create a custom dictionary">
-            <p>CUSTOM DICT</p>
-          </DocumentTitle>
+      let contents = "";
+      let truncatedMessage = ``;
+      contents = JSON.stringify(this.state.dictionary.contents)
+        .split('",')
+        .join('",\n');
+      contents = "{\n" + contents.slice(1, contents.length); // split first line {"STROKE": "TRANSLATION", on {"
+
+      let contentsArray = contents.split("\n");
+      let contentsArrayLength = contentsArray.length;
+      let truncationLimit = 1000;
+      if (contentsArrayLength > truncationLimit) {
+        truncatedMessage = (
+          <p className="bg-danger dark:text-coolgrey-900">
+            The dictionary is too large to display in full so this only shows
+            the first {truncationLimit} entries.
+          </p>
         );
-      } else {
-        let contents = "";
-        let truncatedMessage = ``;
-        contents = JSON.stringify(this.state.dictionary.contents)
-          .split('",')
-          .join('",\n');
-        contents = "{\n" + contents.slice(1, contents.length); // split first line {"STROKE": "TRANSLATION", on {"
+        let newContents = contentsArray.slice(0, truncationLimit);
+        newContents[truncationLimit - 1] = newContents[
+          truncationLimit - 1
+        ].slice(0, -1); // removing trailing comma
+        newContents.push("}");
+        contents = newContents.join("\n");
+      }
 
-        let contentsArray = contents.split("\n");
-        let contentsArrayLength = contentsArray.length;
-        let truncationLimit = 1000;
-        if (contentsArrayLength > truncationLimit) {
-          truncatedMessage = (
-            <p className="bg-danger dark:text-coolgrey-900">
-              The dictionary is too large to display in full so this only shows
-              the first {truncationLimit} entries.
-            </p>
-          );
-          let newContents = contentsArray.slice(0, truncationLimit);
-          newContents[truncationLimit - 1] = newContents[
-            truncationLimit - 1
-          ].slice(0, -1); // removing trailing comma
-          newContents.push("}");
-          contents = newContents.join("\n");
-        }
-
-        let externalLink = "";
-        let internalLink = "";
-        let dictLink = this.state.dictionary.link;
-        if (
-          dictLink.startsWith("/typey-type") ||
-          dictLink.startsWith("/dictionaries/") ||
-          dictLink.startsWith("/lessons/") ||
-          dictLink.startsWith("/support")
-        ) {
+      let externalLink = "";
+      let internalLink = "";
+      let dictLink = this.state.dictionary.link;
+      if (
+        dictLink.startsWith("/typey-type") ||
+        dictLink.startsWith("/dictionaries/") ||
+        dictLink.startsWith("/lessons/") ||
+        dictLink.startsWith("/support")
+      ) {
+        internalLink = (
+          <p>
+            <Link to={dictLink}>Learn more</Link>
+          </p>
+        );
+        if (dictLink.startsWith(process.env.PUBLIC_URL + "/lessons")) {
           internalLink = (
             <p>
-              <Link to={dictLink}>Learn more</Link>
-            </p>
-          );
-          if (dictLink.startsWith(process.env.PUBLIC_URL + "/lessons")) {
-            internalLink = (
-              <p>
-                <Link to={dictLink}>Lesson: {this.state.dictionary.title}</Link>
-              </p>
-            );
-          }
-          // better check would be `//`, `http`
-        } else {
-          externalLink = (
-            <p className="mt3">
-              <a
-                href={this.state.dictionary.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn more
-                <Tooltip
-                  title="Opens in a new tab"
-                  animation="shift"
-                  arrow="true"
-                  className=""
-                  duration="200"
-                  tabIndex={0}
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={this.props.setAnnouncementMessage}
-                >
-                  <IconExternal
-                    ariaHidden="true"
-                    role="presentation"
-                    iconWidth="24"
-                    iconHeight="24"
-                    className="ml1 svg-icon-wrapper svg-baseline"
-                    iconTitle=""
-                  />
-                </Tooltip>
-              </a>
+              <Link to={dictLink}>Lesson: {this.state.dictionary.title}</Link>
             </p>
           );
         }
-
-        return (
-          <DocumentTitle
-            title={"Typey Type | Dictionary: " + this.state.dictionary.title}
-          >
-            <main id="main">
-              <Subheader>
-                <div className="flex mr1 self-center">
-                  <header className="flex items-center min-h-40">
-                    <h2
-                      className="table-cell mr2"
-                      ref={(heading) => {
-                        this.mainHeading = heading;
-                      }}
-                      tabIndex={-1}
-                    >
-                      {this.state.loadingDictionaryContents ? (
-                        <span>Loading dictionary…</span>
-                      ) : (
-                        this.state.dictionary.title
-                      )}
-                      {this.state.loadingError && <span>Loading failed.</span>}
-                    </h2>
-                  </header>
-                </div>
-                <div className="flex mxn2">
-                  <a
-                    href={process.env.PUBLIC_URL + this.state.dictionary.path}
-                    download=""
-                    onClick={this.downloadDictionary.bind(this)}
-                    className="link-button link-button-ghost table-cell mr1"
-                  >
-                    Download
-                  </a>
-                  <PseudoContentButton
-                    className="js-clipboard-button button button--secondary table-cell mr1 copy-to-clipboard"
-                    style={{ lineHeight: 2 }}
-                    dataClipboardTarget="#js-dictionary-json-pre"
-                  >
-                    Copy to clipboard
-                  </PseudoContentButton>
-                </div>
-              </Subheader>
-              <div className="p3 mx-auto mw-1024">
-                <div className="mw-568">
-                  {this.state.dictionary.author &&
-                    !this.state.dictionary.tagline.includes("Loading") && (
-                      <p className="text-small text-uppercase de-emphasized mt3">
-                        Contributor: {this.state.dictionary.author}
-                      </p>
-                    )}
-
-                  {this.state.dictionary.tagline &&
-                    !this.state.dictionary.tagline.includes("Loading") && (
-                      <p>{this.state.dictionary.tagline}</p>
-                    )}
-
-                  {this.state.dictionary.link &&
-                    !this.state.dictionary.link.includes(
-                      "/typey-type/support"
-                    ) &&
-                    internalLink}
-                  {this.state.dictionary.link && externalLink}
-
-                  <h3>The dictionary</h3>
-                  {this.state.loadingError && <p>Loading failed.</p>}
-                  {!this.state.loadingDictionaryContents && truncatedMessage}
-                  {this.state.loadingDictionaryContents ? (
-                    <p>Loading…</p>
-                  ) : (
-                    <pre
-                      className="quote h-168 overflow-scroll mw-384 mt1 mb3"
-                      id="js-dictionary-json-pre"
-                      tabIndex={0}
-                    >
-                      <code>{contents}</code>
-                    </pre>
-                  )}
-                </div>
-                <p>
-                  <a
-                    href={this.prefillSurveyLink()}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    ref={(surveyLink) => {
-                      this.surveyLink = surveyLink;
-                    }}
-                    onClick={this.prefillSurveyLink.bind(this)}
-                    id="ga--dictionary--give-feedback"
-                  >
-                    Give feedback on this dictionary (form opens in a new tab)
-                  </a>
-                </p>
-              </div>
-            </main>
-          </DocumentTitle>
+        // better check would be `//`, `http`
+      } else {
+        externalLink = (
+          <p className="mt3">
+            <a
+              href={this.state.dictionary.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn more
+              <Tooltip
+                title="Opens in a new tab"
+                animation="shift"
+                arrow="true"
+                className=""
+                duration="200"
+                tabIndex={0}
+                tag="span"
+                theme="didoesdigital"
+                trigger="mouseenter focus click"
+                onShow={this.props.setAnnouncementMessage}
+              >
+                <IconExternal
+                  ariaHidden="true"
+                  role="presentation"
+                  iconWidth="24"
+                  iconHeight="24"
+                  className="ml1 svg-icon-wrapper svg-baseline"
+                  iconTitle=""
+                />
+              </Tooltip>
+            </a>
+          </p>
         );
       }
+
+      return (
+        <DocumentTitle
+          title={"Typey Type | Dictionary: " + this.state.dictionary.title}
+        >
+          <main id="main">
+            <Subheader>
+              <div className="flex mr1 self-center">
+                <header className="flex items-center min-h-40">
+                  <h2
+                    className="table-cell mr2"
+                    ref={(heading) => {
+                      this.mainHeading = heading;
+                    }}
+                    tabIndex={-1}
+                  >
+                    {this.state.loadingDictionaryContents ? (
+                      <span>Loading dictionary…</span>
+                    ) : (
+                      this.state.dictionary.title
+                    )}
+                    {this.state.loadingError && <span>Loading failed.</span>}
+                  </h2>
+                </header>
+              </div>
+              <div className="flex mxn2">
+                <a
+                  href={process.env.PUBLIC_URL + this.state.dictionary.path}
+                  download=""
+                  onClick={this.downloadDictionary.bind(this)}
+                  className="link-button link-button-ghost table-cell mr1"
+                >
+                  Download
+                </a>
+                <PseudoContentButton
+                  className="js-clipboard-button button button--secondary table-cell mr1 copy-to-clipboard"
+                  style={{ lineHeight: 2 }}
+                  dataClipboardTarget="#js-dictionary-json-pre"
+                >
+                  Copy to clipboard
+                </PseudoContentButton>
+              </div>
+            </Subheader>
+            <div className="p3 mx-auto mw-1024">
+              <div className="mw-568">
+                {this.state.dictionary.author &&
+                  !this.state.dictionary.tagline.includes("Loading") && (
+                    <p className="text-small text-uppercase de-emphasized mt3">
+                      Contributor: {this.state.dictionary.author}
+                    </p>
+                  )}
+
+                {this.state.dictionary.tagline &&
+                  !this.state.dictionary.tagline.includes("Loading") && (
+                    <p>{this.state.dictionary.tagline}</p>
+                  )}
+
+                {this.state.dictionary.link &&
+                  !this.state.dictionary.link.includes("/typey-type/support") &&
+                  internalLink}
+                {this.state.dictionary.link && externalLink}
+
+                <h3>The dictionary</h3>
+                {this.state.loadingError && <p>Loading failed.</p>}
+                {!this.state.loadingDictionaryContents && truncatedMessage}
+                {this.state.loadingDictionaryContents ? (
+                  <p>Loading…</p>
+                ) : (
+                  <pre
+                    className="quote h-168 overflow-scroll mw-384 mt1 mb3"
+                    id="js-dictionary-json-pre"
+                    tabIndex={0}
+                  >
+                    <code>{contents}</code>
+                  </pre>
+                )}
+              </div>
+              <p>
+                <a
+                  href={this.prefillSurveyLink()}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  ref={(surveyLink) => {
+                    this.surveyLink = surveyLink;
+                  }}
+                  onClick={this.prefillSurveyLink.bind(this)}
+                  id="ga--dictionary--give-feedback"
+                >
+                  Give feedback on this dictionary (form opens in a new tab)
+                </a>
+              </p>
+            </div>
+          </main>
+        </DocumentTitle>
+      );
     } else {
       console.log("no dictionary");
       return (
