b3d2ea78228441307204bd1c504fa60874dbe94f
Di <didoesdigital+git@gmail.com>
2023-03-12 09:52:58 +1000

    Housekeeping: format DictionaryNotFound

--- src/components/DictionaryNotFound.jsx
+++ src/components/DictionaryNotFound.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import GoogleAnalytics from 'react-ga';
-import * as Sentry from '@sentry/browser';
-import DocumentTitle from 'react-document-title';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import GoogleAnalytics from "react-ga";
+import * as Sentry from "@sentry/browser";
+import DocumentTitle from "react-document-title";
+import { Link } from "react-router-dom";
 import Subheader from "./Subheader";
 
 // fullURL = "https://docs.google.com/forms/d/e/1FAIpQLSfqBBEs5Fl8vgay03fEXzSU7Ey_pms6Y6Nt2Yk8gFftGhAWQA/viewform?usp=pp_url&entry.1884511690=Example";
@@ -17,12 +17,12 @@ class DictionaryNotFound extends Component {
     }
 
     GoogleAnalytics.event({
-      category: 'Dictionary not found',
-      action: 'Visited',
-      label: labelString
+      category: "Dictionary not found",
+      action: "Visited",
+      label: labelString,
     });
 
-    Sentry.captureException('Dictionary not found');
+    Sentry.captureException("Dictionary not found");
 
     if (this.mainHeading) {
       this.mainHeading.focus();
@@ -31,22 +31,50 @@ class DictionaryNotFound extends Component {
 
   render() {
     return (
-      <DocumentTitle title={'Typey Type | Missing dictionary'}>
+      <DocumentTitle title={"Typey Type | Missing dictionary"}>
         <main id="main">
           <Subheader>
             <div className="flex mr1 self-center">
               <header className="flex items-center min-h-40">
-                <h2 className="table-cell mr2" ref={(heading) => { this.mainHeading = heading; }} tabIndex={-1}>Missing dictionary</h2>
+                <h2
+                  className="table-cell mr2"
+                  ref={(heading) => {
+                    this.mainHeading = heading;
+                  }}
+                  tabIndex={-1}
+                >
+                  Missing dictionary
+                </h2>
               </header>
             </div>
           </Subheader>
           <div className="mx-auto mw-1024 p3">
             <div className="mw-568">
-              <p className="mt3">That dictionary couldn’t be found. Try another:</p>
+              <p className="mt3">
+                That dictionary couldn’t be found. Try another:
+              </p>
               <ul>
-                <li><Link to="/dictionaries">All dictionaries</Link></li>
+                <li>
+                  <Link to="/dictionaries">All dictionaries</Link>
+                </li>
               </ul>
-              <p>Or <a href={googleFormURL + encodeURIComponent(this.props.location?.pathname || "")} target="_blank" rel="noopener noreferrer" ref={(surveyLink) => { this.surveyLink = surveyLink; }} id="ga--dictionary--give-feedback">let me know (form opens in a new tab)</a></p>
+              <p>
+                Or{" "}
+                <a
+                  href={
+                    googleFormURL +
+                    encodeURIComponent(this.props.location?.pathname || "")
+                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  ref={(surveyLink) => {
+                    this.surveyLink = surveyLink;
+                  }}
+                  id="ga--dictionary--give-feedback"
+                >
+                  let me know (form opens in a new tab)
+                </a>
+              </p>
             </div>
           </div>
         </main>
