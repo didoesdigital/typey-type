25dff3952194ddfbb8aacb26fc709e2a0add0488
Di <didoesdigital+git@gmail.com>
2023-04-17 17:46:13 +1000

    Housekeeping: format combineValidDictionaries.ts

--- src/utils/transformingDictionaries/combineValidDictionaries.ts
+++ src/utils/transformingDictionaries/combineValidDictionaries.ts
@@ -1,6 +1,6 @@
 import LATEST_PLOVER_DICT_NAME from "../../constant/latestPloverDictName";
 import SOURCE_NAMESPACES from "../../constant/sourceNamespaces";
-import { addOutlinesToWordsInCombinedDict } from './transformingDictionaries';
+import { addOutlinesToWordsInCombinedDict } from "./transformingDictionaries";
 import {
   PersonalDictionaryNameAndContents,
   StenoDictionary,
@@ -12,7 +12,8 @@ const combineValidDictionaries = (
   ploverDict: any = null
 ) => {
   let combinedLookupDictionary = new Map();
-  let numberOfPersonalDictionaries = personalDictionariesNamesAndContents.length;
+  let numberOfPersonalDictionaries =
+    personalDictionariesNamesAndContents.length;
   let outlinesWeHaveSeen = new Set();
   // eslint-disable-next-line
   let _;
@@ -21,21 +22,37 @@ const combineValidDictionaries = (
   for (let i = 0; i < numberOfPersonalDictionaries; i++) {
     let dictName = personalDictionariesNamesAndContents[i][0];
     let dictContent = personalDictionariesNamesAndContents[i][1];
-    [combinedLookupDictionary, outlinesWeHaveSeen] = addOutlinesToWordsInCombinedDict(dictContent, combinedLookupDictionary, `${SOURCE_NAMESPACES.get('user')}:${dictName}`, outlinesWeHaveSeen);
+    [combinedLookupDictionary, outlinesWeHaveSeen] =
+      addOutlinesToWordsInCombinedDict(
+        dictContent,
+        combinedLookupDictionary,
+        `${SOURCE_NAMESPACES.get("user")}:${dictName}`,
+        outlinesWeHaveSeen
+      );
   }
 
   // 2. Add Typey Type entries
-  [combinedLookupDictionary, _] = addOutlinesToWordsInCombinedDict(dictTypeyType, combinedLookupDictionary, `${SOURCE_NAMESPACES.get('typey')}:typey-type.json`, new Set());
+  [combinedLookupDictionary, _] = addOutlinesToWordsInCombinedDict(
+    dictTypeyType,
+    combinedLookupDictionary,
+    `${SOURCE_NAMESPACES.get("typey")}:typey-type.json`,
+    new Set()
+  );
 
   // 3. Add Plover dictionary entries
   if (!!ploverDict) {
     // eslint-disable-next-line
-    [combinedLookupDictionary, _] = addOutlinesToWordsInCombinedDict(ploverDict, combinedLookupDictionary, `${SOURCE_NAMESPACES.get('plover')}:${LATEST_PLOVER_DICT_NAME}`, new Set());
+    [combinedLookupDictionary, _] = addOutlinesToWordsInCombinedDict(
+      ploverDict,
+      combinedLookupDictionary,
+      `${SOURCE_NAMESPACES.get("plover")}:${LATEST_PLOVER_DICT_NAME}`,
+      new Set()
+    );
   }
 
   outlinesWeHaveSeen = new Set();
 
   return combinedLookupDictionary;
-}
+};
 
 export default combineValidDictionaries;
