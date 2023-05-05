cbe8981fe080917f7221b645a289d35ef327d7bb
Di <didoesdigital+git@gmail.com>
2023-05-05 12:37:17 +1000

    Remove info panel from Lookup
    The white panel is large and obvious so when the lookup loads it shifts
    layout. There are few responses provided to the linked Google form from
    this page.

--- src/pages/lookup/Lookup.tsx
+++ src/pages/lookup/Lookup.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from 'react-router-dom';
-import OutboundLink from "../../components/OutboundLink";
 import StrokesForWords from "../../components/StrokesForWords";
 import PseudoContentButton from "../../components/PseudoContentButton";
-import { IconExternal } from "../../components/Icon";
-import { Tooltip } from "react-tippy";
 import Subheader from "../../components/Subheader";
 import getWordFamilyGroup from "./utilities/getWordFamilyGroup";
 
@@ -73,6 +70,7 @@ const Lookup = ({
   const setUpCustomLesson = () => {
     const words = wordFamilyGroup.slice();
     words.unshift(trackedPhrase);
+
     const material = words.map(word => {
       if (globalLookupDictionary.get(word)) {
         return ({
@@ -135,48 +133,6 @@ const Lookup = ({
             updatePersonalDictionaries={updatePersonalDictionaries}
             userSettings={userSettings}
           />
-        </div>
-        <div className="panel bg-white dark:bg-coolgrey-1000 p3 mt3">
-          <p>
-            This lookup uses Plover’s latest dictionary and Typey&nbsp;Type’s
-            suggestions.
-          </p>
-          <p className="mb0">
-            If you notice any odd strokes,{" "}
-            <OutboundLink
-              eventLabel="post to the feedback form"
-              aria-label="post to the feedback form (external link opens in new tab)"
-              to="https://docs.google.com/forms/d/e/1FAIpQLSeevsX2oYEvnDHd3y8weg5_7-T8QZsF93ElAo28JO9Tmog-7Q/viewform?usp=sf_link"
-            >
-              use the{" "}
-              <span className="whitespace-nowrap">
-                feedback form
-                {/* @ts-ignore */}
-                <Tooltip
-                  title="Opens in new tab"
-                  className=""
-                  animation="shift"
-                  arrow="true"
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={setAnnouncementMessage}
-                >
-                  <IconExternal
-                    ariaHidden="true"
-                    role="presentation"
-                    iconWidth="24"
-                    iconHeight="24"
-                    className="ml1 svg-icon-wrapper svg-baseline"
-                    iconTitle=""
-                  />
-                </Tooltip>
-              </span>
-            </OutboundLink>
-            .
-          </p>
         </div>
         <div className="">
           <div className="mt0">
