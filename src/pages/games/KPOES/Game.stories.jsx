cfe3547655f7666f9e517c0814da170fbf167889
Di <didoesdigital+git@gmail.com>
2023-07-03 15:43:10 +1000

    Add KPOES story

---
+++ src/pages/games/KPOES/Game.stories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Game from "./Game";
+import userSettings from "../../../stories/fixtures/userSettings";
+import globalUserSettings from "../../../stories/fixtures/globalUserSettings";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "Games/KPOES game",
+  component: Game,
+};
+
+const Template = (args) => (
+  <div className="p3">
+    <Game
+      fetchAndSetupGlobalDict={() => Promise.resolve(true)}
+      globalLookupDictionary={new Map()}
+      globalLookupDictionaryLoaded={true}
+      metWords={{}}
+      personalDictionaries={{}}
+      globalUserSettings={globalUserSettings}
+      userSettings={userSettings}
+      updateMultipleMetWords={() => {}}
+      {...args}
+    />
+  </div>
+);
+
+export const KPOESGameStory = Template.bind({});
---
+++ src/stories/fixtures/globalUserSettings.js
@@ -0,0 +1,11 @@
+const globalUserSettings = {
+  "experiments": {
+    "stenohintsonthefly": true,
+    "timesSeen": false,
+  },
+  "flashcardsCourseLevel": "noviceCourse",
+  "showMisstrokesInLookup": true,
+  "writerInput": "raw",
+};
+
+export default globalUserSettings;
