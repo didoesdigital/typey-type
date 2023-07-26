68218cbe6933a4d8a168e0d2e2d38a8f30f44843
Di <didoesdigital+git@gmail.com>
2023-07-26 10:02:26 +1000

    Fix game dark mode styles

--- src/pages/games/components/Intro.jsx
+++ src/pages/games/components/Intro.jsx
@@ -4,7 +4,7 @@ export default function Intro({ introText, robot }) {
   return (
     <div className="mw-824 mr3 flex-grow">
       <div className="flex">
-        <div className="w-100 mw-48 mr3">{robot}</div>
+        <div className="w-100 mw-48 mr3 game-robot">{robot}</div>
         <p>{introText}</p>
       </div>
     </div>
