0b746fa03c7b50e7be00d8ea59c9149cceb07ab9
Di <didoesdigital+git@gmail.com>
2023-10-29 15:29:16 +1000

    Update BreakStory to CSF 3

--- src/pages/break/Break.stories.jsx
+++ src/pages/break/Break.stories.jsx
@@ -1,14 +1,12 @@
-import React from "react";
 import Break from "./Break";
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  title: "Pages/Break",
-  component: Break,
-};
+const meta = { component: Break, title: "Pages/Break" };
+export default meta;
 
-const Template = (args) => {
-  return <Break {...args} />;
+export const BreakStory = {
+  args: {
+    setAnnouncementMessageString: () => {
+      console.log("announce: your break is done");
+    },
+  },
 };
-
-export const BreakStory = Template.bind({});
