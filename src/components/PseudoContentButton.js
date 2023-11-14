bd24752186f6305739df28b3ef1d7720a00758df
Di <didoesdigital+git@gmail.com>
2023-11-14 21:04:52 +1000

    Housekeeping: format PseudoContentButton

--- src/components/PseudoContentButton.js
+++ src/components/PseudoContentButton.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import Clipboard from 'clipboard';
+import React, { Component } from "react";
+import Clipboard from "clipboard";
 import GoogleAnalytics from "react-ga4";
 
 class PseudoContentButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false
-    }
+      clicked: false,
+    };
   }
 
   componentDidMount() {
-    this.clipboard = new Clipboard('.js-clipboard-button');
+    this.clipboard = new Clipboard(".js-clipboard-button");
 
-    this.setState({clicked: false});
+    this.setState({ clicked: false });
   }
 
   componentWillUnmount() {
@@ -24,54 +24,66 @@ class PseudoContentButton extends Component {
     if (this.props.onClick) {
       this.props.onClick();
     }
-    this.setState({clicked: true}, () => {
+    this.setState({ clicked: true }, () => {
       let timeoutID = window.setTimeout(() => {
-        this.setState({clicked: false});
+        this.setState({ clicked: false });
         window.clearTimeout(timeoutID);
       }, 1000);
     });
 
     let target = this.props.dataClipboardTarget;
-    if (target === '#js-metwords-from-typey-type') { target = 'Copied progress to clipboard'; }
-    if (target === '#js-custom-lesson-dictionary-entries') { target = 'Copied custom lesson to clipboard'; }
-    if (target === '#js-your-words-for-dictionary-entries') { target = 'Copied your words to clipboard'; }
+    if (target === "#js-metwords-from-typey-type") {
+      target = "Copied progress to clipboard";
+    }
+    if (target === "#js-custom-lesson-dictionary-entries") {
+      target = "Copied custom lesson to clipboard";
+    }
+    if (target === "#js-your-words-for-dictionary-entries") {
+      target = "Copied your words to clipboard";
+    }
 
     if (target && target.includes("Copied progress")) {
       GoogleAnalytics.event({
-        category: 'Progress',
-        action: 'Copied progress',
-        label: 'Target: ' + target
+        category: "Progress",
+        action: "Copied progress",
+        label: "Target: " + target,
       });
-    }
-    else if (target && target.includes("Copied")) {
+    } else if (target && target.includes("Copied")) {
       GoogleAnalytics.event({
-        category: 'Copy button',
-        action: 'Click',
-        label: 'Target: ' + target
+        category: "Copy button",
+        action: "Click",
+        label: "Target: " + target,
       });
-    }
-    else if (target) {
+    } else if (target) {
       GoogleAnalytics.event({
-        category: 'Button',
-        action: 'Click',
-        label: 'Target: ' + target
+        category: "Button",
+        action: "Click",
+        label: "Target: " + target,
       });
-    }
-    else if (this.props.children === "Load progress from text" || this.props.children === "Load") {
+    } else if (
+      this.props.children === "Load progress from text" ||
+      this.props.children === "Load"
+    ) {
       // already handled by Progress.js restoreButtonOnClickFunction() {}
-    }
-    else {
+    } else {
       GoogleAnalytics.event({
-        category: 'Button',
-        action: 'Click',
-        label: 'Target: NO_TARGET'
+        category: "Button",
+        action: "Click",
+        label: "Target: NO_TARGET",
       });
     }
   }
 
-  render () {
+  render() {
     return (
-      <button className={this.props.className + (this.state.clicked ? ' fade-out-up' : '')} data-clipboard-target={this.props.dataClipboardTarget} onClick={this.animatedPseudoContent.bind(this)} style={this.props.style || {}}>
+      <button
+        className={
+          this.props.className + (this.state.clicked ? " fade-out-up" : "")
+        }
+        data-clipboard-target={this.props.dataClipboardTarget}
+        onClick={this.animatedPseudoContent.bind(this)}
+        style={this.props.style || {}}
+      >
         {this.props.children}
       </button>
     );
