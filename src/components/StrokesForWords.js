e12cd41c09439701a6c802ae093fd209b78b1c80
Di <didoesdigital+git@gmail.com>
2023-11-17 22:02:32 +1000

    Housekeeping: format StrokesForWords

--- src/components/StrokesForWords.js
+++ src/components/StrokesForWords.js
@@ -1,36 +1,46 @@
-import * as React from 'react'
-import { Component } from 'react';
-import SOURCE_NAMESPACES from '../constant/sourceNamespaces';
-import splitBriefsIntoStrokes from './../utils/splitBriefsIntoStrokes';
+import * as React from "react";
+import { Component } from "react";
+import SOURCE_NAMESPACES from "../constant/sourceNamespaces";
+import splitBriefsIntoStrokes from "./../utils/splitBriefsIntoStrokes";
 import lookupListOfStrokesAndDicts from "../utils/lookupListOfStrokesAndDicts";
 
-import misstrokesJSON from '../json/misstrokes.json'
+import misstrokesJSON from "../json/misstrokes.json";
 import PloverMisstrokesDetail from "./PloverMisstrokesDetail";
-import StrokesAsDiagrams from './StrokesAsDiagrams';
-import MatchedModifiedTranslation from './MatchedModifiedTranslation';
-import LookupResultsOutlinesAndDicts from './LookupResultsOutlinesAndDicts';
+import StrokesAsDiagrams from "./StrokesAsDiagrams";
+import MatchedModifiedTranslation from "./MatchedModifiedTranslation";
+import LookupResultsOutlinesAndDicts from "./LookupResultsOutlinesAndDicts";
 
 class StrokesForWords extends Component {
   state = {
     modifiedWordOrPhrase: "",
     phrase: "",
-    listOfStrokesAndDicts: []
-  }
+    listOfStrokesAndDicts: [],
+  };
 
   componentDidMount() {
     // if (this.props.globalLookupDictionary && this.props.globalLookupDictionary.size < 2 && !this.props.globalLookupDictionaryLoaded) {
 
-    const shouldUsePersonalDictionaries = this.props.personalDictionaries
-      && Object.entries(this.props.personalDictionaries).length > 0
-      && !!this.props.personalDictionaries.dictionariesNamesAndContents;
+    const shouldUsePersonalDictionaries =
+      this.props.personalDictionaries &&
+      Object.entries(this.props.personalDictionaries).length > 0 &&
+      !!this.props.personalDictionaries.dictionariesNamesAndContents;
 
-    this.props.fetchAndSetupGlobalDict(true, shouldUsePersonalDictionaries ? this.props.personalDictionaries : null).then(() => {
-      if (this.props.lookupTerm && this.props.lookupTerm !== undefined && this.props.lookupTerm.length > 0) {
-        this.setState({phrase: this.props.lookupTerm});
-        this.updateWordsForStrokes(this.props.lookupTerm);
-      }
-    })
-      .catch(error => {
+    this.props
+      .fetchAndSetupGlobalDict(
+        true,
+        shouldUsePersonalDictionaries ? this.props.personalDictionaries : null
+      )
+      .then(() => {
+        if (
+          this.props.lookupTerm &&
+          this.props.lookupTerm !== undefined &&
+          this.props.lookupTerm.length > 0
+        ) {
+          this.setState({ phrase: this.props.lookupTerm });
+          this.updateWordsForStrokes(this.props.lookupTerm);
+        }
+      })
+      .catch((error) => {
         console.error(error);
         // this.showDictionaryErrorNotification();
       });
@@ -47,11 +57,23 @@ class StrokesForWords extends Component {
       this.props.onChange(phrase);
     }
 
-    let [listOfStrokesAndDicts, modifiedWordOrPhrase] = lookupListOfStrokesAndDicts(phrase, this.props.globalLookupDictionary);
+    let [listOfStrokesAndDicts, modifiedWordOrPhrase] =
+      lookupListOfStrokesAndDicts(phrase, this.props.globalLookupDictionary);
 
-    if (!(this.props.globalUserSettings && this.props.globalUserSettings.showMisstrokesInLookup)) {
-      listOfStrokesAndDicts = listOfStrokesAndDicts
-        .filter(row => row[2] === SOURCE_NAMESPACES.get("user") || !(misstrokesJSON[row[0]] && modifiedWordOrPhrase === misstrokesJSON[row[0]]))
+    if (
+      !(
+        this.props.globalUserSettings &&
+        this.props.globalUserSettings.showMisstrokesInLookup
+      )
+    ) {
+      listOfStrokesAndDicts = listOfStrokesAndDicts.filter(
+        (row) =>
+          row[2] === SOURCE_NAMESPACES.get("user") ||
+          !(
+            misstrokesJSON[row[0]] &&
+            modifiedWordOrPhrase === misstrokesJSON[row[0]]
+          )
+      );
     }
 
     if (this.props.trackPhrase) {
@@ -61,59 +83,71 @@ class StrokesForWords extends Component {
     this.setState({
       modifiedWordOrPhrase: modifiedWordOrPhrase,
       phrase: phrase,
-      listOfStrokesAndDicts: listOfStrokesAndDicts
-    })
+      listOfStrokesAndDicts: listOfStrokesAndDicts,
+    });
   }
 
-  render () {
-    const stenoLayout = (this.props.userSettings && this.props.userSettings.stenoLayout) ? this.props.userSettings.stenoLayout : 'stenoLayoutAmericanSteno';
+  render() {
+    const stenoLayout =
+      this.props.userSettings && this.props.userSettings.stenoLayout
+        ? this.props.userSettings.stenoLayout
+        : "stenoLayoutAmericanSteno";
 
-    const brief = (this.state.listOfStrokesAndDicts && this.state.listOfStrokesAndDicts[0] && this.state.listOfStrokesAndDicts[0][0]) ? this.state.listOfStrokesAndDicts[0][0] : '';
+    const brief =
+      this.state.listOfStrokesAndDicts &&
+      this.state.listOfStrokesAndDicts[0] &&
+      this.state.listOfStrokesAndDicts[0][0]
+        ? this.state.listOfStrokesAndDicts[0][0]
+        : "";
 
     let strokes = splitBriefsIntoStrokes(brief);
 
-    return (
-      this.props.globalLookupDictionaryLoaded ?
-        <React.Fragment>
-          <label htmlFor="words-for-strokes" className="input-textarea-label input-textarea-label--large mb1 overflow-hidden">Enter words to look up</label>
-          <textarea
-            autoCapitalize="off"
-            autoComplete="off"
-            autoCorrect="off"
-            className="input-textarea input-textarea--large mb3 w-100 overflow-hidden"
-            id="words-for-strokes"
-            onChange={this.handleWordsOnChange.bind(this)}
-            placeholder="e.g. quadruplicate"
-            rows={1}
-            spellCheck={false}
-            value={this.state.phrase}
-            wrap="off"
-            >
-          </textarea>
-          <MatchedModifiedTranslation
-            listOfStrokesAndDicts={this.state.listOfStrokesAndDicts}
-            modifiedWordOrPhrase={this.state.modifiedWordOrPhrase}
-            phrase={this.state.phrase}
-          />
-          <div className="mb1">
-            <StrokesAsDiagrams
-              listOfStrokesAndDicts={this.state.listOfStrokesAndDicts}
-              stenoLayout={stenoLayout}
-              strokes={strokes}
-              userSettings={this.props.userSettings}
-            />
-          </div>
-          <LookupResultsOutlinesAndDicts
+    return this.props.globalLookupDictionaryLoaded ? (
+      <React.Fragment>
+        <label
+          htmlFor="words-for-strokes"
+          className="input-textarea-label input-textarea-label--large mb1 overflow-hidden"
+        >
+          Enter words to look up
+        </label>
+        <textarea
+          autoCapitalize="off"
+          autoComplete="off"
+          autoCorrect="off"
+          className="input-textarea input-textarea--large mb3 w-100 overflow-hidden"
+          id="words-for-strokes"
+          onChange={this.handleWordsOnChange.bind(this)}
+          placeholder="e.g. quadruplicate"
+          rows={1}
+          spellCheck={false}
+          value={this.state.phrase}
+          wrap="off"
+        ></textarea>
+        <MatchedModifiedTranslation
+          listOfStrokesAndDicts={this.state.listOfStrokesAndDicts}
+          modifiedWordOrPhrase={this.state.modifiedWordOrPhrase}
+          phrase={this.state.phrase}
+        />
+        <div className="mb1">
+          <StrokesAsDiagrams
             listOfStrokesAndDicts={this.state.listOfStrokesAndDicts}
             stenoLayout={stenoLayout}
+            strokes={strokes}
+            userSettings={this.props.userSettings}
           />
-          <PloverMisstrokesDetail
-            showMisstrokesInLookup={this.props.globalUserSettings?.showMisstrokesInLookup}
-          />
-        </React.Fragment>
-      : (
-        <>Loading…</>
-      )
+        </div>
+        <LookupResultsOutlinesAndDicts
+          listOfStrokesAndDicts={this.state.listOfStrokesAndDicts}
+          stenoLayout={stenoLayout}
+        />
+        <PloverMisstrokesDetail
+          showMisstrokesInLookup={
+            this.props.globalUserSettings?.showMisstrokesInLookup
+          }
+        />
+      </React.Fragment>
+    ) : (
+      <>Loading…</>
     );
   }
 }
