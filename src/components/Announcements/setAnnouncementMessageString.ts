2792ec4638a94f1b497f8eb7eed4e9a657acc628
Di <didoesdigital+git@gmail.com>
2023-11-20 19:36:07 +1000

    Remove no longer used set announcement methods

--- src/components/Announcements/setAnnouncementMessageString.ts
+++
@@ -1,6 +0,0 @@
-function setAnnouncementMessageString(message: string) {
-  // @ts-ignore 'this' implicitly has type 'any' because it does not have a type annotation.
-  this.setState({ announcementMessage: message });
-}
-
-export default setAnnouncementMessageString;
