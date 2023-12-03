7735083e6596a9ec2de40f9dc230af09c4047fd3
Di <didoesdigital+git@gmail.com>
2023-12-03 10:30:39 +1000

    Add GA4 events for completing Typey Type

--- src/pages/progress/components/ProgressSummaryCompleted.tsx
+++ src/pages/progress/components/ProgressSummaryCompleted.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import GoogleAnalytics from "react-ga4";
 import ProgressLessonLinks from "./ProgressLessonLinks";
 import { format } from "d3-format";
 
@@ -16,6 +17,46 @@ type CompletedProps = {
   yourSeenWordCount: number;
 };
 
+type IntroProps = {
+  yourWordCount: number;
+};
+
+const IntroToCelebrate10000Memorised = ({ yourWordCount }: IntroProps) => {
+  useEffect(() => {
+    GoogleAnalytics.event({
+      category: "MemorisedTypeyType10000",
+      action: "MemorisedTypeyType10000",
+      label: "MemorisedTypeyType10000",
+    });
+  }, []);
+
+  return (
+    <>
+      Woohoo! You rock! What a magnificent effort to memorise 10,000 words. You
+      are an expert stenographer now! You’ve successfully typed{" "}
+      {format(",")(yourWordCount)} words. It’s time to{" "}
+    </>
+  );
+};
+
+const IntroToCelebrate10000Typed = ({ yourWordCount }: IntroProps) => {
+  useEffect(() => {
+    GoogleAnalytics.event({
+      category: "CompletedTypeyType10000",
+      action: "CompletedTypeyType10000",
+      label: "CompletedTypeyType10000",
+    });
+  }, []);
+
+  return (
+    <>
+      Woohoo! You rock! You’ve successfully typed {format(",")(yourWordCount)}{" "}
+      words. You are an accomplished stenographer now! You’ve completed 100% of
+      10,000 words. It’s time to{" "}
+    </>
+  );
+};
+
 const ProgressSummaryCompleted = ({
   restartConfetti,
   userSettings,
@@ -27,18 +68,9 @@ const ProgressSummaryCompleted = ({
     <>
       <p>
         {yourMemorisedWordCount >= 10000 ? (
-          <>
-            Woohoo! You rock! What a magnificent effort to memorise 10,000
-            words. You are an expert stenographer now! You’ve successfully typed{" "}
-            {format(",")(yourWordCount)} words. It’s time to{" "}
-          </>
+          <IntroToCelebrate10000Memorised yourWordCount={yourWordCount} />
         ) : (
-          <>
-            Woohoo! You rock! You’ve successfully typed{" "}
-            {format(",")(yourWordCount)} words. You are an accomplished
-            stenographer now! You’ve completed 100% of 10,000 words. It’s time
-            to{" "}
-          </>
+          <IntroToCelebrate10000Typed yourWordCount={yourWordCount} />
         )}
         <button
           className="button-that-looks-like-a-link"
