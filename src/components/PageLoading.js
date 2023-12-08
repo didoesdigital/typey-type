07058318d8935fdba6351a4792943d49c9da2782
Di <didoesdigital+git@gmail.com>
2023-12-09 09:11:24 +1000

    Convert PageLoading to function component

--- src/components/PageLoading.js
+++ src/components/PageLoading.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import * as Sentry from '@sentry/browser';
-import PageLoadingFailed from './PageLoadingFailed';
-import PageLoadingPastDelay from './PageLoadingPastDelay';
+import React from "react";
+import * as Sentry from "@sentry/browser";
+import PageLoadingFailed from "./PageLoadingFailed";
+import PageLoadingPastDelay from "./PageLoadingPastDelay";
 
-class PageLoading extends Component {
-
-  render() {
-    if (this.props.error) {
-      // When the loader has errored
-      Sentry.captureException(this.props.error);
-      return <PageLoadingFailed />;
-    } else if (this.props.timedOut) {
-      // When the loader has taken longer than the timeout
-      return <div>Taking a long time… <button onClick={ this.props.retry }>Retry</button></div>;
-    } else if (this.props.pastDelay) {
-      // When the loader has taken longer than the delay
-      return <PageLoadingPastDelay />;
-    } else {
-      // When the loader has just started
-      return null;
-    }
+const PageLoading = (props) => {
+  if (props.error) {
+    // When the loader has errored
+    Sentry.captureException(props.error);
+    return <PageLoadingFailed />;
+  } else if (props.timedOut) {
+    // When the loader has taken longer than the timeout
+    return (
+      <div>
+        Taking a long time… <button onClick={props.retry}>Retry</button>
+      </div>
+    );
+  } else if (props.pastDelay) {
+    // When the loader has taken longer than the delay
+    return <PageLoadingPastDelay />;
+  } else {
+    // When the loader has just started
+    return null;
   }
-}
+};
 
 export default PageLoading;
