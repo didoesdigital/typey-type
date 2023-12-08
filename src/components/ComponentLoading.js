b587b173f4078b7c55692de18b1a779f4945c56b
Di <didoesdigital+git@gmail.com>
2023-12-09 09:25:57 +1000

    Housekeeping: format file

--- src/components/ComponentLoading.js
+++ src/components/ComponentLoading.js
@@ -4,24 +4,24 @@ import ComponentLoadingFailed from "./ComponentLoadingFailed";
 import ComponentLoadingPastDelay from "./ComponentLoadingPastDelay";
 
 const ComponentLoading = (props) => {
-    if (props.error) {
-      // When the loader has errored
-      Sentry.captureException(props.error);
-      return <ComponentLoadingFailed />;
-    } else if (props.timedOut) {
-      // When the loader has taken longer than the timeout
-      return (
-        <div>
-          Taking a long time… <button onClick={props.retry}>Retry</button>
-        </div>
-      );
-    } else if (props.pastDelay) {
-      // When the loader has taken longer than the delay
-      return <ComponentLoadingPastDelay />;
-    } else {
-      // When the loader has just started
-      return null;
-    }
+  if (props.error) {
+    // When the loader has errored
+    Sentry.captureException(props.error);
+    return <ComponentLoadingFailed />;
+  } else if (props.timedOut) {
+    // When the loader has taken longer than the timeout
+    return (
+      <div>
+        Taking a long time… <button onClick={props.retry}>Retry</button>
+      </div>
+    );
+  } else if (props.pastDelay) {
+    // When the loader has taken longer than the delay
+    return <ComponentLoadingPastDelay />;
+  } else {
+    // When the loader has just started
+    return null;
+  }
 };
 
 export default ComponentLoading;
