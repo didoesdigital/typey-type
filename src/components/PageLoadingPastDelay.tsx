37a5080e8ab5a81d67bd7bab88cbe67442545128
Di <didoesdigital+git@gmail.com>
2023-12-16 09:07:37 +1000

    Convert PageLoadingPastDelay to function component

--- src/components/PageLoadingPastDelay.tsx
+++ src/components/PageLoadingPastDelay.tsx
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import Subheader from "./Subheader";
 
-class PageLoadingPastDelay extends Component {
-  mainHeading?: HTMLHeadingElement | null;
+const PageLoadingPastDelay = () => {
+  const mainHeading = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    if (mainHeading) {
+      mainHeading.current?.focus();
+    }
+  }, []);
 
-  render() {
-    return (
-      <main id="main">
-        <Subheader>
-          <div className="flex mr1 self-center">
-            <header className="flex items-center min-h-40">
-              <h2
-                ref={(heading) => {
-                  this.mainHeading = heading;
-                }}
-                tabIndex={-1}
-              >
-                Loading…
-              </h2>
-            </header>
-          </div>
-        </Subheader>
-        <div className="p3 mx-auto mw-1024">Loading…</div>
-      </main>
-    );
-  }
-}
+  return (
+    <main id="main">
+      <Subheader>
+        <div className="flex mr1 self-center">
+          <header className="flex items-center min-h-40">
+            <h2 ref={mainHeading} tabIndex={-1}>
+              Loading…
+            </h2>
+          </header>
+        </div>
+      </Subheader>
+      <div className="p3 mx-auto mw-1024">Loading…</div>
+    </main>
+  );
+};
 
 export default PageLoadingPastDelay;
