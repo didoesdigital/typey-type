ffba253220d115f1aadd5549c69d28d9f815599a
Di <didoesdigital+git@gmail.com>
2023-12-16 08:49:02 +1000

    Housekeeping: format and remove unused import

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
-import { IconClosingCross } from './Icon';
-// import GoogleAnalytics from "react-ga4";
+import React, { Component } from "react";
+import { IconClosingCross } from "./Icon";
 
 class Notification extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showNotification: false
-    }
+      showNotification: false,
+    };
   }
 
   componentDidMount() {
     this.setState({
-      showNotification: true
+      showNotification: true,
     });
   }
 
   handleDismiss() {
     if (this.props.onDismiss) {
+      this.setState(
+        {
+          showNotification: false,
+        },
+        () => {
+          this.props.onDismiss();
+        }
+      );
+    } else {
       this.setState({
-        showNotification: false
-      }, () => {
-        this.props.onDismiss();
-      });
-    }
-    else {
-      this.setState({
-        showNotification: false
+        showNotification: false,
       });
     }
   }
@@ -34,20 +35,22 @@ class Notification extends Component {
   render() {
     return (
       <div>
-        { this.state.showNotification ?
+        {this.state.showNotification ? (
           <div className="notification notification--global fixed w-100 flex wrap justify-between pa1 p1 p3 pa3 items-center bg-danger dark:text-coolgrey-900">
-            <div className="notification__message">
-              {this.props.children}
-            </div>
+            <div className="notification__message">{this.props.children}</div>
             <div onClick={this.handleDismiss.bind(this)}>
-              <IconClosingCross role="img" iconWidth="24" iconHeight="24" className="ml1 svg-icon-wrapper svg-baseline" iconTitle="Dismiss notification" />
+              <IconClosingCross
+                role="img"
+                iconWidth="24"
+                iconHeight="24"
+                className="ml1 svg-icon-wrapper svg-baseline"
+                iconTitle="Dismiss notification"
+              />
             </div>
           </div>
-          :
-          null
-        }
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
