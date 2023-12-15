168be80a6d4e3ad4f3e9ba751cf4a7910d0df1e4
Di <didoesdigital+git@gmail.com>
2023-12-16 09:01:19 +1000

    Convert Notification to function component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,57 +1,37 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { IconClosingCross } from "./Icon";
 
-class Notification extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showNotification: false,
-    };
-  }
+const Notification = ({ onDismiss, children }) => {
+  const [showNotification, setShowNotification] = useState(true);
 
-  componentDidMount() {
-    this.setState({
-      showNotification: true,
-    });
-  }
-
-  handleDismiss() {
-    if (this.props.onDismiss) {
-      this.setState(
-        {
-          showNotification: false,
-        },
-        () => {
-          this.props.onDismiss();
-        }
-      );
-    } else {
-      this.setState({
-        showNotification: false,
-      });
+  useEffect(() => {
+    if (!showNotification && onDismiss) {
+      onDismiss();
     }
+  }, [showNotification, onDismiss]);
+
+  function handleDismiss() {
+    setShowNotification(false);
   }
 
-  render() {
-    return (
-      <div>
-        {this.state.showNotification ? (
-          <div className="notification notification--global fixed w-100 flex wrap justify-between pa1 p1 p3 pa3 items-center bg-danger dark:text-coolgrey-900">
-            <div className="notification__message">{this.props.children}</div>
-            <div onClick={this.handleDismiss.bind(this)}>
-              <IconClosingCross
-                role="img"
-                iconWidth="24"
-                iconHeight="24"
-                className="ml1 svg-icon-wrapper svg-baseline"
-                iconTitle="Dismiss notification"
-              />
-            </div>
+  return (
+    <div>
+      {showNotification ? (
+        <div className="notification notification--global fixed w-100 flex wrap justify-between pa1 p1 p3 pa3 items-center bg-danger dark:text-coolgrey-900">
+          <div className="notification__message">{children}</div>
+          <div onClick={handleDismiss.bind(this)}>
+            <IconClosingCross
+              role="img"
+              iconWidth="24"
+              iconHeight="24"
+              className="ml1 svg-icon-wrapper svg-baseline"
+              iconTitle="Dismiss notification"
+            />
           </div>
-        ) : null}
-      </div>
-    );
-  }
-}
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 export default Notification;
