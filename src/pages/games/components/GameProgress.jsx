64e736b3e340119ad4f71a82f4b2c6244b30f530
Di <didoesdigital+git@gmail.com>
2024-02-17 11:17:13 +1000

    Reorganise shared GameProgress.jsx

--- src/pages/games/components/GameProgress.jsx
+++ src/pages/games/components/GameProgress.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-export default function GameProgress({
-  level,
-  levelToWin,
-  round,
-  roundToWin,
-}) {
+const Round = ({ round, roundToWin }) => (
+  <>
+    Round:{" "}
+    <TransitionGroup className={"dib"} component={"span"} key={round}>
+      <CSSTransition timeout={500} classNames="bloop" appear={true}>
+        <strong className="dib">{round}</strong>
+      </CSSTransition>
+    </TransitionGroup>{" "}
+    of {roundToWin}
+    <br />
+  </>
+);
+
+const Level = ({ level, levelToWin }) => {
+  return level ? (
+    <>
+      Level: <strong>{level || 1}</strong> of {levelToWin}
+    </>
+  ) : null;
+};
+
+export default function GameProgress({ level, levelToWin, round, roundToWin }) {
   return (
     <div className="flex flex-grow">
       <p className="text-center w-100">
-        Round:{" "}
-        <TransitionGroup className={"dib"} component={"span"} key={round}>
-          <CSSTransition timeout={500} classNames="bloop" appear={true}>
-            <strong className="dib">{round}</strong>
-          </CSSTransition>
-        </TransitionGroup>{" "}
-        of {roundToWin}
-        <br />
-        {level && (
-          <>
-            Level: <strong>{level || 1}</strong> of {levelToWin}
-          </>
-        )}
+        <Round round={round} roundToWin={roundToWin} />
+        <Level level={level} levelToWin={levelToWin} />
       </p>
     </div>
   );
