417c1fbb580b859766fc5b370b21d3e1d7952b8b
Di <didoesdigital+git@gmail.com>
2024-02-17 13:08:33 +1000

    Convert shared GameProgress to TSX

---
+++ src/pages/games/components/GameProgress.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+type RoundProps = {
+  round: number;
+  roundToWin: number;
+};
+
+const Round: FC<RoundProps> = ({ round, roundToWin }) => (
+  <>
+    Round:{" "}
+    <TransitionGroup className={"dib"} component={"span"} key={round}>
+      <CSSTransition timeout={500} classNames="bloop" appear={true}>
+        <strong className="dib">{round}</strong>
+      </CSSTransition>
+    </TransitionGroup>{" "}
+    of {roundToWin}
+    <br />
+  </>
+);
+
+type LevelProps = {
+  level: number;
+  levelToWin: number;
+};
+
+const Level: FC<LevelProps> = ({ level, levelToWin }) => {
+  return level ? (
+    <>
+      Level: <strong>{level || 1}</strong> of {levelToWin}
+    </>
+  ) : null;
+};
+
+type Props = {
+  round: number;
+  roundToWin: number;
+  level: number;
+  levelToWin: number;
+};
+
+const GameProgress: FC<Props> = ({ level, levelToWin, round, roundToWin }) => (
+  <div className="flex flex-grow">
+    <p className="text-center w-100">
+      <Round round={round} roundToWin={roundToWin} />
+      <Level level={level} levelToWin={levelToWin} />
+    </p>
+  </div>
+);
+
+export default GameProgress;
