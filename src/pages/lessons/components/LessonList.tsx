ce8270b43006ac80852e6786ded1415e07a0883a
na2hiro <a@81.la>
2024-03-31 09:02:14 +0900

    Persist search term on lessons page

--- src/pages/lessons/components/LessonList.tsx
+++ src/pages/lessons/components/LessonList.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import GoogleAnalytics from "react-ga4";
-import { Link } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import { groups } from "d3-array";
 import type { LessonIndexEntry } from "../../../types";
 
@@ -72,9 +72,46 @@ const scrollToHeading = (hash: string) => {
   }
 };
 
+function filterLessons(searchTerm: string, lessonIndex: LessonIndexEntry[]) {
+  const cleanedSearchTerm = searchTerm
+    .trim()
+    .toLowerCase()
+    .replaceAll(/[^ A-Za-z0-9’',*:-]/g, ""); // all characters that don't appear in lesson titles
+
+  if(cleanedSearchTerm.length === 0) return lessonIndex;
+
+  let filteredLessons = lessonIndex.filter((lesson) => {
+    const cleanedLessonTitle = lesson.title.toLowerCase();
+    const searchSnippets = cleanedSearchTerm.split(" ");
+    const titleMatches = searchSnippets.reduce(
+      (trueSoFar, searchSnippet) =>
+        trueSoFar && cleanedLessonTitle.includes(searchSnippet),
+      true
+    );
+    return titleMatches;
+  });
+
+  if (filteredLessons.length === 0) {
+    filteredLessons = lessonIndex.filter((lesson) => {
+      const searchSnippets = cleanedSearchTerm.split(" ");
+      const categoryMatches = searchSnippets.reduce(
+        (trueSoFar, searchSnippet) =>
+          trueSoFar &&
+          (lesson.category.toLowerCase().includes(searchSnippet) ||
+            lesson.subcategory.toLowerCase().includes(searchSnippet)),
+        true
+      );
+      return categoryMatches;
+    });
+  }
+
+  return filteredLessons;
+}
+
 export default function LessonList({ lessonIndex, url }: LessonListProps) {
-  const [searchFilter, setSearchFilter] = useState("");
-  const [filteredLessonIndex, setFilteredLessonIndex] = useState(lessonIndex);
+  const location = useLocation();
+  const searchFilter = new URLSearchParams(location.search).get("q") ?? "";
+  const filteredLessonIndex = filterLessons(searchFilter, lessonIndex);
 
   const searchInputRef = useRef<HTMLInputElement>(null);
 
@@ -96,41 +133,11 @@ export default function LessonList({ lessonIndex, url }: LessonListProps) {
     window.onhashchange = scrollToAnchor;
   }, [lessonIndex]);
 
-  useEffect(() => {
-    setFilteredLessonIndex(lessonIndex);
-  }, [lessonIndex]);
+  const history = useHistory()
 
   const changeSearchFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value;
-    const cleanedSearchTerm = searchTerm
-      .trim()
-      .toLowerCase()
-      .replaceAll(/[^ A-Za-z0-9’',*:-]/g, ""); // all characters that don't appear in lesson titles
-
-    let filteredLessons = lessonIndex.filter((lesson) => {
-      const cleanedLessonTitle = lesson.title.toLowerCase();
-      const searchSnippets = cleanedSearchTerm.split(" ");
-      const titleMatches = searchSnippets.reduce(
-        (trueSoFar, searchSnippet) =>
-          trueSoFar && cleanedLessonTitle.includes(searchSnippet),
-        true
-      );
-      return titleMatches;
-    });
-
-    if (filteredLessons.length === 0) {
-      filteredLessons = lessonIndex.filter((lesson) => {
-        const searchSnippets = cleanedSearchTerm.split(" ");
-        const categoryMatches = searchSnippets.reduce(
-          (trueSoFar, searchSnippet) =>
-            trueSoFar &&
-            (lesson.category.toLowerCase().includes(searchSnippet) ||
-              lesson.subcategory.toLowerCase().includes(searchSnippet)),
-          true
-        );
-        return categoryMatches;
-      });
-    }
+    history.replace({ search: `?q=${searchTerm}` })
 
     if (searchTerm && searchTerm.toString()) {
       GoogleAnalytics.event({
@@ -145,9 +152,6 @@ export default function LessonList({ lessonIndex, url }: LessonListProps) {
         label: "EMPTY_SEARCH_TEXT",
       });
     }
-
-    setSearchFilter(searchTerm);
-    setFilteredLessonIndex(filteredLessons);
   };
 
   const groupedLessons = groups(
