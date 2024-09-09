4058b3c68e7319b5d32b4863740b30e98ebf6e58
Di <didoesdigital+git@gmail.com>
2024-09-09 16:04:13 +1000

    Simplify external links on LessonsIndex
    - replace icon and tooltip with parenthetical link text

--- src/pages/lessons/flashcards/components/FlashcardsModal.tsx
+++ src/pages/lessons/flashcards/components/FlashcardsModal.tsx
@@ -1,17 +1,12 @@
 import React, { useEffect, useState } from "react";
 import OutboundLink from "../../../../components/OutboundLink";
 import ReactModal from "react-modal";
-import { Tooltip } from "react-tippy";
-import { IconExternal } from "../../../../components/IconExternal";
-import useAnnounceTooltip from "../../../../components/Announcer/useAnnounceTooltip";
 
 type Props = {
   fullscreen: boolean;
 };
 
 const FlashcardsModal = ({ fullscreen }: Props) => {
-  const announceTooltip = useAnnounceTooltip();
-
   const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     ReactModal.setAppElement("#js-app");
@@ -79,73 +74,30 @@ const FlashcardsModal = ({ fullscreen }: Props) => {
             more time has passed.
           </p>
           <p>
-            Thanks to Jim Ladd, you can also use the{" "}
-            <OutboundLink
-              eventLabel="Anki"
-              aria-label="Anki (external link opens in new tab)"
-              to="https://apps.ankiweb.net/"
-            >
-              Anki
-              <span className="whitespace-nowrap">
-                {/* @ts-ignore */}
-                <Tooltip
-                  title="(external link opens in new tab)"
-                  className=""
-                  animation="shift"
-                  arrow="true"
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={announceTooltip}
-                >
-                  <IconExternal
-                    ariaHidden="true"
-                    role="presentation"
-                    iconWidth="24"
-                    iconHeight="24"
-                    className="ml1 svg-icon-wrapper svg-baseline"
-                    iconTitle=""
-                  />
-                </Tooltip>
-              </span>
-            </OutboundLink>{" "}
-            app to memorise briefs using the{" "}
-            <OutboundLink
-              eventLabel="flashcard decks for the Top 2000 Words"
-              aria-label="flashcard decks for the Top 2000 Words (external link opens in new tab)"
-              to="https://github.com/jladdjr/anki-decks/tree/master/Plover%20-%20Project%20Gutenberg%20Top%2010k%20Words"
-            >
-              flashcard decks for the Top 2000 Words
-              <span className="whitespace-nowrap">
-                {/* @ts-ignore */}
-                <Tooltip
-                  title="(external link opens in new tab)"
-                  className=""
-                  animation="shift"
-                  arrow="true"
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={announceTooltip}
-                >
-                  <IconExternal
-                    ariaHidden="true"
-                    role="presentation"
-                    iconWidth="24"
-                    iconHeight="24"
-                    className="ml1 svg-icon-wrapper svg-baseline"
-                    iconTitle=""
-                  />
-                </Tooltip>
-              </span>
-            </OutboundLink>{" "}
-            from Project Gutenberg he built using Typey&nbsp;Type steno
-            diagrams.
+            Thanks to Jim Ladd, you can also use the Anki app to memorise
+            briefs. He built flashcard decks for the Top 2000 Words from Project
+            Gutenberg using Typey&nbsp;Type steno diagrams:
           </p>
+          <ul>
+            <li>
+              <OutboundLink
+                eventLabel="Anki"
+                newTabAndIUnderstandTheAccessibilityImplications={true}
+                to="https://apps.ankiweb.net/"
+              >
+                Anki (opens in new tab)
+              </OutboundLink>
+            </li>
+            <li>
+              <OutboundLink
+                eventLabel="flashcard decks for the Top 2000 Words"
+                newTabAndIUnderstandTheAccessibilityImplications={true}
+                to="https://github.com/jladdjr/anki-decks/tree/master/Plover%20-%20Project%20Gutenberg%20Top%2010k%20Words"
+              >
+                Flashcard decks for the Top 2000 Words (opens in new tab)
+              </OutboundLink>
+            </li>
+          </ul>
         </div>
         <div className="text-right">
           <button className="button" onClick={handleCloseModal}>
