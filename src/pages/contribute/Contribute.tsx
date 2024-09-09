a28e48acc56d2d06431dc04c33986c7b96bb11c5
Di <didoesdigital+git@gmail.com>
2024-09-09 18:20:16 +1000

    Simplify external links on Contribute page
    - replace icon and tooltip with parenthetical link text
    - use OutboundLink component for analytics

--- src/pages/contribute/Contribute.tsx
+++ src/pages/contribute/Contribute.tsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import OutboundLink from "../../components/OutboundLink";
 import { Link } from "react-router-dom";
-import { IconExternal } from "../../components/IconExternal";
-import { Tooltip } from "react-tippy";
 import Subheader from "../../components/Subheader";
-import useAnnounceTooltip from "../../components/Announcer/useAnnounceTooltip";
 
 const Contribute = () => {
   const mainHeading = useRef<HTMLHeadingElement>(null);
-  const announceTooltip = useAnnounceTooltip();
 
   useEffect(() => {
     mainHeading.current?.focus();
@@ -34,112 +30,33 @@ const Contribute = () => {
             You can support my efforts on{" "}
             <OutboundLink
               eventLabel="Patreon"
-              aria-label="Patreon (external link opens in new tab)"
+              newTabAndIUnderstandTheAccessibilityImplications={true}
               to="https://www.patreon.com/didoesdigital"
             >
-              Patreon
-              {/* @ts-ignore */}
-              <Tooltip
-                title="Opens in a new tab"
-                animation="shift"
-                arrow="true"
-                className=""
-                duration="200"
-                tabIndex="0"
-                tag="span"
-                theme="didoesdigital"
-                trigger="mouseenter focus click"
-                onShow={announceTooltip}
-              >
-                <IconExternal
-                  ariaHidden="true"
-                  role="presentation"
-                  iconWidth="24"
-                  iconHeight="24"
-                  className="ml1 svg-icon-wrapper svg-baseline"
-                  iconTitle=""
-                />
-              </Tooltip>
+              Patreon (opens in new tab)
             </OutboundLink>
             . A monthly donation helps me build more lessons and features to
-            help you fast-track your steno progress.
+            help you fast-track your steno&nbsp;progress.
           </p>
 
           <h3 id="lessons">Lessons</h3>
           <p>
             You can create your own{" "}
             <Link to="/lessons/custom">custom lesson</Link> and add it to the{" "}
-            <a
-              className=""
-              href="https://docs.google.com/spreadsheets/d/1AlO2SSUwuv3yrz7RI9ix_z1Efbiu_j50c_ibGYwdsgc/edit?usp=sharing"
-              target="_blank"
-              rel="noopener noreferrer"
+            <OutboundLink
+              eventLabel="community’s lessons (opens in new tab)"
+              newTabAndIUnderstandTheAccessibilityImplications={true}
+              to="https://docs.google.com/spreadsheets/d/1AlO2SSUwuv3yrz7RI9ix_z1Efbiu_j50c_ibGYwdsgc/edit?usp=sharing"
             >
-              <span className="whitespace-nowrap">
-                community’s lessons
-                {/* @ts-ignore */}
-                <Tooltip
-                  title="Opens in a new tab"
-                  className=""
-                  animation="shift"
-                  arrow="true"
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={announceTooltip}
-                >
-                  <IconExternal
-                    ariaHidden="true"
-                    role="presentation"
-                    iconWidth="24"
-                    iconHeight="24"
-                    className="ml1 svg-icon-wrapper svg-baseline"
-                    iconTitle=""
-                  />
-                </Tooltip>
-              </span>
-            </a>
+              community’s lessons (opens in new tab)
+            </OutboundLink>
             .
           </p>
           <p>
             If you have an idea for a new lesson,{" "}
             <a href="mailto:typeytype@didoesdigital.com">
               email typeytype@didoesdigital.com
-            </a>{" "}
-            or{" "}
-            <OutboundLink
-              eventLabel="tweet @DiDoesDigital"
-              aria-label="tweet @DiDoesDigital (external link opens in new tab)"
-              to="https://twitter.com/didoesdigital"
-            >
-              <span className="whitespace-nowrap">
-                tweet @DiDoesDigital
-                {/* @ts-ignore */}
-                <Tooltip
-                  title="Opens in a new tab"
-                  className=""
-                  animation="shift"
-                  arrow="true"
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={announceTooltip}
-                >
-                  <IconExternal
-                    ariaHidden="true"
-                    role="presentation"
-                    iconWidth="24"
-                    iconHeight="24"
-                    className="ml1 svg-icon-wrapper svg-baseline"
-                    iconTitle=""
-                  />
-                </Tooltip>
-              </span>
-            </OutboundLink>
+            </a>
             . You can also find Di on Discord.
           </p>
 
@@ -149,132 +66,40 @@ const Contribute = () => {
             faster, add your custom dictionaries to the{" "}
             <OutboundLink
               eventLabel="community’s dictionaries"
-              aria-label="community’s dictionaries (external link opens in new tab)"
+              newTabAndIUnderstandTheAccessibilityImplications={true}
               to="https://docs.google.com/spreadsheets/d/1w-9GciR8D7sWuLVxw9ATstF1tcyCjCe7UtIn7l80cXk/edit?usp=sharing"
             >
-              community’s&nbsp;dictionaries
-              {/* @ts-ignore */}
-              <Tooltip
-                title="Opens in a new tab"
-                animation="shift"
-                arrow="true"
-                className=""
-                duration="200"
-                tabIndex="0"
-                tag="span"
-                theme="didoesdigital"
-                trigger="mouseenter focus click"
-                onShow={announceTooltip}
-              >
-                <IconExternal
-                  ariaHidden="true"
-                  role="presentation"
-                  iconWidth="24"
-                  iconHeight="24"
-                  className="ml1 svg-icon-wrapper svg-baseline"
-                  iconTitle=""
-                />
-              </Tooltip>
+              community’s&nbsp;dictionaries (opens in new tab)
             </OutboundLink>
             .
           </p>
           <p>
             If you notice anything unexpected,{" "}
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSfqBBEs5Fl8vgay03fEXzSU7Ey_pms6Y6Nt2Yk8gFftGhAWQA/viewform?usp=pp_url&entry.1884511690="
-              target="_blank"
-              rel="noopener noreferrer"
-              id="ga--contribute--give-feedback-on-dictionary"
+            <OutboundLink
+              eventLabel="share your feedback on that dictionary (opens in new tab)"
+              newTabAndIUnderstandTheAccessibilityImplications={true}
+              to="https://docs.google.com/forms/d/e/1FAIpQLSfqBBEs5Fl8vgay03fEXzSU7Ey_pms6Y6Nt2Yk8gFftGhAWQA/viewform?usp=pp_url&entry.1884511690="
             >
-              share your feedback on that dictionary
-              {/* @ts-ignore */}
-              <Tooltip
-                title="Opens in a new tab"
-                className=""
-                animation="shift"
-                arrow="true"
-                duration="200"
-                tabIndex="0"
-                tag="span"
-                theme="didoesdigital"
-                trigger="mouseenter focus click"
-                onShow={announceTooltip}
-              >
-                <IconExternal
-                  ariaHidden="true"
-                  role="presentation"
-                  iconWidth="24"
-                  iconHeight="24"
-                  className="ml1 svg-icon-wrapper svg-baseline"
-                  iconTitle=""
-                />
-              </Tooltip>
-            </a>
+              share your feedback on that dictionary (opens in new tab)
+            </OutboundLink>
             .
           </p>
           <p>
             <OutboundLink
               eventLabel="Di’s Steno Dictionaries repo"
-              aria-label="Di’s Steno Dictionaries repo (external link opens in new tab)"
+              newTabAndIUnderstandTheAccessibilityImplications={true}
               to="https://github.com/didoesdigital/steno-dictionaries"
             >
-              Di’s Steno Dictionaries{" "}
-              <span className="whitespace-nowrap">
-                repo
-                {/* @ts-ignore */}
-                <Tooltip
-                  title="Opens in a new tab"
-                  animation="shift"
-                  arrow="true"
-                  className=""
-                  duration="200"
-                  tabIndex="0"
-                  tag="span"
-                  theme="didoesdigital"
-                  trigger="mouseenter focus click"
-                  onShow={announceTooltip}
-                >
-                  <IconExternal
-                    ariaHidden="true"
-                    role="presentation"
-                    iconWidth="24"
-                    iconHeight="24"
-                    className="ml1 svg-icon-wrapper svg-baseline"
-                    iconTitle=""
-                  />
-                </Tooltip>
-              </span>
+              Di’s Steno Dictionaries repo (opens in new tab)
             </OutboundLink>{" "}
             has many dictionaries. These power Typey&nbsp;Type’s stroke
             suggestions. See the{" "}
             <OutboundLink
               eventLabel="repo’s contributing section"
-              aria-label="repo’s contributing section (external link opens in new tab)"
+              newTabAndIUnderstandTheAccessibilityImplications={true}
               to="https://github.com/didoesdigital/steno-dictionaries#contributing"
             >
-              repo’s contributing section
-              {/* @ts-ignore */}
-              <Tooltip
-                title="Opens in a new tab"
-                animation="shift"
-                arrow="true"
-                className=""
-                duration="200"
-                tabIndex="0"
-                tag="span"
-                theme="didoesdigital"
-                trigger="mouseenter focus click"
-                onShow={announceTooltip}
-              >
-                <IconExternal
-                  ariaHidden="true"
-                  role="presentation"
-                  iconWidth="24"
-                  iconHeight="24"
-                  className="ml1 svg-icon-wrapper svg-baseline"
-                  iconTitle=""
-                />
-              </Tooltip>
+              repo’s contributing section (opens in new tab)
             </OutboundLink>
             .
           </p>
@@ -283,32 +108,10 @@ const Contribute = () => {
           <p>
             <OutboundLink
               eventLabel="Typey Type for Stenographers feedback form"
-              aria-label="Share your feedback (form opens in new tab)"
+              newTabAndIUnderstandTheAccessibilityImplications={true}
               to="https://docs.google.com/forms/d/e/1FAIpQLSeevsX2oYEvnDHd3y8weg5_7-T8QZsF93ElAo28JO9Tmog-7Q/viewform?usp=sf_link"
             >
-              Share your feedback
-              {/* @ts-ignore */}
-              <Tooltip
-                title="Form opens in a new tab"
-                animation="shift"
-                arrow="true"
-                className=""
-                duration="200"
-                tabIndex="0"
-                tag="span"
-                theme="didoesdigital"
-                trigger="mouseenter focus click"
-                onShow={announceTooltip}
-              >
-                <IconExternal
-                  ariaHidden="true"
-                  role="presentation"
-                  iconWidth="24"
-                  iconHeight="24"
-                  className="ml1 svg-icon-wrapper svg-baseline"
-                  iconTitle=""
-                />
-              </Tooltip>
+              Share your feedback (opens in new tab)
             </OutboundLink>
             .
           </p>
