86fc5d473d71ea06ba0f2e0443031052534386b5
Di <didoesdigital+git@gmail.com>
2025-06-28 21:59:06 +1000

    Housekeeping: prettier format formatters.ts

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,27 +1,35 @@
 function durationFormatter(durationInMilliseconds: number) {
   // more than 10 hours, return early
   if (durationInMilliseconds >= 36000000) {
-    return "10+ hours"
+    return "10+ hours";
   }
 
   // less than 1 second, return early
   if (durationInMilliseconds < 1000) {
-    return "00:00"
+    return "00:00";
   }
 
   const durationInSeconds = Math.floor(durationInMilliseconds / 1000);
   let hours: string | number = Math.floor(durationInSeconds / 3600);
-  let minutes: string | number = Math.floor((durationInSeconds - (hours * 3600)) / 60);
-  let seconds: string | number = durationInSeconds - (hours * 3600) - (minutes * 60);
+  let minutes: string | number = Math.floor(
+    (durationInSeconds - hours * 3600) / 60
+  );
+  let seconds: string | number =
+    durationInSeconds - hours * 3600 - minutes * 60;
 
-  if (hours < 10) { hours = "0" + hours; }
-  if (minutes < 10) { minutes = "0" + minutes; }
-  if (seconds < 10) { seconds = "0" + seconds; }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
 
   if (durationInMilliseconds < 3600000) {
     return `${minutes}:${seconds}`;
-  }
-  else {
+  } else {
     return `${hours}:${minutes}:${seconds}`;
   }
 }
@@ -29,26 +37,22 @@ function durationFormatter(durationInMilliseconds: number) {
 function humanDurationFormatter(durationInMinutes: number) {
   // more than 10 hours, return early
   if (durationInMinutes >= 600) {
-    return "10+ hours"
+    return "10+ hours";
   }
 
   // less than 1 min, return early
   if (durationInMinutes < 1) {
-    return "<1 min"
+    return "<1 min";
   }
 
   const hours = Math.floor(durationInMinutes / 60);
-  const minutes = Math.floor(durationInMinutes - (hours * 60));
+  const minutes = Math.floor(durationInMinutes - hours * 60);
 
   if (durationInMinutes < 60) {
     return `~${minutes} min`;
-  }
-  else {
+  } else {
     return `~${hours} hr ${minutes} min`;
   }
 }
 
-export {
-  durationFormatter,
-  humanDurationFormatter
-};
+export { durationFormatter, humanDurationFormatter };
