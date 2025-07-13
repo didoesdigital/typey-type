5ced5864f9cfdcf4563866228de4a5618e648185
Di <didoesdigital+git@gmail.com>
2025-07-14 00:06:03 +1000

    Convert KAOES Puzzle to TS

--- src/pages/games/KAOES/Puzzle.tsx
+++ src/pages/games/KAOES/Puzzle.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-// @ts-expect-error TS(7031) FIXME: Binding element 'puzzleText' implicitly has an 'an... Remove this comment to see the full error message
-export default function Puzzle({ puzzleText }) {
+type Props = {
+  puzzleText: string;
+};
+
+export default function Puzzle({ puzzleText }: Props) {
   return (
     <p className="text-center lede mb0">
       <strong>
         Where does the{" "}
         <TransitionGroup className={""} component={"span"} key={puzzleText}>
           <CSSTransition timeout={500} classNames="bloop" appear={true}>
-            <span className="dib" data-chromatic="ignore">{puzzleText}</span>
+            <span className="dib" data-chromatic="ignore">
+              {puzzleText}
+            </span>
           </CSSTransition>
         </TransitionGroup>{" "}
         key belong?
