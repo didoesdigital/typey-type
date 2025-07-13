b859e5520a4c68e1cde35e9b229e4fd2791a91c2
Di <didoesdigital+git@gmail.com>
2025-07-14 01:08:17 +1000

    Convert KAOES utilities to TS

--- src/pages/games/KAOES/utilities.ts
+++ src/pages/games/KAOES/utilities.ts
@@ -28,10 +28,8 @@ const disambiguatedStenoKeys = [
 
 const unambiguousRightHandKeysRegExp = new RegExp(/^-[FBLGDZ]$/);
 
-// @ts-expect-error TS(7006) FIXME: Parameter 'key' implicitly has an 'any' type.
-export const prettyKey = (key) =>
+export const prettyKey = (key: string) =>
   unambiguousRightHandKeysRegExp.test(key) ? key.replace("-", "") : key;
 
-// @ts-expect-error TS(7006) FIXME: Parameter 'previousKey' implicitly has an 'any' ty... Remove this comment to see the full error message
-export const choosePuzzleKey = (previousKey) =>
+export const choosePuzzleKey = (previousKey: string) =>
   shuffle(disambiguatedStenoKeys.filter((key) => key !== previousKey))[0];
