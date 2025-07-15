cdd06ba7fb812e0814bdb54c92995bc4b99aa5e4
Di <didoesdigital+git@gmail.com>
2025-07-15 20:25:30 +1000

    Update Storybook preview syntax and types

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,42 +1,43 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import type { Preview } from "@storybook/react";
+
 import "../src/index.scss";
 
-export const parameters = {
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
-  },
-  options: {
-    storySort: {
-      method: "alphabetical",
-      order: [
-        "Getting Started",
-        ["Overview"],
-        "Pages",
-        ["Home"],
-        "Progress",
-        "Lessons",
-        ["Custom", "Flashcards", "Speed chart", "Metronome"],
-        "Games",
-        [
-          "Games index",
-          "KAOES game",
-          "SHUFL game",
-          "TPEUBGSZ game",
-          "KHAERT game",
-          "Completed",
+    options: {
+      storySort: {
+        method: "alphabetical",
+        order: [
+          "Getting Started",
+          ["Overview"],
+          "Pages",
+          ["Home"],
+          "Progress",
+          "Lessons",
+          ["Custom", "Flashcards", "Speed chart", "Metronome"],
+          "Games",
+          [
+            "Games index",
+            "KAOES game",
+            "SHUFL game",
+            "TPEUBGSZ game",
+            "KHAERT game",
+            "Completed",
+          ],
+          "Material",
         ],
-        "Material",
-      ],
+      },
     },
   },
-};
-
-export const decorators = [
-  (Story) => {
-    return (
+  decorators: [
+    (Story) => (
       <Router basename="/typey-type">
         <Route>
           <div id="js-app" className="app">
@@ -44,6 +45,8 @@ export const decorators = [
           </div>
         </Route>
       </Router>
-    );
-  },
-];
+    ),
+  ],
+};
+
+export default preview;
