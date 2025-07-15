7ec60f705308c55c52e0432b8beb472412ec0c96
Di <didoesdigital+git@gmail.com>
2025-07-15 20:46:06 +1000

    Update story syntax for OutboundLink

--- src/components/OutboundLink.stories.tsx
+++ src/components/OutboundLink.stories.tsx
@@ -1,45 +1,34 @@
 import React from "react";
 import OutboundLink from "./OutboundLink";
 
-export default {
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof OutboundLink> = {
   title: "Components/OutboundLink",
   component: OutboundLink,
 };
 
-// @ts-expect-error TS(7006) FIXME: Parameter 'args' implicitly has an 'any' type.
-const Template = (args) => {
-  const { children, ...propArgs } = args;
+export default meta;
 
-  return (
-    <OutboundLink
-      aria-label="example"
-      eventLabel="example"
-      to="https://joshuagrams.github.io/steno-jig/"
-      {...propArgs}
-    >
-      {children}
-    </OutboundLink>
-  );
+type Story = StoryObj<typeof OutboundLink>;
+
+export const OutboundLinkStory: Story = {
+  args: {
+    children: "Stenojig (external link opens in new tab)",
+    "aria-label": "Stenojig (external link opens in new tab)",
+    eventLabel: "Stenojig (external link opens in new tab)",
+    to: "https://joshuagrams.github.io/steno-jig/",
+  },
 };
 
-export const OutboundLinkStory = Template.bind({});
-// @ts-expect-error TS(2339) FIXME: Property 'args' does not exist on type '(args: any... Remove this comment to see the full error message
-OutboundLinkStory.args = {
-  children: "Stenojig (external link opens in new tab)",
-  "aria-label": "Stenojig (external link opens in new tab)",
-  eventLabel: "Stenojig (external link opens in new tab)",
-  to: "https://joshuagrams.github.io/steno-jig/",
-};
-
-export const OutboundLinkEverythingStory = Template.bind({});
-// @ts-expect-error TS(2339) FIXME: Property 'args' does not exist on type '(args: any... Remove this comment to see the full error message
-OutboundLinkEverythingStory.args = {
-  "aria-label": "Type Racer (external link opens in new tab)",
-  children: <strong>Type Racer</strong>,
-  className: "no-underline",
-  eventLabel: "Type Racer (external link opens in new tab)",
-  // @ts-expect-error TS(7006) FIXME: Parameter 'event' implicitly has an 'any' type.
-  onClick: (event) => console.log(`Clicked: ${event.target}`),
-  style: { lineHeight: 2 },
-  to: "https://play.typeracer.com/?universe=steno",
+export const OutboundLinkEverythingStory: Story = {
+  args: {
+    "aria-label": "Type Racer (external link opens in new tab)",
+    children: <strong>Type Racer</strong>,
+    className: "no-underline",
+    eventLabel: "Type Racer (external link opens in new tab)",
+    onClick: (event) => console.log(`Clicked: ${event.target}`),
+    style: { lineHeight: 2 },
+    to: "https://play.typeracer.com/?universe=steno",
+  },
 };
