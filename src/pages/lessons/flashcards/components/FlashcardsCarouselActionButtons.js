97971ee85089ddd7277a6f374b265898d784e88c
Di <didoesdigital+git@gmail.com>
2025-07-15 21:17:31 +1000

    Convert class component to functional component

--- src/pages/lessons/flashcards/components/FlashcardsCarouselActionButtons.js
+++ src/pages/lessons/flashcards/components/FlashcardsCarouselActionButtons.js
@@ -1,39 +1,99 @@
-import React, { Component } from 'react';
+import React, { useRef } from "react";
 
-class FlashcardsCarouselActionButtons extends Component {
-  render () {
-    let currentSlideContentType = this.props.currentSlideContentType;
-    let ButtonNext = this.props.ButtonNext;
-    let numberOfFlashcards = this.props.numberOfFlashcards;
+// @ts-ignore
+const FlashcardsCarouselActionButtons = (props) => {
+    const showButtonRef = useRef(null);
+    const hardButtonRef = useRef(null);
+    const shuffleButtonRef = useRef(null);
+
+    let currentSlideContentType = props.currentSlideContentType;
+    let ButtonNext = props.ButtonNext;
+    let numberOfFlashcards = props.numberOfFlashcards;
 
     return (
       <React.Fragment>
         <div className="text-center">
-          {/* @ts-expect-error TS(7006) FIXME: Parameter 'showButton' implicitly has an 'any' typ... Remove this comment to see the full error message */}
-          { currentSlideContentType === 'phrase' && numberOfFlashcards > 0 ? <ButtonNext className="link-button carousel__button carousel__button--skip" type="button" onClick={this.props.nextSlide} data-flashcard-feedback="show" value={this.props.currentSlideContent} aria-label="Next card" data-unfocus="true" id="showButton" ref={(showButton) => { this.showButton = showButton; }}>Show</ButtonNext> : null }
+          {currentSlideContentType === "phrase" && numberOfFlashcards > 0 ? (
+            <ButtonNext
+              className="link-button carousel__button carousel__button--skip"
+              type="button"
+              onClick={props.nextSlide}
+              data-flashcard-feedback="show"
+              value={props.currentSlideContent}
+              aria-label="Next card"
+              data-unfocus="true"
+              id="showButton"
+              ref={showButtonRef}
+            >
+              Show
+            </ButtonNext>
+          ) : null}
 
-          { currentSlideContentType === 'stroke' && numberOfFlashcards > 0 ?
-              <>
-                <ButtonNext className="link-button carousel__button carousel__button--easy mr1" type="button" onClick={this.props.nextSlide} data-flashcard-feedback="easy" value={this.props.currentSlideContent} aria-label="Easy, Next card" data-unfocus="true">Easy</ButtonNext>
-              </> : null }
-          { currentSlideContentType === 'stroke' && numberOfFlashcards > 0 ?
-              <>
-                {/* @ts-expect-error TS(7006) FIXME: Parameter 'hardButton' implicitly has an 'any' typ... Remove this comment to see the full error message */}
-                <ButtonNext className="link-button carousel__button carousel__button--hard ml1" type="button" onClick={this.props.nextSlide} data-flashcard-feedback="hard" value={this.props.currentSlideContent} aria-label="Hard, Next card" data-unfocus="true" id="hardButton" ref={(hardButton) => { this.hardButton = hardButton; }}>Hard</ButtonNext>
-              </>
-              :
-              null
-          }
+          {currentSlideContentType === "stroke" && numberOfFlashcards > 0 ? (
+            <>
+              <ButtonNext
+                className="link-button carousel__button carousel__button--easy mr1"
+                type="button"
+                onClick={props.nextSlide}
+                data-flashcard-feedback="easy"
+                value={props.currentSlideContent}
+                aria-label="Easy, Next card"
+                data-unfocus="true"
+              >
+                Easy
+              </ButtonNext>
+            </>
+          ) : null}
+          {currentSlideContentType === "stroke" && numberOfFlashcards > 0 ? (
+            <>
+              <ButtonNext
+                className="link-button carousel__button carousel__button--hard ml1"
+                type="button"
+                onClick={props.nextSlide}
+                data-flashcard-feedback="hard"
+                value={props.currentSlideContent}
+                aria-label="Hard, Next card"
+                data-unfocus="true"
+                id="hardButton"
+                ref={hardButtonRef}
+              >
+                Hard
+              </ButtonNext>
+            </>
+          ) : null}
 
           {/* Finished buttons; they also keep space and avoid subsequent content moving up on finished step */}
-          { currentSlideContentType === 'finished' && numberOfFlashcards > 0 ? <button onClick={this.props.setupFlashCards} className="mr1 link-button carousel__button" data-restart="true" data-unfocus="true">Restart</button> : null }
-          { currentSlideContentType === 'finished' && numberOfFlashcards > 0 ? <button onClick={this.props.setupFlashCards} className="ml1 link-button carousel__button" data-shuffle="true" data-unfocus="true" id="shuffleButton" ref={(shuffleButton) => { this.shuffleButton = shuffleButton; }}>Shuffle</button> : null }
+          {currentSlideContentType === "finished" && numberOfFlashcards > 0 ? (
+            <button
+              onClick={props.setupFlashCards}
+              className="mr1 link-button carousel__button"
+              data-restart="true"
+              data-unfocus="true"
+            >
+              Restart
+            </button>
+          ) : null}
+          {currentSlideContentType === "finished" && numberOfFlashcards > 0 ? (
+            <button
+              onClick={props.setupFlashCards}
+              className="ml1 link-button carousel__button"
+              data-shuffle="true"
+              data-unfocus="true"
+              id="shuffleButton"
+              ref={shuffleButtonRef}
+            >
+              Shuffle
+            </button>
+          ) : null}
 
-          { numberOfFlashcards === 0 ? <p className="mt1 mb0">There are no more flashcards to practice at the moment.</p> : null }
+          {numberOfFlashcards === 0 ? (
+            <p className="mt1 mb0">
+              There are no more flashcards to practice at the moment.
+            </p>
+          ) : null}
         </div>
       </React.Fragment>
     );
-  }
-}
+};
 
 export default FlashcardsCarouselActionButtons;
