39ded49048bd1c49c7e0360da0c012ef037494fa
Di <didoesdigital+git@gmail.com>
2025-07-16 20:19:08 +1000

    Improve HighlightCircle types

--- src/components/Chart/HighlightCircle.tsx
+++ src/components/Chart/HighlightCircle.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
+type Props = {
+  datum: any;
+  xAccessor: (d: any) => number;
+  yAccessor: (d: any) => number;
+  colorAccessor: (d: any) => string;
+};
+
 const HighlightCircle = ({
-  // @ts-expect-error TS(7031) FIXME: Binding element 'datum' implicitly has an 'any' ty... Remove this comment to see the full error message
   datum,
-  // @ts-expect-error TS(7031) FIXME: Binding element 'xAccessor' implicitly has an 'any... Remove this comment to see the full error message
   xAccessor,
-  // @ts-expect-error TS(7031) FIXME: Binding element 'yAccessor' implicitly has an 'any... Remove this comment to see the full error message
   yAccessor,
-  // @ts-expect-error TS(7031) FIXME: Binding element 'colorAccessor' implicitly has an ... Remove this comment to see the full error message
   colorAccessor,
   ...props
-}) => {
+}: Props) => {
   return (
     <circle
       {...props}
