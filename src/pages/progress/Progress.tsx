10b87d4015d233f445a469fc0e73778bc70cbe7f
Di <didoesdigital+git@gmail.com>
2025-07-16 19:49:53 +1000

    Remove old confetti Particle types and use new one

--- src/pages/progress/Progress.tsx
+++ src/pages/progress/Progress.tsx
@@ -32,7 +32,7 @@ import RecommendationBoxFallback from "./components/RecommendationBoxFallback";
 const skipButtonId = "js-flashcards-skip-button";
 const mobileSkipButtonId = "js-mobile-flashcards-skip-button";
 
-let particles: any[] = [];
+let particles: Confetti.ConfettiParticle[] = [];
 
 type Props = {
   lessonsProgress: any;
--- src/utils/confetti.types.ts
+++
@@ -1,9 +0,0 @@
-export type Particle = {
-  /** red, green, blue, alpha */
-  rgbArray: [number, number, number, number];
-  startX: number;
-  startY: number;
-  startTime: number;
-};
-
-export type Particles = Particle[];
