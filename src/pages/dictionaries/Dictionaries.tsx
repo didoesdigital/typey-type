90b7c89c5d67420ad095c67eacd823d1acfc7cde
Di <didoesdigital+git@gmail.com>
2025-07-26 10:30:44 +1000

    Replace useMatch, exact, Route children

--- src/pages/dictionaries/Dictionaries.tsx
+++ src/pages/dictionaries/Dictionaries.tsx
@@ -1,18 +1,18 @@
 import React, { Suspense } from "react";
-import { Route, Routes, useMatch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Loadable from "react-loadable";
 import DictionariesIndex from "./DictionariesIndex";
 import PageLoading from "../../components/PageLoading";
-
-import type {
-  ImportedPersonalDictionaries,
-  LookupDictWithNamespacedDictsAndConfig,
-} from "../../types";
 import { useAppMethods } from "../../states/legacy/AppMethodsContext";
 import { useAtomValue } from "jotai";
 import { userSettingsState } from "../../states/userSettingsState";
 import { useToggleExperiment } from "../lessons/components/UserSettings/updateGlobalUserSetting";
 import DictionaryNotFound from "pages/dictionaries/DictionaryNotFound";
+
+import type {
+  ImportedPersonalDictionaries,
+  LookupDictWithNamespacedDictsAndConfig,
+} from "../../types";
 
 const AsyncDictionary = Loadable({
   loader: () => import("./Dictionary"),
@@ -43,55 +43,57 @@ const Dictionaries = ({
   const { updatePersonalDictionaries, appFetchAndSetupGlobalDict } =
     useAppMethods();
   const toggleExperiment = useToggleExperiment();
-  const match = useMatch({
-    path: "/dictionaries",
-    end: true,
-    caseSensitive: true,
-  });
-  const url = match?.pathname ?? "";
 
   return (
     <div>
       <Routes>
-        <Route
-          path={[
-            `${url}/lessons/:category/:subcategory/:dictionaryPath`,
-            `${url}/lessons/fundamentals/:dictionaryPath`,
-            `${url}/lessons/drills/:dictionaryPath`,
-            `${url}/typey-type/:dictionaryPath`,
-            `${url}/individual/:dictionaryPath`,
-            `${url}/didoesdigital/:dictionaryPath`,
-            `${url}/plover/:dictionaryPath`,
-          ]}
-        >
-          <Suspense fallback={<PageLoading pastDelay={true} />}>
-            <AsyncDictionary />
-          </Suspense>
-        </Route>
-        <Route exact={true} path={`${url}/management`}>
-          <AsyncDictionaryManagement
-            fetchAndSetupGlobalDict={appFetchAndSetupGlobalDict}
-            globalLookupDictionary={globalLookupDictionary}
-            toggleExperiment={toggleExperiment}
-            updatePersonalDictionaries={updatePersonalDictionaries}
-            {...dictionaryProps}
+        {[
+          "lessons/:category/:subcategory/:dictionaryPath",
+          "lessons/fundamentals/:dictionaryPath",
+          "lessons/drills/:dictionaryPath",
+          "typey-type/:dictionaryPath",
+          "individual/:dictionaryPath",
+          "didoesdigital/:dictionaryPath",
+          "plover/:dictionaryPath",
+        ].map((path) => (
+          <Route
+            key={"Dictionaries"}
+            path={path}
+            element={
+              <Suspense fallback={<PageLoading pastDelay={true} />}>
+                <AsyncDictionary />
+              </Suspense>
+            }
           />
-        </Route>
-        <Route exact={true} path={url}>
-          <Suspense fallback={<PageLoading pastDelay={true} />}>
-            <DictionariesIndex
-              userSettings={userSettings}
-              globalLookupDictionary={globalLookupDictionary}
-              globalLookupDictionaryLoaded={globalLookupDictionaryLoaded}
-              personalDictionaries={personalDictionaries}
+        ))}
+        <Route
+          path={"management"}
+          element={
+            <AsyncDictionaryManagement
               fetchAndSetupGlobalDict={appFetchAndSetupGlobalDict}
+              globalLookupDictionary={globalLookupDictionary}
+              toggleExperiment={toggleExperiment}
+              updatePersonalDictionaries={updatePersonalDictionaries}
               {...dictionaryProps}
             />
-          </Suspense>
-        </Route>
-        <Route path={"*"}>
-          <DictionaryNotFound />
-        </Route>
+          }
+        />
+        <Route
+          path={"/"}
+          element={
+            <Suspense fallback={<PageLoading pastDelay={true} />}>
+              <DictionariesIndex
+                userSettings={userSettings}
+                globalLookupDictionary={globalLookupDictionary}
+                globalLookupDictionaryLoaded={globalLookupDictionaryLoaded}
+                personalDictionaries={personalDictionaries}
+                fetchAndSetupGlobalDict={appFetchAndSetupGlobalDict}
+                {...dictionaryProps}
+              />
+            </Suspense>
+          }
+        />
+        <Route path={"*"} element={<DictionaryNotFound />} />
       </Routes>
     </div>
   );
