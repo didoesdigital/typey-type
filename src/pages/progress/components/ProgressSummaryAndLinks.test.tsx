65dc7dd76e20bb86110c33735166e70ffe1a1b3b
Di <didoesdigital+git@gmail.com>
2025-07-26 18:45:22 +1000

    Update .test.tsx for Router v6 syntax

--- src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
+++ src/pages/progress/components/ProgressSummaryAndLinks.test.tsx
@@ -48,15 +48,20 @@ describe("progress summary and links", () => {
   it("renders", () => {
     render(
       <Router basename="/">
-        <Route>
-          <ProgressSummaryAndLinks
-            metWords={metWordsNovice}
-            restartConfetti={() => undefined}
-            userSettings={userSettings}
-            yourMemorisedWordCount={1}
-            yourSeenWordCount={1}
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <ProgressSummaryAndLinks
+                metWords={metWordsNovice}
+                restartConfetti={() => undefined}
+                userSettings={userSettings}
+                yourMemorisedWordCount={1}
+                yourSeenWordCount={1}
+              />
+            }
           />
-        </Route>
+        </Routes>
       </Router>
     );
     const textElement = screen.getByText(/successfully typed/i);
@@ -66,15 +71,20 @@ describe("progress summary and links", () => {
   it("celebrates finishing Typey Type", () => {
     render(
       <Router basename="/">
-        <Route>
-          <ProgressSummaryAndLinks
-            metWords={metWords10000WordsSeen10000Memorised}
-            restartConfetti={() => undefined}
-            userSettings={userSettings}
-            yourMemorisedWordCount={10000}
-            yourSeenWordCount={10000}
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <ProgressSummaryAndLinks
+                metWords={metWords10000WordsSeen10000Memorised}
+                restartConfetti={() => undefined}
+                userSettings={userSettings}
+                yourMemorisedWordCount={10000}
+                yourSeenWordCount={10000}
+              />
+            }
           />
-        </Route>
+        </Routes>
       </Router>
     );
     const textElement = screen.getByText(/You rock!/i);
@@ -84,17 +94,22 @@ describe("progress summary and links", () => {
   it("has 1 seen and 0 memorised", () => {
     render(
       <Router basename="/">
-        <Route>
-          <div data-testid="test-wrapper">
-            <ProgressSummaryAndLinks
-              metWords={{ "one": 1 }}
-              restartConfetti={() => undefined}
-              userSettings={userSettings}
-              yourMemorisedWordCount={0}
-              yourSeenWordCount={1}
-            />
-          </div>
-        </Route>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <div data-testid="test-wrapper">
+                <ProgressSummaryAndLinks
+                  metWords={{ "one": 1 }}
+                  restartConfetti={() => undefined}
+                  userSettings={userSettings}
+                  yourMemorisedWordCount={0}
+                  yourSeenWordCount={1}
+                />
+              </div>
+            }
+          />
+        </Routes>
       </Router>
     );
 
@@ -109,17 +124,22 @@ describe("progress summary and links", () => {
   it("has 1 seen and 1 memorised", () => {
     render(
       <Router basename="/">
-        <Route>
-          <div data-testid="test-wrapper">
-            <ProgressSummaryAndLinks
-              metWords={{ "one": 1, "two": 30 }}
-              restartConfetti={() => undefined}
-              userSettings={userSettings}
-              yourMemorisedWordCount={1}
-              yourSeenWordCount={1}
-            />
-          </div>
-        </Route>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <div data-testid="test-wrapper">
+                <ProgressSummaryAndLinks
+                  metWords={{ "one": 1, "two": 30 }}
+                  restartConfetti={() => undefined}
+                  userSettings={userSettings}
+                  yourMemorisedWordCount={1}
+                  yourSeenWordCount={1}
+                />
+              </div>
+            }
+          />
+        </Routes>
       </Router>
     );
 
@@ -134,17 +154,22 @@ describe("progress summary and links", () => {
   it("has 0 seen and 1 memorised", () => {
     render(
       <Router basename="/">
-        <Route>
-          <div data-testid="test-wrapper">
-            <ProgressSummaryAndLinks
-              metWords={{ "memorised": 30 }}
-              restartConfetti={() => undefined}
-              userSettings={userSettings}
-              yourMemorisedWordCount={30}
-              yourSeenWordCount={0}
-            />
-          </div>
-        </Route>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <div data-testid="test-wrapper">
+                <ProgressSummaryAndLinks
+                  metWords={{ "memorised": 30 }}
+                  restartConfetti={() => undefined}
+                  userSettings={userSettings}
+                  yourMemorisedWordCount={30}
+                  yourSeenWordCount={0}
+                />
+              </div>
+            }
+          />
+        </Routes>
       </Router>
     );
 
@@ -159,28 +184,33 @@ describe("progress summary and links", () => {
   it("has 10 seen and 0 memorised", () => {
     render(
       <Router basename="/">
-        <Route>
-          <div data-testid="test-wrapper">
-            <ProgressSummaryAndLinks
-              metWords={{
-                "seen1": 1,
-                "seen2": 1,
-                "seen3": 1,
-                "seen4": 1,
-                "seen5": 1,
-                "seen6": 1,
-                "seen7": 1,
-                "seen8": 1,
-                "seen9": 1,
-                "seen10": 1,
-              }}
-              restartConfetti={() => undefined}
-              userSettings={userSettings}
-              yourMemorisedWordCount={0}
-              yourSeenWordCount={10}
-            />
-          </div>
-        </Route>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <div data-testid="test-wrapper">
+                <ProgressSummaryAndLinks
+                  metWords={{
+                    "seen1": 1,
+                    "seen2": 1,
+                    "seen3": 1,
+                    "seen4": 1,
+                    "seen5": 1,
+                    "seen6": 1,
+                    "seen7": 1,
+                    "seen8": 1,
+                    "seen9": 1,
+                    "seen10": 1,
+                  }}
+                  restartConfetti={() => undefined}
+                  userSettings={userSettings}
+                  yourMemorisedWordCount={0}
+                  yourSeenWordCount={10}
+                />
+              </div>
+            }
+          />
+        </Routes>
       </Router>
     );
 
@@ -195,29 +225,34 @@ describe("progress summary and links", () => {
   it("has 10 seen and 1 memorised", () => {
     render(
       <Router basename="/">
-        <Route>
-          <div data-testid="test-wrapper">
-            <ProgressSummaryAndLinks
-              metWords={{
-                "seen1": 1,
-                "seen2": 1,
-                "seen3": 1,
-                "seen4": 1,
-                "seen5": 1,
-                "seen6": 1,
-                "seen7": 1,
-                "seen8": 1,
-                "seen9": 1,
-                "seen10": 1,
-                "memorised": 30,
-              }}
-              restartConfetti={() => undefined}
-              userSettings={userSettings}
-              yourMemorisedWordCount={1}
-              yourSeenWordCount={10}
-            />
-          </div>
-        </Route>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <div data-testid="test-wrapper">
+                <ProgressSummaryAndLinks
+                  metWords={{
+                    "seen1": 1,
+                    "seen2": 1,
+                    "seen3": 1,
+                    "seen4": 1,
+                    "seen5": 1,
+                    "seen6": 1,
+                    "seen7": 1,
+                    "seen8": 1,
+                    "seen9": 1,
+                    "seen10": 1,
+                    "memorised": 30,
+                  }}
+                  restartConfetti={() => undefined}
+                  userSettings={userSettings}
+                  yourMemorisedWordCount={1}
+                  yourSeenWordCount={10}
+                />
+              </div>
+            }
+          />
+        </Routes>
       </Router>
     );
 
@@ -230,30 +265,35 @@ describe("progress summary and links", () => {
   it("has 10 seen and 2 memorised", () => {
     render(
       <Router basename="/">
-        <Route>
-          <div data-testid="test-wrapper">
-            <ProgressSummaryAndLinks
-              metWords={{
-                "seen1": 1,
-                "seen2": 1,
-                "seen3": 1,
-                "seen4": 1,
-                "seen5": 1,
-                "seen6": 1,
-                "seen7": 1,
-                "seen8": 1,
-                "seen9": 1,
-                "seen10": 1,
-                "memorised1": 30,
-                "memorised2": 30,
-              }}
-              restartConfetti={() => undefined}
-              userSettings={userSettings}
-              yourMemorisedWordCount={2}
-              yourSeenWordCount={10}
-            />
-          </div>
-        </Route>
+        <Routes>
+          <Route
+            path={"/"}
+            element={
+              <div data-testid="test-wrapper">
+                <ProgressSummaryAndLinks
+                  metWords={{
+                    "seen1": 1,
+                    "seen2": 1,
+                    "seen3": 1,
+                    "seen4": 1,
+                    "seen5": 1,
+                    "seen6": 1,
+                    "seen7": 1,
+                    "seen8": 1,
+                    "seen9": 1,
+                    "seen10": 1,
+                    "memorised1": 30,
+                    "memorised2": 30,
+                  }}
+                  restartConfetti={() => undefined}
+                  userSettings={userSettings}
+                  yourMemorisedWordCount={2}
+                  yourSeenWordCount={10}
+                />
+              </div>
+            }
+          />
+        </Routes>
       </Router>
     );
 
@@ -267,17 +307,20 @@ describe("progress summary and links", () => {
     render(
       <Router basename="/">
         <Routes>
-          <Route>
-            <div data-testid="test-wrapper">
-              <ProgressSummaryAndLinks
-                metWords={{}}
-                restartConfetti={() => undefined}
-                userSettings={userSettings}
-                yourMemorisedWordCount={0}
-                yourSeenWordCount={0}
-              />
-            </div>
-          </Route>
+          <Route
+            path={"/"}
+            element={
+              <div data-testid="test-wrapper">
+                <ProgressSummaryAndLinks
+                  metWords={{}}
+                  restartConfetti={() => undefined}
+                  userSettings={userSettings}
+                  yourMemorisedWordCount={0}
+                  yourSeenWordCount={0}
+                />
+              </div>
+            }
+          />
         </Routes>
       </Router>
     );
