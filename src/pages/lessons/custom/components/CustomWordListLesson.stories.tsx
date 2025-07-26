df922e85060dc0f345326546b74d7baeef8f6df0
Di <didoesdigital+git@gmail.com>
2025-07-26 20:21:42 +1000

    Update CustomWordListLesson stories syntax

--- src/pages/lessons/custom/components/CustomWordListLesson.stories.tsx
+++ src/pages/lessons/custom/components/CustomWordListLesson.stories.tsx
@@ -1,64 +1,67 @@
 import React from "react";
 
-import { within, userEvent } from "@storybook/test";
-import { expect } from "@storybook/test";
+import { expect, within, userEvent } from "@storybook/test";
 
 import CustomWordListLesson from "./CustomWordListLesson";
 
-export default {
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof CustomWordListLesson> = {
   title: "Lessons/Custom/CustomWordListLesson",
   component: CustomWordListLesson,
-};
-
-// @ts-expect-error TS(7006) FIXME: Parameter 'args' implicitly has an 'any' type.
-const Template = (args) => (
-  <div className="p3">
-    <CustomWordListLesson
-      globalLookupDictionary={
-        new Map([
-          ["hello", [["H-L", "typey:typey-type.json"]]],
-          ["world", [["WORLD", "typey:typey-type.json"]]],
-        ])
-      }
-      {...args}
-    />
-  </div>
-);
-
-export const CustomWordListLessonEmptyState = Template.bind({});
-// @ts-expect-error TS(2339) FIXME: Property 'storyName' does not exist on type '(args... Remove this comment to see the full error message
-CustomWordListLessonEmptyState.storyName = "Empty state";
-// @ts-expect-error TS(2339) FIXME: Property 'play' does not exist on type '(args: any... Remove this comment to see the full error message
-CustomWordListLessonEmptyState.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-
-  await expect(
-    canvas.getByTestId("custom-lesson-formatted-pre"),
-  ).not.toHaveClass("quote");
-};
-
-export const CustomWordListLessonFilled = Template.bind({});
-// @ts-expect-error TS(2339) FIXME: Property 'storyName' does not exist on type '(args... Remove this comment to see the full error message
-CustomWordListLessonFilled.storyName = "Dictionary filled";
-// @ts-expect-error TS(2339) FIXME: Property 'play' does not exist on type '(args: any... Remove this comment to see the full error message
-CustomWordListLessonFilled.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-
-  await userEvent.type(
-    within(canvasElement).getByLabelText(
-      "Paste a word list without strokes here to create a custom lesson (using Plover theory by default):",
+  decorators: [
+    (Story) => (
+      <div className="p3">
+        <Story />
+      </div>
     ),
-    `hello
-world`,
-  );
+  ],
+};
 
-  await expect(canvas.getByTestId("custom-lesson-formatted-pre")).toHaveClass(
-    "quote",
-  );
-  await expect(
-    canvas.getByTestId("custom-lesson-formatted-code"),
-  ).toHaveTextContent(/WORLD/);
-  await expect(
-    canvas.getByTestId("custom-lesson-formatted-code"),
-  ).toHaveTextContent(/H-L/);
+export default meta;
+
+type Story = StoryObj<typeof CustomWordListLesson>;
+
+export const CustomWordListLessonEmptyState: Story = {
+  name: "Empty state",
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(
+      canvas.getByTestId("custom-lesson-formatted-pre")
+    ).not.toHaveClass("quote");
+  },
+};
+
+export const CustomWordListLessonFilled: Story = {
+  name: "Dictionary filled",
+  args: {
+    globalLookupDictionary: new Map([
+      ["hello", [["H-L", "typey:typey-type.json"]]],
+      ["world", [["WORLD", "typey:typey-type.json"]]],
+    ]),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const wordListInput = canvas.getByLabelText(
+      "Paste a word list without strokes here to create a custom lesson (using Plover theory by default):"
+    );
+
+    await userEvent.type(
+      wordListInput,
+      `hello
+world`
+    );
+
+    await expect(canvas.getByTestId("custom-lesson-formatted-pre")).toHaveClass(
+      "quote"
+    );
+    await expect(
+      canvas.getByTestId("custom-lesson-formatted-code")
+    ).toHaveTextContent(/WORLD/);
+    await expect(
+      canvas.getByTestId("custom-lesson-formatted-code")
+    ).toHaveTextContent(/H-L/);
+  },
 };
