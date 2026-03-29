479469448a99de540996de5420def6b2de6f5a5c
Di <didoesdigital+git@gmail.com>
2026-03-29 11:31:27 +1000

    Replace TransitionGroup with Motion in Puzzle

--- src/pages/games/KAOES/Puzzle.tsx
+++ src/pages/games/KAOES/Puzzle.tsx
@@ -1,21 +1,36 @@
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { motion } from "motion/react";
+import { useReducedMotion } from "framer-motion";
 
 type Props = {
   puzzleText: string;
 };
 
 export default function Puzzle({ puzzleText }: Props) {
+  const shouldReduceMotion = useReducedMotion();
   return (
     <p className="text-center lede mb0">
       <strong>
         Where does the{" "}
-        <TransitionGroup className={""} component={"span"} key={puzzleText}>
-          <CSSTransition timeout={500} classNames="bloop" appear={true}>
-            <span className="dib" data-chromatic="ignore">
-              {puzzleText}
-            </span>
-          </CSSTransition>
-        </TransitionGroup>{" "}
+        <motion.span
+          className={"dib"}
+          animate={{
+            transform: shouldReduceMotion
+              ? "scale(1)"
+              : ["scale(1)", "scale(2)", "scale(1)"],
+          }}
+          transition={{
+            duration: 1,
+            ease: [
+              [0.41, 0, 0.48, 1],
+              [0.61, 0, 0.28, 1],
+            ],
+          }}
+          key={puzzleText}
+        >
+          <span className="dib" data-chromatic="ignore">
+            {puzzleText}
+          </span>
+        </motion.span>{" "}
         key belong?
       </strong>
     </p>
